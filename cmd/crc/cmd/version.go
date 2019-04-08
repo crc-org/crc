5f8145a63383e1923294e3f4d4d20b0cf2289769
Lalatendu Mohanty <lmohanty@redhat.com>
2019-04-08 19:52:06 +0530

    Issue #13 Add code for --output json for commands
    Signed-off-by: Lalatendu Mohanty <lmohanty@redhat.com>

--- cmd/crc/cmd/version.go
+++ cmd/crc/cmd/version.go
@@ -1,13 +1,36 @@
+/*
+Copyright (C) 2019 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
 	crcPkg "github.com/code-ready/crc/pkg/crc"
+	outputPkg "github.com/code-ready/crc/pkg/crc/output"
 	"github.com/spf13/cobra"
 )
 
+var (
+	outputFormat string
+)
+
 func init() {
+	versionCmd.Flags().StringVarP(&outputFormat, "output", "o", "", "json, for output in json format")
 	rootCmd.AddCommand(versionCmd)
 }
 
@@ -21,5 +44,15 @@ var versionCmd = &cobra.Command{
 }
 
 func runPrintVersion(arguments []string) {
-	fmt.Printf("version: %s+%s\n", crcPkg.GetCRCVersion(), crcPkg.GetCommitSha())
+	output := fmt.Sprintf("%s+%s\n", crcPkg.GetCRCVersion(), crcPkg.GetCommitSha())
+	if outputFormat == "json" {
+		strOutput := outputPkg.VersionOutputStruct{output}
+		jsonOutput, err := json.Marshal(strOutput)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(jsonOutput))
+	} else {
+		fmt.Println("Version: ", output)
+	}
 }
