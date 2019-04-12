c3dd11b2094f970d11c194048dee2f4d749e2901
Lalatendu Mohanty <lmohanty@redhat.com>
2019-04-12 16:59:58 +0530

    Issue #29 Adding travis CI and make test
    Signed-off-by: Lalatendu Mohanty <lmohanty@redhat.com>

--- pkg/crc/logging/logging.go
+++ pkg/crc/logging/logging.go
@@ -4,7 +4,7 @@ import (
 	"github.com/code-ready/crc/pkg/crc/output"
 )
 
-func Log(s string, args ... interface{}) {
+func Log(s string, args ...interface{}) {
 	// temporary solution until log framework has been decided
-	output.Out(s, args)
+	output.Out(s, args...)
 }
