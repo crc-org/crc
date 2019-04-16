ea48701ff4d13f279bc2d8f5d77b0cc6c6300f1a
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2019-04-16 17:33:06 +0530

    Issue #7 Add preflight test for Mac/Virtualbox

--- pkg/crc/preflight/preflight_checks_darwin.go
+++ pkg/crc/preflight/preflight_checks_darwin.go
@@ -1,3 +1,72 @@
 package preflight
 
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	crcos "github.com/code-ready/crc/pkg/os"
+)
+
+const (
+	virtualBoxDownloadURL   = "https://download.virtualbox.org/virtualbox/6.0.4/VirtualBox-6.0.4-128413-OSX.dmg"
+	virtualBoxMountLocation = "/Volumes/VirtualBox"
+)
+
+var (
+	virtualBoxPkgLocation = fmt.Sprintf("%s/VirtualBox.pkg", virtualBoxMountLocation)
+)
+
 // Add darwin specific checks
+func checkVirtualBoxInstalled() (bool, error) {
+	path, err := exec.LookPath("VBoxManage")
+	if err != nil {
+		return false, errors.New("VirtualBox cli VBoxManage is not found in the path")
+	}
+	fi, _ := os.Stat(path)
+	if fi.Mode()&os.ModeSymlink != 0 {
+		path, err = os.Readlink(path)
+		if err != nil {
+			return false, errors.New("VirtualBox cli VBoxManage is not found in the path")
+		}
+	}
+	return true, nil
+}
+
+func fixVirtualBoxInstallation() (bool, error) {
+	// Download the driver binary in /tmp
+	tempFilePath := filepath.Join(os.TempDir(), "virtualbox.dmg")
+	out, err := os.Create(tempFilePath)
+	if err != nil {
+		return false, err
+	}
+	defer out.Close()
+	resp, err := http.Get(virtualBoxDownloadURL)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return false, err
+	}
+
+	stdOut, stdErr, err := crcos.RunWithPrivilege("hdiutil", "attach", tempFilePath)
+	if err != nil {
+		return false, fmt.Errorf("Could not able to mount the virtualbox.dmg file: %s %v: %s", stdOut, err, stdErr)
+	}
+	stdOut, stdErr, err = crcos.RunWithPrivilege("installer", "-package", virtualBoxPkgLocation, "-target", "/")
+	if err != nil {
+		return false, fmt.Errorf("Could not able to install VirtualBox.pkg: %s %v: %s", stdOut, err, stdErr)
+	}
+	stdOut, stdErr, err = crcos.RunWithPrivilege("hdiutil", "detach", virtualBoxMountLocation)
+	if err != nil {
+		return false, fmt.Errorf("Could not able to install VirtualBox.pkg: %s %v: %s", stdOut, err, stdErr)
+	}
+	return true, nil
+}
--- pkg/crc/preflight/preflight_darwin.go
+++ pkg/crc/preflight/preflight_darwin.go
@@ -1,11 +1,25 @@
 package preflight
 
+import (
+	cmdConfig "github.com/code-ready/crc/cmd/crc/cmd/config"
+	"github.com/code-ready/crc/pkg/crc/config"
+)
+
 // StartPreflightChecks performs the preflight checks before starting the cluster
 func StartPreflightChecks() {
-
+	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckVirtualBoxInstalled.Name),
+		checkVirtualBoxInstalled,
+		"Checking if VirtualBox is Installed",
+		config.GetBool(cmdConfig.WarnCheckVirtualBoxInstalled.Name),
+	)
 }
 
 // SetupHost performs the prerequisite checks and setups the host to run the cluster
 func SetupHost() {
-
+	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckVirtualBoxInstalled.Name),
+		checkVirtualBoxInstalled,
+		fixVirtualBoxInstallation,
+		"Setting up virtualization",
+		config.GetBool(cmdConfig.WarnCheckVirtualBoxInstalled.Name),
+	)
 }
