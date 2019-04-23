fef884912125f3c423947b759a8c31d6d982c38c
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2019-04-23 17:57:43 +0530

    Issue #80 Update the vendor directory

---
+++ vendor/github.com/code-ready/machine/libmachine/libmachine.go
@@ -0,0 +1,152 @@
+package libmachine
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"io"
+
+	"github.com/code-ready/machine/drivers/errdriver"
+	"github.com/code-ready/machine/libmachine/auth"
+	"github.com/code-ready/machine/libmachine/drivers"
+	"github.com/code-ready/machine/libmachine/drivers/plugin/localbinary"
+	"github.com/code-ready/machine/libmachine/drivers/rpc"
+	"github.com/code-ready/machine/libmachine/host"
+	"github.com/code-ready/machine/libmachine/log"
+	"github.com/code-ready/machine/libmachine/mcnerror"
+	"github.com/code-ready/machine/libmachine/mcnutils"
+	"github.com/code-ready/machine/libmachine/persist"
+	"github.com/code-ready/machine/libmachine/ssh"
+	"github.com/code-ready/machine/libmachine/state"
+	"github.com/code-ready/machine/libmachine/version"
+)
+
+type API interface {
+	io.Closer
+	NewHost(driverName string, rawDriver []byte) (*host.Host, error)
+	Create(h *host.Host) error
+	persist.Store
+	GetMachinesDir() string
+}
+
+type Client struct {
+	certsDir       string
+	IsDebug        bool
+	SSHClientType  ssh.ClientType
+	GithubAPIToken string
+	*persist.Filestore
+	clientDriverFactory rpcdriver.RPCClientDriverFactory
+}
+
+func NewClient(storePath, certsDir string) *Client {
+	return &Client{
+		certsDir:            certsDir,
+		IsDebug:             false,
+		SSHClientType:       ssh.External,
+		Filestore:           persist.NewFilestore(storePath, certsDir, certsDir),
+		clientDriverFactory: rpcdriver.NewRPCClientDriverFactory(),
+	}
+}
+
+func (api *Client) NewHost(driverName string, rawDriver []byte) (*host.Host, error) {
+	driver, err := api.clientDriverFactory.NewRPCClientDriver(driverName, rawDriver)
+	if err != nil {
+		return nil, err
+	}
+
+	return &host.Host{
+		ConfigVersion: version.ConfigVersion,
+		Name:          driver.GetMachineName(),
+		Driver:        driver,
+		DriverName:    driver.DriverName(),
+		HostOptions: &host.Options{
+			AuthOptions: &auth.Options{
+				CertDir:          api.certsDir,
+				CaCertPath:       filepath.Join(api.certsDir, "ca.pem"),
+				CaPrivateKeyPath: filepath.Join(api.certsDir, "ca-key.pem"),
+				ClientCertPath:   filepath.Join(api.certsDir, "cert.pem"),
+				ClientKeyPath:    filepath.Join(api.certsDir, "key.pem"),
+				ServerCertPath:   filepath.Join(api.GetMachinesDir(), "server.pem"),
+				ServerKeyPath:    filepath.Join(api.GetMachinesDir(), "server-key.pem"),
+			},
+		},
+	}, nil
+}
+
+func (api *Client) Load(name string) (*host.Host, error) {
+	h, err := api.Filestore.Load(name)
+	if err != nil {
+		return nil, err
+	}
+
+	d, err := api.clientDriverFactory.NewRPCClientDriver(h.DriverName, h.RawDriver)
+	if err != nil {
+		// Not being able to find a driver binary is a "known error"
+		if _, ok := err.(localbinary.ErrPluginBinaryNotFound); ok {
+			h.Driver = errdriver.NewDriver(h.DriverName)
+			return h, nil
+		}
+		return nil, err
+	}
+
+	if h.DriverName == "virtualbox" {
+		h.Driver = drivers.NewSerialDriver(d)
+	} else {
+		h.Driver = d
+	}
+
+	return h, nil
+}
+
+// Create is the wrapper method which covers all of the boilerplate around
+// actually creating, provisioning, and persisting an instance in the store.
+func (api *Client) Create(h *host.Host) error {
+	log.Info("Running pre-create checks...")
+
+	if err := h.Driver.PreCreateCheck(); err != nil {
+		return mcnerror.ErrDuringPreCreate{
+			Cause: err,
+		}
+	}
+
+	if err := api.Save(h); err != nil {
+		return fmt.Errorf("Error saving host to store before attempting creation: %s", err)
+	}
+
+	log.Info("Creating machine...")
+
+	if err := api.performCreate(h); err != nil {
+		return fmt.Errorf("Error creating machine: %s", err)
+	}
+
+	log.Debug("Reticulating splines...")
+
+	return nil
+}
+
+func (api *Client) performCreate(h *host.Host) error {
+	if err := h.Driver.Create(); err != nil {
+		return fmt.Errorf("Error in driver during machine creation: %s", err)
+	}
+
+	if err := api.Save(h); err != nil {
+		return fmt.Errorf("Error saving host to store after attempting creation: %s", err)
+	}
+
+	// TODO: Not really a fan of just checking "none" or "ci-test" here.
+	if h.Driver.DriverName() == "none" || h.Driver.DriverName() == "ci-test" {
+		return nil
+	}
+
+	log.Info("Waiting for machine to be running, this may take a few minutes...")
+	if err := mcnutils.WaitFor(drivers.MachineInState(h.Driver, state.Running)); err != nil {
+		return fmt.Errorf("Error waiting for machine to be running: %s", err)
+	}
+
+	log.Info("Machine is up and running!")
+	return nil
+}
+
+func (api *Client) Close() error {
+	return api.clientDriverFactory.Close()
+}
