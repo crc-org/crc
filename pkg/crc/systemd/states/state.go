3d3f6b520e351b47feb926922dd04f847b759251
Gerard Braad <me@gbraad.nl>
2019-04-25 16:10:32 +0800

    Issue #94 Add systemd interaction

---
+++ pkg/crc/systemd/states/state.go
@@ -0,0 +1,44 @@
+package states
+
+
+import (
+	"strings"
+)
+
+type State int
+
+const (
+	Unknown  State = iota
+	Running
+	Stopped
+	NotFound
+	Error
+)
+
+var states = []string {
+	"unknown",
+	"active (running)",
+	"inactive (dead)",
+	"could not be found",
+	"error",
+}
+
+func (s State) String() string {
+	if int(s) >= 0 && int(s) < len(states) {
+		return states[s]
+	}
+	return ""
+}
+
+func Compare(input string) State {
+	if strings.Contains(input, states[Running]) {
+		return Running		
+	}
+	if strings.Contains(input, states[Stopped]) {
+		return Stopped		
+	}
+	if strings.Contains(input, states[NotFound]) {
+		return NotFound		
+	}
+	return Unknown
+}
