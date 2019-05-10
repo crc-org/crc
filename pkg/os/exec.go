8030f9210e6375af20a1c9a5f7c2b2c738ee9cfe
Anjan Nath <kaludios@gmail.com>
2019-05-10 09:08:13 +0530

    Issue #123 Fix preflight checks to work on localized environments

--- pkg/os/exec.go
+++ pkg/os/exec.go
@@ -2,6 +2,7 @@ package os
 
 import (
 	"bytes"
+	"os"
 	"os/exec"
 )
 
@@ -13,6 +14,20 @@ func RunWithPrivilege(cmdAndArgs ...string) (string, string, error) {
 	cmd := exec.Command(sudo, cmdAndArgs...)
 	stdOut := new(bytes.Buffer)
 	stdErr := new(bytes.Buffer)
+	cmd.Stdout = stdOut
+	cmd.Stderr = stdErr
 	err = cmd.Run()
+	return stdOut.String(), stdErr.String(), err
+}
+
+func RunWithDefaultLocale(command string, args ...string) (string, string, error) {
+	cmd := exec.Command(command, args...)
+	cmd.Env = ReplaceEnv(os.Environ(), "LC_ALL", "C")
+	cmd.Env = ReplaceEnv(cmd.Env, "LANG", "C")
+	stdOut := new(bytes.Buffer)
+	stdErr := new(bytes.Buffer)
+	cmd.Stdout = stdOut
+	cmd.Stderr = stdErr
+	err := cmd.Run()
 	return stdOut.String(), stdErr.String(), err
 }
