ef4c7a2325c0e6cf3a0dce8c95867a010561bd97
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2019-06-11 13:40:10 +0530

    Update the bundle version to 4.1.0

--- pkg/crc/constants/constants_linux.go
+++ pkg/crc/constants/constants_linux.go
@@ -3,5 +3,5 @@ package constants
 const (
 	OcBinaryName  = "oc"
 	DefaultOcURL  = "https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz"
-	DefaultBundle = "crc_libvirt_4.1.0-rc.5.tar.xz"
+	DefaultBundle = "crc_libvirt_4.1.0.tar.xz"
 )
