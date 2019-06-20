8818839317345e4c987e3830df6d39016d6d85b1
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2019-06-20 07:01:11 +0530

    Issue #249 Add Instance IP to host /etc/resolv.conf
    This will allow us to add instance IP on fly so user doesn't
    need to do it manually.
    
    As preflight check we are also changing the permission for
    host /etc/resolv.conf file to provide RW access to current user.
    
    Upstream side issue to make sure oc should honour the resolver file.
    - https://github.com/openshift/origin/issues/23266

--- cmd/crc/cmd/config/config_darwin.go
+++ cmd/crc/cmd/config/config_darwin.go
@@ -6,6 +6,8 @@ import (
 
 var (
 	// Preflight checks
-	SkipCheckResolverFilePermissions = createSetting("skip-check-resolver-file-permissions", nil, []validationFnType{validations.ValidateBool})
-	WarnCheckResolverFilePermissions = createSetting("warn-check-resolver-file-permissions", nil, []validationFnType{validations.ValidateBool})
+	SkipCheckResolverFilePermissions   = createSetting("skip-check-resolver-file-permissions", nil, []validationFnType{validations.ValidateBool})
+	WarnCheckResolverFilePermissions   = createSetting("warn-check-resolver-file-permissions", nil, []validationFnType{validations.ValidateBool})
+	SkipCheckResolvConfFilePermissions = createSetting("skip-check-resolv-conf-file-permissions", nil, []validationFnType{validations.ValidateBool})
+	WarnCheckResolvConfFilePermissions = createSetting("warn-check-resolv-conf-file-permissions", nil, []validationFnType{validations.ValidateBool})
 )
