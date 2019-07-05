8b08bc824a340a0637aba0db2f85896df3562b53
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2019-07-05 09:12:45 +0530

    Issue #286 Add GetDefaultBundle function
    Right now, all the bundle version are hard
    coded to platform specific contants, this
    makes update to bundle version error prone.

--- pkg/crc/constants/constants_darwin.go
+++ pkg/crc/constants/constants_darwin.go
@@ -1,7 +1,15 @@
 package constants
 
-const (
-	OcBinaryName  = "oc"
-	DefaultOcURL  = "https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/macosx/oc.tar.gz"
-	DefaultBundle = "crc_hyperkit_4.1.3.tar.xz"
+import (
+	"fmt"
+	"github.com/code-ready/crc/pkg/crc/version"
 )
+
+const (
+	OcBinaryName = "oc"
+	DefaultOcURL = "https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/macosx/oc.tar.gz"
+)
+
+func GetDefaultBundle() string {
+	return fmt.Sprintf("crc_hyperkit_%s.tar.xz", version.GetBundleVersion())
+}
--- pkg/crc/constants/constants_windows.go
+++ pkg/crc/constants/constants_windows.go
@@ -1,8 +1,16 @@
 package constants
 
+import (
+	"fmt"
+	"github.com/code-ready/crc/pkg/crc/version"
+)
+
 const (
 	OcBinaryName = "oc.exe"
 	DefaultOcURL = "https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/windows/oc.zip"
-	// TODO: we will change once we have correct bundle for windows
-	DefaultBundle = "crc_hyperv_4.1.3.tar.xz"
 )
+
+func GetDefaultBundle() string {
+	// TODO: we will change once we have correct bundle for windows
+	return fmt.Sprintf("crc_hyperv_%s.tar.xz", version.GetBundleVersion())
+}
