4088403ac62ae0c0a0e6a641438cd0159f1d8191
Christophe Fergeau <cfergeau@redhat.com>
2019-07-08 20:28:29 +0200

    Add hyperkit preflight checks
    crc-driver-hyperkit and hyperkit will be downloaded to
    ~/.crc/bin and need both to be suid.

--- cmd/crc/cmd/config/config_darwin.go
+++ cmd/crc/cmd/config/config_darwin.go
@@ -10,4 +10,8 @@ var (
 	WarnCheckResolverFilePermissions   = createSetting("warn-check-resolver-file-permissions", nil, []validationFnType{validations.ValidateBool})
 	SkipCheckResolvConfFilePermissions = createSetting("skip-check-resolv-conf-file-permissions", nil, []validationFnType{validations.ValidateBool})
 	WarnCheckResolvConfFilePermissions = createSetting("warn-check-resolv-conf-file-permissions", nil, []validationFnType{validations.ValidateBool})
+	SkipCheckHyperKitDriver            = createSetting("skip-check-hyperkit-installed", nil, []validationFnType{validations.ValidateBool})
+	WarnCheckHyperKitDriver            = createSetting("warn-check-hyperkit-installed", nil, []validationFnType{validations.ValidateBool})
+	SkipCheckHyperKitInstalled         = createSetting("skip-check-hyperkit-driver", nil, []validationFnType{validations.ValidateBool})
+	WarnCheckHyperKitInstalled         = createSetting("warn-check-hyperkit-driver", nil, []validationFnType{validations.ValidateBool})
 )
--- pkg/crc/preflight/preflight_checks_darwin.go
+++ pkg/crc/preflight/preflight_checks_darwin.go
@@ -6,13 +6,19 @@ import (
 	"golang.org/x/sys/unix"
 	"io"
 	"net/http"
+	neturl "net/url"
 	"os"
 	"os/exec"
 	"os/user"
+	"path"
 	"path/filepath"
+	"strings"
+	"syscall"
 
+	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/logging"
 	"github.com/code-ready/crc/pkg/crc/oc"
+	dl "github.com/code-ready/crc/pkg/download"
 	crcos "github.com/code-ready/crc/pkg/os"
 )
 
@@ -20,13 +26,18 @@ const (
 	virtualBoxDownloadURL   = "https://download.virtualbox.org/virtualbox/6.0.4/VirtualBox-6.0.4-128413-OSX.dmg"
 	virtualBoxMountLocation = "/Volumes/VirtualBox"
 
+	hyperkitDriverCommand = "crc-driver-hyperkit"
+	hyperkitDriverVersion = "0.12.3"
+
 	resolverDir  = "/etc/resolver"
 	resolverFile = "/etc/resolver/testing"
 	hostFile     = "/etc/hosts"
 )
 
 var (
-	virtualBoxPkgLocation = fmt.Sprintf("%s/VirtualBox.pkg", virtualBoxMountLocation)
+	virtualBoxPkgLocation     = fmt.Sprintf("%s/VirtualBox.pkg", virtualBoxMountLocation)
+	hyperkitDownloadURL       = fmt.Sprintf("https://github.com/code-ready/machine-driver-hyperkit/releases/download/v%s/hyperkit", hyperkitDriverVersion)
+	hyperkitDriverDownloadURL = fmt.Sprintf("https://github.com/code-ready/machine-driver-hyperkit/releases/download/v%s/crc-driver-hyperkit", hyperkitDriverVersion)
 )
 
 // Add darwin specific checks
@@ -82,6 +93,146 @@ func fixVirtualBoxInstallation() (bool, error) {
 		return false, fmt.Errorf("Could not install VirtualBox.pkg: %s %v: %s", stdOut, err, stdErr)
 	}
 	logging.Debug("VirtualBox installed")
+	return true, nil
+}
+
+func tryRemoveDestFile(url string, destDir string) error {
+	u, err := neturl.Parse(url)
+	if err != nil {
+		return fmt.Errorf("Cannot parse URL %s", url)
+	}
+
+	urlPath, err := neturl.PathUnescape(u.EscapedPath())
+	if err != nil {
+		return fmt.Errorf("Cannot unescape URL path %s", urlPath)
+	}
+	destFilename := path.Base(urlPath)
+	destPath := filepath.Join(destDir, destFilename)
+	err = os.Remove(destPath)
+	if err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("Could not remove %s: %v", destPath, err)
+	}
+
+	return nil
+}
+
+func download(url string, destDir string, mode os.FileMode) (string, error) {
+	err := os.MkdirAll(destDir, 0111|mode)
+	if err != nil && !os.IsExist(err) {
+		return "", fmt.Errorf("Cannot create directory %s", destDir)
+	}
+
+	// If the destination file already exists, dl.Download may not be able to
+	// overwrite it if we made it suid. We can however delete it beforehand.
+	err = tryRemoveDestFile(url, destDir)
+	if err != nil {
+		return "", err
+	}
+
+	filename, err := dl.Download(url, destDir)
+	if err != nil {
+		return "", err
+	}
+
+	err = os.Chmod(filename, mode)
+	if err != nil {
+		os.Remove(filename)
+		return "", err
+	}
+
+	return filename, nil
+}
+
+func setSuid(path string) error {
+	logging.DebugF("Making %s suid", path)
+
+	stdOut, stdErr, err := crcos.RunWithPrivilege("chown", "root:wheel", path)
+	if err != nil {
+		return fmt.Errorf("Unable to set ownership of %s to root:wheel: %s %v: %s",
+			path, stdOut, err, stdErr)
+	}
+
+	/* Can't do this before the chown as the chown will reset the suid bit */
+	stdOut, stdErr, err = crcos.RunWithPrivilege("chmod", "u+s", path)
+	if err != nil {
+		return fmt.Errorf("Unable to set suid bit on %s: %s %v: %s", path, stdOut, err, stdErr)
+	}
+	return nil
+}
+
+func checkSuid(path string) (bool, error) {
+	fi, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	if fi.Mode()&os.ModeSetuid == 0 {
+		return false, fmt.Errorf("%s does not have the SUID bit set (%s)", path, fi.Mode().String())
+	}
+	if fi.Sys().(*syscall.Stat_t).Uid != 0 {
+		return false, fmt.Errorf("%s is not owned by root", path)
+	}
+
+	return true, nil
+}
+
+func checkHyperKitInstalled() (bool, error) {
+	logging.DebugF("Checking if hyperkit is installed")
+	hyperkitPath := filepath.Join(constants.CrcBinDir, "hyperkit")
+	err := unix.Access(hyperkitPath, unix.X_OK)
+	if err != nil {
+		logging.DebugF("%s not executable", hyperkitPath)
+		return false, err
+	}
+
+	return checkSuid(hyperkitPath)
+}
+
+func fixHyperKitInstallation() (bool, error) {
+	hyperkitFile, err := download(hyperkitDownloadURL, constants.CrcBinDir, 0755)
+	if err != nil {
+		return false, err
+	}
+
+	err = setSuid(hyperkitFile)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func checkMachineDriverHyperKitInstalled() (bool, error) {
+	logging.DebugF("Checking if %s is installed", hyperkitDriverCommand)
+	hyperkitPath := filepath.Join(constants.CrcBinDir, hyperkitDriverCommand)
+	err := unix.Access(hyperkitPath, unix.X_OK)
+	if err != nil {
+		return false, err
+	}
+
+	// Check the version of driver if it matches to supported one
+	stdOut, stdErr, err := crcos.RunWithDefaultLocale(hyperkitPath, "version")
+	if err != nil {
+		return false, err
+	}
+	if !strings.Contains(stdOut, hyperkitDriverVersion) {
+		return false, fmt.Errorf("%s does not have right version \n Required: %s \n Got: %s use 'crc setup' command.\n %v\n", hyperkitDriverCommand, hyperkitDriverVersion, stdOut, stdErr)
+	}
+	logging.DebugF("%s is already installed in %s", hyperkitDriverCommand, hyperkitPath)
+
+	return checkSuid(hyperkitPath)
+}
+
+func fixMachineDriverHyperKitInstalled() (bool, error) {
+	hyperkitDriverPath, err := download(hyperkitDriverDownloadURL, constants.CrcBinDir, 0755)
+	if err != nil {
+		return false, err
+	}
+
+	err = setSuid(hyperkitDriverPath)
+	if err != nil {
+		return false, err
+	}
+
 	return true, nil
 }
 
