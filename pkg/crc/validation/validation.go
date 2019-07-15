fbd67715b86e22dbee26dd34719d6e8a6be8ed2f
Anjan Nath <kaludios@gmail.com>
2019-07-15 20:55:09 +0530

    Issue #317 Update bundle validation check and extract function

--- pkg/crc/validation/validation.go
+++ pkg/crc/validation/validation.go
@@ -47,8 +47,8 @@ func ValidateBundle(bundle string) error {
 	// Check if the version of the bundle provided by user is same as what is released with crc.
 	releaseBundleVersion := version.GetBundleVersion()
 	userProvidedBundleVersion := filepath.Base(bundle)
-	if !strings.Contains(userProvidedBundleVersion, fmt.Sprintf("%s.tar.xz", releaseBundleVersion)) {
-		return errors.NewF("%s bundle is not supported for this release use updated one (crc_<hypervisor>_%s.tar.xz)", userProvidedBundleVersion, releaseBundleVersion)
+	if !strings.Contains(userProvidedBundleVersion, fmt.Sprintf("%s.crcbundle", releaseBundleVersion)) {
+		return errors.NewF("%s bundle is not supported for this release use updated one (crc_<hypervisor>_%s.crcbundle)", userProvidedBundleVersion, releaseBundleVersion)
 	}
 	return nil
 }
