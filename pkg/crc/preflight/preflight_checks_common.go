crc-org/crc

commit 69f693fe45ea41b1c6b61b6eaf2cc0632bb45443

Issue #356 Unpack bundle to disk on crc setup command

pkg/crc/preflight/preflight_checks_common.go
@@ -1,0 +1,46 @@
+package preflight
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/YourFin/binappend"
+	"github.com/kardianos/osext"
+
+	"github.com/code-ready/crc/pkg/crc/constants"
+)
+
+func checkBundleCached() (bool, error) {
+	if _, err := os.Stat(constants.DefaultBundlePath); os.IsNotExist(err) {
+		return false, err
+	}
+	return true, nil
+}
+
+func fixBundleCached() (bool, error) {
+	if constants.BundleEmbedded() {
+		currentExecutable, err := osext.Executable()
+		if err != nil {
+			return false, err
+		}
+		extractor, err := binappend.MakeExtractor(currentExecutable)
+		if err != nil {
+			return false, err
+		}
+		f, err := os.Create(constants.DefaultBundlePath)
+		if err != nil {
+			return false, err
+		}
+		available := extractor.AvalibleData()
+		data, err := extractor.ByteArray(available[0])
+		if err != nil {
+			return false, err
+		}
+		w := bufio.NewWriter(f)
+		defer w.Flush()
+		w.Write(data)
+		return true, nil
+	}
+	return false, fmt.Errorf("CRC bundle is not embedded in the binary, see 'crc help' for more details.")
+}

cmd/crc/cmd/config/config.go
@@ -33,6 +33,8 @@ var (
 	// Preflight checks
 	SkipCheckVirtualBoxInstalled = createSetting("skip-check-virtualbox-installed", nil, []validationFnType{validations.ValidateBool})
 	WarnCheckVirtualBoxInstalled = createSetting("warn-check-virtualbox-installed", nil, []validationFnType{validations.ValidateBool})
+	SkipCheckBundleCached        = createSetting("skip-check-bundle-cached", nil, []validationFnType{validations.ValidateBool})
+	WarnCheckBundleCached        = createSetting("warn-check-bundle-cached", true, []validationFnType{validations.ValidateBool})
 )
 
 // CreateSetting returns a filled struct of ConfigSetting

cmd/crc/cmd/start.go
@@ -78,7 +78,7 @@ func runStart(arguments []string) {
 
 func initStartCmdFlagSet() *pflag.FlagSet {
 	flagSet := pflag.NewFlagSet("start", pflag.ExitOnError)
-	flagSet.StringP(config.Bundle.Name, "b", constants.GetDefaultBundle(), "The system bundle used for deployment of the OpenShift cluster.")
+	flagSet.StringP(config.Bundle.Name, "b", constants.DefaultBundlePath, "The system bundle used for deployment of the OpenShift cluster.")
 	flagSet.StringP(config.VMDriver.Name, "d", machine.DefaultDriver.Driver, fmt.Sprintf("The driver to use for the CRC VM. Possible values: %v", machine.SupportedDriverValues()))
 	flagSet.StringP(config.PullSecretFile.Name, "p", "", fmt.Sprintf("File path of Image pull secret for User (Download it from %s)", constants.DefaultPullSecretURL))
 	flagSet.IntP(config.CPUs.Name, "c", constants.DefaultCPUs, "Number of CPU cores to allocate to the CRC VM")

go.mod
@@ -3,6 +3,7 @@ module github.com/code-ready/crc
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/DATA-DOG/godog v0.7.13
+	github.com/YourFin/binappend v0.0.0-20181105185800-0add4bf0b9ad
 	github.com/cavaliercoder/grab v2.0.0+incompatible
 	github.com/code-ready/clicumber v0.0.0-20190503113956-2563aed4ef12
 	github.com/code-ready/goodhosts v0.0.0-20190712111040-f090f3f77c26

go.sum
@@ -4,6 +4,8 @@ github.com/DATA-DOG/godog v0.7.13 h1:JmgpKcra7Vf3yzI9vPsWyoQRx13tyKziHtXWDCUUgok
 github.com/DATA-DOG/godog v0.7.13/go.mod h1:z2OZ6a3X0/YAKVqLfVzYBwFt3j6uSt3Xrqa7XTtcQE0=
 github.com/Netflix/go-expect v0.0.0-20180615182759-c93bf25de8e8 h1:xzYJEypr/85nBpB11F9br+3HUrpgb+fcm5iADzXXYEw=
 github.com/Netflix/go-expect v0.0.0-20180615182759-c93bf25de8e8/go.mod h1:oX5x61PbNXchhh0oikYAH+4Pcfw5LKv21+Jnpr6r6Pc=
+github.com/YourFin/binappend v0.0.0-20181105185800-0add4bf0b9ad h1:OUogh+sUEo6yRwGEkqifcr3MfXNi0AtdCSwh7H8yTUM=
+github.com/YourFin/binappend v0.0.0-20181105185800-0add4bf0b9ad/go.mod h1:QhzJSct0NZ/q7HOtQrw867061u93HYPWa4KTotzdzls=
 github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6/go.mod h1:grANhF5doyWs3UAsr3K4I6qtAmlQcZDesFNEHPZAzj8=
 github.com/bugsnag/bugsnag-go v1.5.1/go.mod h1:2oa8nejYd4cQ/b0hMIopN0lCRxU0bueqREvZLWFrtK8=
 github.com/bugsnag/panicwrap v1.2.0/go.mod h1:D/8v3kj0zr8ZAKg1AQ6crr+5VwKN5eIywRkfhyM/+dE=

pkg/crc/constants/constants_darwin.go
@@ -3,11 +3,16 @@ package constants
 import (
 	"fmt"
 	"github.com/code-ready/crc/pkg/crc/version"
+	"path/filepath"
 )
 
 const (
 	OcBinaryName = "oc"
 	DefaultOcURL = "https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/macosx/oc.tar.gz"
+)
+
+var (
+	DefaultBundlePath = filepath.Join(CrcBaseDir, GetDefaultBundle())
 )
 
 func GetDefaultBundle() string {

pkg/crc/constants/constants_linux.go
@@ -3,11 +3,16 @@ package constants
 import (
 	"fmt"
 	"github.com/code-ready/crc/pkg/crc/version"
+	"path/filepath"
 )
 
 const (
 	OcBinaryName = "oc"
 	DefaultOcURL = "https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/linux/oc.tar.gz"
+)
+
+var (
+	DefaultBundlePath = filepath.Join(CrcBaseDir, GetDefaultBundle())
 )
 
 func GetDefaultBundle() string {

pkg/crc/constants/constants_windows.go
@@ -3,11 +3,16 @@ package constants
 import (
 	"fmt"
 	"github.com/code-ready/crc/pkg/crc/version"
+	"path/filepath"
 )
 
 const (
 	OcBinaryName = "oc.exe"
 	DefaultOcURL = "https://mirror.openshift.com/pub/openshift-v4/clients/oc/latest/windows/oc.zip"
+)
+
+var (
+	DefaultBundlePath = filepath.Join(CrcBaseDir, GetDefaultBundle())
 )
 
 func GetDefaultBundle() string {

commit a75b35951d6d2d9824388ca61913ab3163604e81

Issue #414 preflight: Move oc caching to common code
Now that 69f693fe introduced a file for preflight checks common to all
platforms, we can move the preflight check that the oc binary is present
to that file rather than duplicating it between all the platforms.

This fixes https://github.com/code-ready/crc/issues/380 and
https://github.com/code-ready/crc/issues/414

pkg/crc/preflight/preflight_checks_common.go
@@ -2,6 +2,7 @@ package preflight
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 
@@ -11,6 +12,8 @@ import (
 	cmdConfig "github.com/code-ready/crc/cmd/crc/cmd/config"
 	"github.com/code-ready/crc/pkg/crc/config"
 	"github.com/code-ready/crc/pkg/crc/constants"
+	"github.com/code-ready/crc/pkg/crc/logging"
+	"github.com/code-ready/crc/pkg/crc/oc"
 )
 
 func checkBundleCached() (bool, error) {
@@ -45,4 +48,23 @@ func fixBundleCached() (bool, error) {
 		return true, nil
 	}
 	return false, fmt.Errorf("CRC bundle is not embedded in the binary, see 'crc help' for more details.")
+}
+
+// Check if oc binary is cached or not
+func checkOcBinaryCached() (bool, error) {
+	oc := oc.OcCached{}
+	if !oc.IsCached() {
+		return false, errors.New("oc binary is not cached.")
+	}
+	logging.Debug("oc binary already cached")
+	return true, nil
+}
+
+func fixOcBinaryCached() (bool, error) {
+	oc := oc.OcCached{}
+	if err := oc.EnsureIsCached(); err != nil {
+		return false, fmt.Errorf("Not able to download oc %v", err)
+	}
+	logging.Debug("oc binary cached")
+	return true, nil
 }

pkg/crc/preflight/preflight_checks_darwin.go
@@ -17,7 +17,6 @@ import (
 
 	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/logging"
-	"github.com/code-ready/crc/pkg/crc/oc"
 	dl "github.com/code-ready/crc/pkg/download"
 	crcos "github.com/code-ready/crc/pkg/os"
 )
@@ -256,25 +255,6 @@ func fixResolverFilePermissions() (bool, error) {
 	}
 
 	return addFileWritePermissionToUser(resolverFile)
-}
-
-// Check if oc binary is cached or not
-func checkOcBinaryCached() (bool, error) {
-	oc := oc.OcCached{}
-	if !oc.IsCached() {
-		return false, errors.New("oc binary is not cached.")
-	}
-	logging.Debug("oc binary already cached")
-	return true, nil
-}
-
-func fixOcBinaryCached() (bool, error) {
-	oc := oc.OcCached{}
-	if err := oc.EnsureIsCached(); err != nil {
-		return false, fmt.Errorf("Not able to download oc %v", err)
-	}
-	logging.Debug("oc binary cached")
-	return true, nil
 }
 
 func checkResolvConfFilePermissions() (bool, error) {

pkg/crc/preflight/preflight_checks_linux.go
@@ -18,7 +18,6 @@ import (
 	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/logging"
 	"github.com/code-ready/crc/pkg/crc/machine/libvirt"
-	"github.com/code-ready/crc/pkg/crc/oc"
 	"github.com/code-ready/crc/pkg/crc/systemd"
 
 	crcos "github.com/code-ready/crc/pkg/os"
@@ -469,24 +468,5 @@ func fixCrcNetworkManagerConfig() (bool, error) {
 	}
 
 	logging.Debug("NetworkManager configuration fixed")
-	return true, nil
-}
-
-// Check if oc binary is cached or not
-func checkOcBinaryCached() (bool, error) {
-	oc := oc.OcCached{}
-	if !oc.IsCached() {
-		return false, errors.New("oc binary is not cached.")
-	}
-	logging.Debug("oc binary already cached")
-	return true, nil
-}
-
-func fixOcBinaryCached() (bool, error) {
-	oc := oc.OcCached{}
-	if err := oc.EnsureIsCached(); err != nil {
-		return false, fmt.Errorf("Not able to download oc %v", err)
-	}
-	logging.Debug("oc binary cached")
 	return true, nil
 }

pkg/crc/preflight/preflight_checks_windows.go
@@ -7,9 +7,6 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/code-ready/crc/pkg/crc/logging"
-	"github.com/code-ready/crc/pkg/crc/oc"
-
 	"github.com/code-ready/crc/pkg/crc/errors"
 	"github.com/code-ready/crc/pkg/os/windows/powershell"
 )
@@ -21,25 +18,6 @@ const (
 	hypervDefaultVirtualSwitchName = "Default Switch"
 	hypervDefaultVirtualSwitchId   = "c08cb7b8-9b3c-408e-8e30-5e16a3aeb444"
 )
-
-// Check if oc binary is cached or not
-func checkOcBinaryCached() (bool, error) {
-	oc := oc.OcCached{}
-	if !oc.IsCached() {
-		return false, errors.New("oc binary is not cached.")
-	}
-	logging.Debug("oc binary already cached")
-	return true, nil
-}
-
-func fixOcBinaryCached() (bool, error) {
-	oc := oc.OcCached{}
-	if err := oc.EnsureIsCached(); err != nil {
-		return false, fmt.Errorf("Not able to download oc %v", err)
-	}
-	logging.Debug("oc binary cached")
-	return true, nil
-}
 
 func checkVersionOfWindowsUpdate() (bool, error) {
 	windowsReleaseId := `(Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ReleaseId).ReleaseId`