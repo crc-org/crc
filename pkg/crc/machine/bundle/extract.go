870b6c388784a717bd0b1b2c37f3f78475ff2b0c
Christophe Fergeau <cfergeau@redhat.com>
2019-08-15 18:52:03 +0200

    Issue #424 Use new extract.Uncompress helper
    This fixes https://github.com/code-ready/crc/issues/424

--- pkg/crc/machine/bundle/extract.go
+++ pkg/crc/machine/bundle/extract.go
@@ -1,79 +1,16 @@
 package bundle
 
 import (
-	"archive/tar"
-	"github.com/xi2/xz"
-	"io"
-	"os"
 	"path/filepath"
 
 	"github.com/code-ready/crc/pkg/crc/constants"
+	"github.com/code-ready/crc/pkg/extract"
 )
 
 func Extract(sourcepath string) (*CrcBundleInfo, error) {
-	file, err := os.Open(filepath.Clean(sourcepath))
-
+	err := extract.Uncompress(sourcepath, constants.MachineCacheDir)
 	if err != nil {
 		return nil, err
-	}
-	defer file.Close()
-
-	var fileReader io.Reader
-
-	if fileReader, err = xz.NewReader(file, 0); err != nil {
-		return nil, err
-	}
-
-	tarBallReader := tar.NewReader(fileReader)
-
-	// Extracting tarred files
-	for {
-		header, err := tarBallReader.Next()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return nil, err
-		}
-		// get the individual filename and extract to the specified directory
-		filename := filepath.Join(constants.MachineCacheDir, header.Name)
-
-		switch header.Typeflag {
-		case tar.TypeDir:
-			// handle directory
-			// log directory (filename)
-			err = os.MkdirAll(filename, os.FileMode(header.Mode))
-
-			if err != nil {
-				return nil, err
-			}
-
-		case tar.TypeReg, tar.TypeGNUSparse:
-			// handle normal file
-			// log file (filename)
-			writer, err := os.Create(filename)
-
-			if err != nil {
-				return nil, err
-			}
-			defer writer.Close()
-
-			_, err = io.Copy(writer, tarBallReader)
-
-			if err != nil {
-				return nil, err
-			}
-
-			err = os.Chmod(filename, os.FileMode(header.Mode))
-
-			if err != nil {
-				return nil, err
-			}
-
-		default:
-			// ignore
-		}
-
 	}
 
 	return GetCachedBundleInfo(filepath.Base(sourcepath))
