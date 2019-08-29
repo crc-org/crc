13fc56814f0229ab7afef1aa81aa630aabeb063a
Christophe Fergeau <cfergeau@redhat.com>
2019-08-29 18:11:14 +0200

    Issue #518: config: Sort the configuration keys
    They currently are shown in an arbitrary order, this commit sorts them
    before printing them so that the normal config comes before all the
    preflight keys, and the preflight keys are sorted ignoring their
    'warn-'/'skip-' prefixes
    It would probably make sense to only show the preflight options when a
    --preflight flag is used, so that the regular option are easier to see.
    
    https://github.com/code-ready/crc/issues/518

--- cmd/crc/cmd/config/config.go
+++ cmd/crc/cmd/config/config.go
@@ -1,12 +1,13 @@
 package config
 
 import (
-	"github.com/code-ready/crc/pkg/crc/constants"
-	"github.com/spf13/cobra"
+	"sort"
 	"strings"
 
 	cfg "github.com/code-ready/crc/pkg/crc/config"
+	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/machine"
+	"github.com/spf13/cobra"
 )
 
 var (
@@ -41,9 +42,47 @@ to the options that you set when you run the 'crc start' command.
 Configurable properties (enter as SUBCOMMAND): ` + "\n\n" + configurableFields()
 }
 
+func isPreflightKey(key string) bool {
+	return strings.HasPrefix(key, "skip-") || strings.HasPrefix(key, "warn-")
+}
+
+// less is used to sort the config keys. We want to sort first the regular keys, and
+// then the keys related to preflight starting with a skip- or warn- prefix. We want
+// these preflight keys to be grouped by pair: 'skip-bar', 'warn-bar', 'skip-foo', 'warn-foo'
+// would be sorted in that order.
+func less(lhsKey, rhsKey string) bool {
+	if isPreflightKey(lhsKey) {
+		if isPreflightKey(rhsKey) {
+			// lhs is preflight, rhs is preflight
+			if lhsKey[4:] == rhsKey[4:] {
+				// we want skip-foo before warn-foo
+				return lhsKey < rhsKey
+			} else {
+				// ignore skip-/warn- prefix
+				return lhsKey[4:] < rhsKey[4:]
+			}
+		} else {
+			// lhs is preflight, rhs is not preflight
+			return false
+		}
+	}
+
+	if isPreflightKey(rhsKey) {
+		// lhs is not preflight, rhs is preflight
+		return true
+	}
+
+	// lhs is not preflight, rhs is not preflight
+	return lhsKey < rhsKey
+}
+
 func configurableFields() string {
 	var fields []string
-	for _, key := range cfg.AllConfigKeys() {
+	keys := cfg.AllConfigKeys()
+	sort.Slice(keys, func(i, j int) bool {
+		return less(keys[i], keys[j])
+	})
+	for _, key := range keys {
 		fields = append(fields, " * "+key)
 	}
 	return strings.Join(fields, "\n")
