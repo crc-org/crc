32f6d75e34f5d7655a60aa08746c98072717a462
Anjan Nath <kaludios@gmail.com>
2019-08-29 17:00:41 +0530

    Issue #506 Remove check if bundle is cached for start command
    This is not needed as, if a bundle is not passed via -b flag
    or not configured then start won't even reach this point. if
    a bundle is configured we don't need to check if the embedded
    bundle was cached as validation checks will kick in before running
    preflight checks, so the check is only performed for the setup
    command.

--- pkg/crc/preflight/preflight_linux.go
+++ pkg/crc/preflight/preflight_linux.go
@@ -77,11 +77,6 @@ func StartPreflightChecks(vmDriver string) {
 		"Checking if /etc/NetworkManager/dnsmasq.d/crc.conf exists",
 		config.GetBool(cmdConfig.WarnCheckCrcDnsmasqFile.Name),
 	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckBundleCached.Name),
-		checkBundleCached,
-		"Checking if CRC bundle is cached in '$HOME/.crc'",
-		config.GetBool(cmdConfig.WarnCheckBundleCached.Name),
-	)
 }
 
 // SetupHost performs the prerequisite checks and setups the host to run the cluster
