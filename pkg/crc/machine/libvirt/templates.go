4b709aed9304539a5c1ad0a2b34dd5c29f363b50
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2019-09-12 12:00:44 +0530

    Remove dns entry from crc network template
    Since now dns queries are handled by internal dns server, we don't
    need to have it part of network template.

--- pkg/crc/machine/libvirt/templates.go
+++ pkg/crc/machine/libvirt/templates.go
@@ -11,15 +11,6 @@ const (
 	</forward>
 	<bridge name='crc' stp='on' delay='0'/>
 	<mac address='52:54:00:fd:be:d0'/>
-	<domain name='crc.testing' localOnly='yes'/>
-	<dns>
-	  <srv service='etcd-server-ssl' protocol='tcp' domain='crc.testing' target='etcd-0.crc.testing' port='2380' weight='10'/>
-	  <host ip='{{ .IP }}'>
-		<hostname>api.crc.testing</hostname>
-		<hostname>api-int.crc.testing</hostname>
-		<hostname>etcd-0.crc.testing</hostname>
-	  </host>
-	</dns>
 	<ip family='ipv4' address='192.168.130.1' prefix='24'>
 	  <dhcp>
 		<host mac='{{ .MAC }}' name='{{ .HostName }}' ip='{{ .IP }}'/>
