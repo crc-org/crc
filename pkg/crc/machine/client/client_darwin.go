d51abfff9cc2b0d7ce67d6b82ad515845c00f41b
Gerard Braad <me@gbraad.nl>
2019-09-18 15:23:52 +0000

    Refactor platform specific code

---
+++ pkg/crc/machine/client/client_darwin.go
@@ -0,0 +1,26 @@
+package client
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/code-ready/crc/pkg/crc/errors"
+	"github.com/code-ready/machine/drivers/virtualbox"
+	"github.com/code-ready/machine/libmachine/drivers/plugin"
+	"github.com/code-ready/machine/libmachine/drivers/plugin/localbinary"
+)
+
+// StartDriver starts the desired machine driver if necessary.
+func StartDriver() {
+	if os.Getenv(localbinary.PluginEnvKey) == localbinary.PluginEnvVal {
+		driverName := os.Getenv(localbinary.PluginEnvDriverName)
+		switch driverName {
+		case "virtualbox":
+			plugin.RegisterDriver(virtualbox.NewDriver("", ""))
+		default:
+			errors.ExitWithMessage(1, fmt.Sprintf("Unregistered driver: %s\n", driverName))
+		}
+		return
+	}
+	localbinary.CurrentBinaryIsCRCMachine = true
+}
