270e6b120eda2e33f5886d64c1b987d7d97fb278
Christophe Fergeau <cfergeau@redhat.com>
2019-09-18 17:29:08 +0200

    Issue #586: Use golang.org/x/sys/windows to implement windows.ShellExecute
    No need to reimplement something very similar ourselves.
    
    This has the side-effect of fixing:
    pkg/os/windows/win32/shell32_windows.go:42:9: S1028: should use fmt.Errorf(...) instead of errors.New(fmt.Sprintf(...)) (gosimple)
            return errors.New(fmt.Sprintf("win32 error %v", ret))
    
    https://github.com/code-ready/crc/issues/586

--- pkg/os/windows/win32/shell32_windows.go
+++ pkg/os/windows/win32/shell32_windows.go
@@ -1,45 +1,35 @@
 package win32
 
 import (
-	"errors"
-	"fmt"
 	"github.com/code-ready/crc/pkg/crc/logging"
+	"golang.org/x/sys/windows"
 	"syscall"
-	"unsafe"
 )
 
-var (
-	shell32Lib       = syscall.NewLazyDLL("shell32.dll")
-	procShellExecute = shell32Lib.NewProc("ShellExecuteW")
+const (
+	HWND_DESKTOP = windows.Handle(0)
 )
 
 // Uses "runas" as verb to execute as Elevated privileges
-func ShellExecuteAsAdmin(reason string, hwnd HWND, file, parameters, directory string, showCmd int) error {
+func ShellExecuteAsAdmin(reason string, hwnd windows.Handle, file, parameters, directory string, showCmd int) error {
 	logging.Infof("Will run as admin: %s", reason)
 	return ShellExecute(hwnd, "runas", file, parameters, directory, showCmd)
 }
 
-func toUintptr(input string) uintptr {
-	return uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(input)))
+func toUint16ptr(input string) *uint16 {
+	return syscall.StringToUTF16Ptr(input)
 }
 
-func ShellExecute(hwnd HWND, verb, file, parameters, directory string, showCmd int) error {
-	var op, params, dir uintptr
+func ShellExecute(hwnd windows.Handle, verb, file, parameters, directory string, showCmd int) error {
+	var op, params, dir *uint16
 	if len(verb) != 0 {
-		op = toUintptr(verb)
+		op = toUint16ptr(verb)
 	}
 	if len(parameters) != 0 {
-		params = toUintptr(parameters)
+		params = toUint16ptr(parameters)
 	}
 	if len(directory) != 0 {
-		dir = toUintptr(directory)
+		dir = toUint16ptr(directory)
 	}
-
-	ret, _, _ := procShellExecute.Call(uintptr(hwnd), op, toUintptr(file), params, dir, uintptr(showCmd))
-
-	if ret == 0 || ret < 32 {
-		return nil
-	}
-
-	return errors.New(fmt.Sprintf("win32 error %v", ret))
+	return windows.ShellExecute(hwnd, op, toUint16ptr(file), params, dir, int32(showCmd))
 }
