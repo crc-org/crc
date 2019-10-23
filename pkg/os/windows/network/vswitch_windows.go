b0b1fbf7c933528f3975826255a883a057f02d45
Gerard Braad <me@gbraad.nl>
2019-10-23 05:51:55 +0000

    Issue #758 Force encoding for GetDefaultSwitchName

--- pkg/os/windows/network/vswitch_windows.go
+++ pkg/os/windows/network/vswitch_windows.go
@@ -10,7 +10,7 @@ import (
 const hypervDefaultVirtualSwitchId = "c08cb7b8-9b3c-408e-8e30-5e16a3aeb444"
 
 func GetDefaultSwitchName() (bool, string) {
-	getDefaultSwitchNameCmd := fmt.Sprintf("Get-VMSwitch -Id %s | ForEach-Object { $_.Name }", hypervDefaultVirtualSwitchId)
+	getDefaultSwitchNameCmd := fmt.Sprintf("[Console]::OutputEncoding = [Text.Encoding]::UTF8; Get-VMSwitch -Id %s | ForEach-Object { $_.Name }", hypervDefaultVirtualSwitchId)
 	stdOut, stdErr, _ := powershell.Execute(getDefaultSwitchNameCmd)
 
 	// If stdErr contains the command then execution failed
