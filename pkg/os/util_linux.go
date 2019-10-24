1c6852c328ed7e3d25692e041d5819ffeb69a4cf
Zeeshan Ali <zeeshanak@gnome.org>
2019-10-24 18:17:47 +0200

    Add GetFirstExistentPath() utility function
    Given a set of paths, it retrieves the first existent path.

--- pkg/os/util_linux.go
+++ pkg/os/util_linux.go
@@ -3,6 +3,7 @@ package os
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 
@@ -31,4 +32,24 @@ func writeToFileAsRoot(reason, content, filepath string, append bool) error {
 		return fmt.Errorf("Failed writing to file as root: %s: %s: %v", filepath, buf.String(), err)
 	}
 	return nil
+}
+
+func GetFirstExistentPath(paths []string) (string, error) {
+	readablePath := ""
+	for _, path := range paths {
+		logging.Debug(fmt.Sprintf("Trying %s..", path))
+		_, err := os.Stat(path)
+		if err != nil {
+			logging.Debug(fmt.Sprintf("Failed to open %s: %s", path, err))
+		} else {
+			readablePath = path
+
+			break
+		}
+	}
+	if readablePath == "" {
+		return "", fmt.Errorf("Failed to find a readable file on any of these paths: %s", strings.Join(paths, ", "))
+	}
+
+	return readablePath, nil
 }
