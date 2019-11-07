50a7d7ed35c8db8c87af0c09000a1b24d4db0e9f
Christophe Fergeau <cfergeau@redhat.com>
2019-11-07 11:55:22 +0100

    Issue #872: preflight: Use new preflight API for Windows checks
    This replace the series of
    preflightCheckSucceedsOrFails/preflightCheckAndFix function calls with
    an array of PreflightCheck which is then used by generic functions.
    
    https://github.com/code-ready/crc/issues/872

--- pkg/crc/preflight/preflight_windows.go
+++ pkg/crc/preflight/preflight_windows.go
@@ -2,98 +2,85 @@ package preflight
 
 import (
 	cmdConfig "github.com/code-ready/crc/cmd/crc/cmd/config"
-	"github.com/code-ready/crc/pkg/crc/config"
 )
+
+var genericPreflightChecks = [...]PreflightCheck{
+	{
+		checkDescription: "Caching oc binary",
+		check:            checkOcBinaryCached,
+		fixDescription:   "",
+		fix:              fixOcBinaryCached,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckBundleCached.Name,
+		warnConfigName:   cmdConfig.WarnCheckBundleCached.Name,
+		checkDescription: "Unpacking bundle from the CRC binary",
+		check:            checkBundleCached,
+		fix:              fixBundleCached,
+		flags:            SetupOnly,
+	},
+}
+
+var hypervPreflightChecks = [...]PreflightCheck{
+	{
+		skipConfigName:   cmdConfig.SkipCheckAdministratorUser.Name,
+		warnConfigName:   cmdConfig.WarnCheckAdministratorUser.Name,
+		checkDescription: "Checking if running as normal user",
+		check:            checkIfRunningAsNormalUserInWindows,
+		fix:              fixRunAsNormalUserInWindows,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckWindowsVersionCheck.Name,
+		warnConfigName:   cmdConfig.WarnCheckWindowsVersionCheck.Name,
+		checkDescription: "Check Windows 10 release",
+		check:            checkVersionOfWindowsUpdate,
+		fix:              fixVersionOfWindowsUpdate,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckHyperVInstalled.Name,
+		warnConfigName:   cmdConfig.WarnCheckHyperVInstalled.Name,
+		checkDescription: "Hyper-V installed and operational",
+		check:            checkHyperVInstalled,
+		fix:              fixHyperVInstalled,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckUserInHyperVGroup.Name,
+		warnConfigName:   cmdConfig.WarnCheckUserInHyperVGroup.Name,
+		checkDescription: "Is user a member of the Hyper-V Administrators group",
+		check:            checkIfUserPartOfHyperVAdmins,
+		fix:              fixUserPartOfHyperVAdmins,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckHyperVServiceRunning.Name,
+		warnConfigName:   cmdConfig.WarnCheckHyperVServiceRunning.Name,
+		checkDescription: "Hyper-V service enabled",
+		check:            checkHyperVServiceRunning,
+		fixDescription:   "Hyper-V service enabled",
+		fix:              fixHyperVServiceRunning,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckHyperVSwitch.Name,
+		warnConfigName:   cmdConfig.WarnCheckHyperVSwitch.Name,
+		checkDescription: "Does the Hyper-V virtual switch exist",
+		check:            checkIfHyperVVirtualSwitchExists,
+		fix:              fixHyperVVirtualSwitch,
+	},
+}
+
+func getPreflightChecks() []PreflightCheck {
+	checks := []PreflightCheck{}
+	checks = append(checks, genericPreflightChecks[:]...)
+	checks = append(checks, hypervPreflightChecks[:]...)
+
+	return checks
+}
 
 // StartPreflightChecks performs the preflight checks before starting the cluster
 func StartPreflightChecks() {
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckAdministratorUser.Name),
-		checkIfRunningAsNormalUserInWindows,
-		"Checking if running as normal user",
-		config.GetBool(cmdConfig.WarnCheckAdministratorUser.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckWindowsVersionCheck.Name),
-		checkOcBinaryCached,
-		"Checking if oc binary is cached",
-		config.GetBool(cmdConfig.WarnCheckWindowsVersionCheck.Name),
-	)
-
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckWindowsVersionCheck.Name),
-		checkVersionOfWindowsUpdate,
-		"Check Windows 10 release",
-		config.GetBool(cmdConfig.WarnCheckWindowsVersionCheck.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckHyperVInstalled.Name),
-		checkHyperVInstalled,
-		"Hyper-V installed and operational",
-		config.GetBool(cmdConfig.WarnCheckHyperVInstalled.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckUserInHyperVGroup.Name),
-		checkIfUserPartOfHyperVAdmins,
-		"Is user a member of the Hyper-V Administrators group",
-		config.GetBool(cmdConfig.WarnCheckUserInHyperVGroup.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckHyperVServiceRunning.Name),
-		checkHyperVServiceRunning,
-		"Hyper-V service enabled",
-		config.GetBool(cmdConfig.WarnCheckHyperVServiceRunning.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckHyperVSwitch.Name),
-		checkIfHyperVVirtualSwitchExists,
-		"Does the Hyper-V virtual switch exist",
-		config.GetBool(cmdConfig.WarnCheckHyperVSwitch.Name),
-	)
+	doPreflightChecks(getPreflightChecks())
 }
 
 // SetupHost performs the prerequisite checks and setups the host to run the cluster
 func SetupHost() {
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckAdministratorUser.Name),
-		checkIfRunningAsNormalUserInWindows,
-		fixRunAsNormalUserInWindows,
-		"Checking if running as normal user",
-		config.GetBool(cmdConfig.WarnCheckAdministratorUser.Name),
-	)
-	preflightCheckAndFix(false,
-		checkOcBinaryCached,
-		fixOcBinaryCached,
-		"Caching oc binary",
-		false,
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckBundleCached.Name),
-		checkBundleCached,
-		fixBundleCached,
-		"Unpacking bundle from the CRC binary",
-		config.GetBool(cmdConfig.WarnCheckBundleCached.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckWindowsVersionCheck.Name),
-		checkVersionOfWindowsUpdate,
-		fixVersionOfWindowsUpdate,
-		"Check Windows 10 release",
-		config.GetBool(cmdConfig.WarnCheckWindowsVersionCheck.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckHyperVInstalled.Name),
-		checkHyperVInstalled,
-		fixHyperVInstalled,
-		"Hyper-V installed",
-		config.GetBool(cmdConfig.WarnCheckHyperVInstalled.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckUserInHyperVGroup.Name),
-		// Workaround to an issue the check returns "True"
-		checkIfUserPartOfHyperVAdmins,
-		fixUserPartOfHyperVAdmins,
-		"Is user a member of the Hyper-V Administrators group",
-		config.GetBool(cmdConfig.WarnCheckUserInHyperVGroup.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckHyperVServiceRunning.Name),
-		checkHyperVServiceRunning,
-		fixHyperVServiceRunning,
-		"Hyper-V service enabled",
-		config.GetBool(cmdConfig.WarnCheckHyperVServiceRunning.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckHyperVSwitch.Name),
-		checkIfHyperVVirtualSwitchExists,
-		fixHyperVVirtualSwitch,
-		"Does the Hyper-V virtual switch exist",
-		config.GetBool(cmdConfig.WarnCheckHyperVSwitch.Name),
-	)
+	doFixPreflightChecks(getPreflightChecks())
 }
