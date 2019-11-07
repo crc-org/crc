8e3f2bb46d5091702d970fcb6675c68a76b211e5
Christophe Fergeau <cfergeau@redhat.com>
2019-11-07 11:55:10 +0100

    Issue #872: preflight: Use new preflight API for Linux checks
    This replace the series of
    preflightCheckSucceedsOrFails/preflightCheckAndFix function calls with
    an array of PreflightCheck which is then used by generic functions.
    
    This fixes https://github.com/code-ready/crc/issues/872 but there will
    be additional commits in order to make more use of the new
    PreflightCheck data structure.

--- pkg/crc/preflight/preflight_linux.go
+++ pkg/crc/preflight/preflight_linux.go
@@ -2,201 +2,167 @@ package preflight
 
 import (
 	cmdConfig "github.com/code-ready/crc/cmd/crc/cmd/config"
-	"github.com/code-ready/crc/pkg/crc/config"
 )
+
+var genericPreflightChecks = [...]PreflightCheck{
+	{
+		skipConfigName:   cmdConfig.SkipCheckRootUser.Name,
+		warnConfigName:   cmdConfig.WarnCheckRootUser.Name,
+		checkDescription: "Checking if running as non-root",
+		check:            checkIfRunningAsNormalUser,
+		fix:              fixRunAsNormalUser,
+	},
+	{
+		checkDescription: "Checking if oc binary is cached",
+		check:            checkOcBinaryCached,
+		fixDescription:   "Caching oc binary",
+		fix:              fixOcBinaryCached,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckBundleCached.Name,
+		warnConfigName:   cmdConfig.WarnCheckBundleCached.Name,
+		checkDescription: "Checking if CRC bundle is cached in '$HOME/.crc'",
+		check:            checkBundleCached,
+		fixDescription:   "Unpacking bundle from the CRC binary",
+		fix:              fixBundleCached,
+		flags:            SetupOnly,
+	},
+}
+
+var libvirtPreflightChecks = [...]PreflightCheck{
+	{
+		skipConfigName:   cmdConfig.SkipCheckVirtEnabled.Name,
+		warnConfigName:   cmdConfig.WarnCheckVirtEnabled.Name,
+		checkDescription: "Checking if Virtualization is enabled",
+		check:            checkVirtualizationEnabled,
+		fixDescription:   "Setting up virtualization",
+		fix:              fixVirtualizationEnabled,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckKvmEnabled.Name,
+		warnConfigName:   cmdConfig.WarnCheckKvmEnabled.Name,
+		checkDescription: "Checking if KVM is enabled",
+		check:            checkKvmEnabled,
+		fixDescription:   "Setting up KVM",
+		fix:              fixKvmEnabled,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckLibvirtInstalled.Name,
+		warnConfigName:   cmdConfig.WarnCheckLibvirtInstalled.Name,
+		checkDescription: "Checking if libvirt is installed",
+		check:            checkLibvirtInstalled,
+		fixDescription:   "Installing libvirt service and dependencies",
+		fix:              fixLibvirtInstalled,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckUserInLibvirtGroup.Name,
+		warnConfigName:   cmdConfig.WarnCheckUserInLibvirtGroup.Name,
+		checkDescription: "Checking if user is part of libvirt group",
+		check:            checkUserPartOfLibvirtGroup,
+		fixDescription:   "Adding user to libvirt group",
+		fix:              fixUserPartOfLibvirtGroup,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckLibvirtEnabled.Name,
+		warnConfigName:   cmdConfig.WarnCheckLibvirtEnabled.Name,
+		checkDescription: "Checking if libvirt is enabled",
+		check:            checkLibvirtEnabled,
+		fixDescription:   "Enabling libvirt",
+		fix:              fixLibvirtEnabled,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckLibvirtRunning.Name,
+		warnConfigName:   cmdConfig.WarnCheckLibvirtRunning.Name,
+		checkDescription: "Checking if libvirt daemon is running",
+		check:            checkLibvirtServiceRunning,
+		fixDescription:   "Starting libvirt service",
+		fix:              fixLibvirtServiceRunning,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckLibvirtVersionCheck.Name,
+		warnConfigName:   cmdConfig.WarnCheckLibvirtVersionCheck.Name,
+		checkDescription: "Checking if a supported libvirt version is installed",
+		check:            checkLibvirtVersion,
+		fix:              fixLibvirtVersion,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckLibvirtDriver.Name,
+		warnConfigName:   cmdConfig.WarnCheckLibvirtDriver.Name,
+		checkDescription: "Checking if crc-driver-libvirt is installed",
+		check:            checkMachineDriverLibvirtInstalled,
+		fixDescription:   "Installing crc-driver-libvirt",
+		fix:              fixMachineDriverLibvirtInstalled,
+	},
+	{
+		check:          checkOldMachineDriverLibvirtInstalled,
+		fixDescription: "Removing older system-wide crc-driver-libvirt",
+		fix:            fixOldMachineDriverLibvirtInstalled,
+		flags:          SetupOnly,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckCrcNetwork.Name,
+		warnConfigName:   cmdConfig.WarnCheckCrcNetwork.Name,
+		checkDescription: "Checking if libvirt 'crc' network is available",
+		check:            checkLibvirtCrcNetworkAvailable,
+		fixDescription:   "Setting up libvirt 'crc' network",
+		fix:              fixLibvirtCrcNetworkAvailable,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckCrcNetworkActive.Name,
+		warnConfigName:   cmdConfig.WarnCheckCrcNetworkActive.Name,
+		checkDescription: "Checking if libvirt 'crc' network is active",
+		check:            checkLibvirtCrcNetworkActive,
+		fixDescription:   "Starting libvirt 'crc' network",
+		fix:              fixLibvirtCrcNetworkActive,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckNetworkManagerInstalled.Name,
+		warnConfigName:   cmdConfig.WarnCheckNetworkManagerInstalled.Name,
+		checkDescription: "Checking if NetworkManager is installed",
+		check:            checkNetworkManagerInstalled,
+		fixDescription:   "Checking if NetworkManager is installed",
+		fix:              fixNetworkManagerInstalled,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckNetworkManagerRunning.Name,
+		warnConfigName:   cmdConfig.WarnCheckNetworkManagerRunning.Name,
+		checkDescription: "Checking if NetworkManager service is running",
+		check:            CheckNetworkManagerIsRunning,
+		fixDescription:   "Checking if NetworkManager service is running",
+		fix:              fixNetworkManagerIsRunning,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckCrcNetworkManagerConfig.Name,
+		warnConfigName:   cmdConfig.WarnCheckCrcNetworkManagerConfig.Name,
+		checkDescription: "Checking if /etc/NetworkManager/conf.d/crc-nm-dnsmasq.conf exists",
+		check:            checkCrcNetworkManagerConfig,
+		fixDescription:   "Writing Network Manager config for crc",
+		fix:              fixCrcNetworkManagerConfig,
+	},
+	{
+		skipConfigName:   cmdConfig.SkipCheckCrcDnsmasqFile.Name,
+		warnConfigName:   cmdConfig.WarnCheckCrcDnsmasqFile.Name,
+		checkDescription: "Checking if /etc/NetworkManager/dnsmasq.d/crc.conf exists",
+		check:            checkCrcDnsmasqConfigFile,
+		fixDescription:   "Writing dnsmasq config for crc",
+		fix:              fixCrcDnsmasqConfigFile,
+	},
+}
+
+func getPreflightChecks() []PreflightCheck {
+	checks := []PreflightCheck{}
+	checks = append(checks, genericPreflightChecks[:]...)
+	checks = append(checks, libvirtPreflightChecks[:]...)
+
+	return checks
+}
 
 // StartPreflightChecks performs the preflight checks before starting the cluster
 func StartPreflightChecks() {
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckRootUser.Name),
-		checkIfRunningAsNormalUser,
-		"Checking if running as non-root",
-		config.GetBool(cmdConfig.WarnCheckRootUser.Name),
-	)
-	preflightCheckSucceedsOrFails(false,
-		checkOcBinaryCached,
-		"Checking if oc binary is cached",
-		false,
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckVirtEnabled.Name),
-		checkVirtualizationEnabled,
-		"Checking if Virtualization is enabled",
-		config.GetBool(cmdConfig.WarnCheckVirtEnabled.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckKvmEnabled.Name),
-		checkKvmEnabled,
-		"Checking if KVM is enabled",
-		config.GetBool(cmdConfig.WarnCheckKvmEnabled.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckLibvirtInstalled.Name),
-		checkLibvirtInstalled,
-		"Checking if libvirt is installed",
-		config.GetBool(cmdConfig.WarnCheckLibvirtInstalled.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckUserInLibvirtGroup.Name),
-		checkUserPartOfLibvirtGroup,
-		"Checking if user is part of libvirt group",
-		config.GetBool(cmdConfig.WarnCheckUserInLibvirtGroup.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckLibvirtEnabled.Name),
-		checkLibvirtEnabled,
-		"Checking if libvirt is enabled",
-		config.GetBool(cmdConfig.WarnCheckLibvirtEnabled.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckLibvirtRunning.Name),
-		checkLibvirtServiceRunning,
-		"Checking if libvirt daemon is running",
-		config.GetBool(cmdConfig.WarnCheckLibvirtRunning.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckLibvirtVersionCheck.Name),
-		checkLibvirtVersion,
-		"Checking if a supported libvirt version is installed",
-		config.GetBool(cmdConfig.WarnCheckLibvirtVersionCheck.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckLibvirtDriver.Name),
-		checkMachineDriverLibvirtInstalled,
-		"Checking if crc-driver-libvirt is installed",
-		config.GetBool(cmdConfig.WarnCheckLibvirtDriver.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckCrcNetwork.Name),
-		checkLibvirtCrcNetworkAvailable,
-		"Checking if libvirt 'crc' network is available",
-		config.GetBool(cmdConfig.WarnCheckCrcNetwork.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckCrcNetworkActive.Name),
-		checkLibvirtCrcNetworkActive,
-		"Checking if libvirt 'crc' network is active",
-		config.GetBool(cmdConfig.WarnCheckCrcNetworkActive.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckNetworkManagerInstalled.Name),
-		checkNetworkManagerInstalled,
-		"Checking if NetworkManager is installed",
-		config.GetBool(cmdConfig.WarnCheckNetworkManagerInstalled.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckNetworkManagerRunning.Name),
-		CheckNetworkManagerIsRunning,
-		"Checking if NetworkManager service is running",
-		config.GetBool(cmdConfig.WarnCheckNetworkManagerRunning.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckCrcNetworkManagerConfig.Name),
-		checkCrcNetworkManagerConfig,
-		"Checking if /etc/NetworkManager/conf.d/crc-nm-dnsmasq.conf exists",
-		config.GetBool(cmdConfig.WarnCheckCrcNetworkManagerConfig.Name),
-	)
-	preflightCheckSucceedsOrFails(config.GetBool(cmdConfig.SkipCheckCrcDnsmasqFile.Name),
-		checkCrcDnsmasqConfigFile,
-		"Checking if /etc/NetworkManager/dnsmasq.d/crc.conf exists",
-		config.GetBool(cmdConfig.WarnCheckCrcDnsmasqFile.Name),
-	)
+	doPreflightChecks(getPreflightChecks())
 }
 
 // SetupHost performs the prerequisite checks and setups the host to run the cluster
 func SetupHost() {
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckRootUser.Name),
-		checkIfRunningAsNormalUser,
-		fixRunAsNormalUser,
-		"Checking if running as non-root",
-		config.GetBool(cmdConfig.WarnCheckRootUser.Name),
-	)
-	preflightCheckAndFix(false,
-		checkOcBinaryCached,
-		fixOcBinaryCached,
-		"Caching oc binary",
-		false,
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckVirtEnabled.Name),
-		checkVirtualizationEnabled,
-		fixVirtualizationEnabled,
-		"Setting up virtualization",
-		config.GetBool(cmdConfig.WarnCheckVirtEnabled.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckKvmEnabled.Name),
-		checkKvmEnabled,
-		fixKvmEnabled,
-		"Setting up KVM",
-		config.GetBool(cmdConfig.WarnCheckKvmEnabled.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckLibvirtInstalled.Name),
-		checkLibvirtInstalled,
-		fixLibvirtInstalled,
-		"Installing libvirt service and dependencies",
-		config.GetBool(cmdConfig.WarnCheckLibvirtInstalled.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckUserInLibvirtGroup.Name),
-		checkUserPartOfLibvirtGroup,
-		fixUserPartOfLibvirtGroup,
-		"Adding user to libvirt group",
-		config.GetBool(cmdConfig.WarnCheckUserInLibvirtGroup.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckLibvirtEnabled.Name),
-		checkLibvirtEnabled,
-		fixLibvirtEnabled,
-		"Enabling libvirt",
-		config.GetBool(cmdConfig.WarnCheckLibvirtEnabled.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckLibvirtRunning.Name),
-		checkLibvirtServiceRunning,
-		fixLibvirtServiceRunning,
-		"Starting libvirt service",
-		config.GetBool(cmdConfig.WarnCheckLibvirtRunning.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckLibvirtVersionCheck.Name),
-		checkLibvirtVersion,
-		fixLibvirtVersion,
-		"Checking if a supported libvirt version is installed",
-		config.GetBool(cmdConfig.WarnCheckLibvirtVersionCheck.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckLibvirtDriver.Name),
-		checkMachineDriverLibvirtInstalled,
-		fixMachineDriverLibvirtInstalled,
-		"Installing crc-driver-libvirt",
-		config.GetBool(cmdConfig.WarnCheckLibvirtDriver.Name),
-	)
-	preflightCheckAndFix(false,
-		checkOldMachineDriverLibvirtInstalled,
-		fixOldMachineDriverLibvirtInstalled,
-		"Removing older system-wide crc-driver-libvirt",
-		false,
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckCrcNetwork.Name),
-		checkLibvirtCrcNetworkAvailable,
-		fixLibvirtCrcNetworkAvailable,
-		"Setting up libvirt 'crc' network",
-		config.GetBool(cmdConfig.WarnCheckCrcNetwork.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckCrcNetworkActive.Name),
-		checkLibvirtCrcNetworkActive,
-		fixLibvirtCrcNetworkActive,
-		"Starting libvirt 'crc' network",
-		config.GetBool(cmdConfig.WarnCheckCrcNetworkActive.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckNetworkManagerInstalled.Name),
-		checkNetworkManagerInstalled,
-		fixNetworkManagerInstalled,
-		"Checking if NetworkManager is installed",
-		config.GetBool(cmdConfig.WarnCheckNetworkManagerInstalled.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckNetworkManagerRunning.Name),
-		CheckNetworkManagerIsRunning,
-		fixNetworkManagerIsRunning,
-		"Checking if NetworkManager service is running",
-		config.GetBool(cmdConfig.WarnCheckNetworkManagerRunning.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckCrcNetworkManagerConfig.Name),
-		checkCrcNetworkManagerConfig,
-		fixCrcNetworkManagerConfig,
-		"Writing Network Manager config for crc",
-		config.GetBool(cmdConfig.WarnCheckCrcNetworkManagerConfig.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckCrcDnsmasqFile.Name),
-		checkCrcDnsmasqConfigFile,
-		fixCrcDnsmasqConfigFile,
-		"Writing dnsmasq config for crc",
-		config.GetBool(cmdConfig.WarnCheckCrcDnsmasqFile.Name),
-	)
-	preflightCheckAndFix(config.GetBool(cmdConfig.SkipCheckBundleCached.Name),
-		checkBundleCached,
-		fixBundleCached,
-		"Unpacking bundle from the CRC binary",
-		config.GetBool(cmdConfig.WarnCheckBundleCached.Name),
-	)
+	doFixPreflightChecks(getPreflightChecks())
 }
