df7c9cf2ed10470c6428a20aaaf78ecbae25d8e6
Christophe Fergeau <cfergeau@redhat.com>
2019-11-27 12:47:54 +0100

    Issue #848: Remove '7 days' check/warnings
    With OpenShift 4.2.2, when the cluster is started close to the cert
    expiry date (I tried a few days before), the certificates will be
    automatically renewed a few minutes after the cluster starts, so we no
    longer need to warn the user about it.
    
    This fixes https://github.com/code-ready/crc/issues/848

--- pkg/crc/machine/machine.go
+++ pkg/crc/machine/machine.go
@@ -131,16 +131,6 @@ func Start(startConfig StartConfig) (StartResult, error) {
 			return *result, errors.Newf("Error getting bundle metadata: %v", err)
 		}
 
-		// Check if certificate is going to expire in next 7 days
-		buildTime, err := crcBundleMetadata.GetBundleBuildTime()
-		if err != nil {
-			result.Error = err.Error()
-			return *result, errors.Newf("Error getting bundle build time: %v", err)
-		}
-		if goingToExpire, duration := cluster.CheckCertsValidityUsingBundleBuildTime(buildTime); goingToExpire {
-			logging.Warnf("Bundle certificates are going to expire in %d days, better to use new release", duration)
-		}
-
 		openshiftVersion := crcBundleMetadata.GetOpenshiftVersion()
 		if openshiftVersion == "" {
 			logging.Infof("Creating VM...")
@@ -257,13 +247,13 @@ func Start(startConfig StartConfig) (StartResult, error) {
 	// Check the certs validity inside the vm
 	needsCertsRenewal := false
 	logging.Info("Verifying validity of the cluster certificates ...")
-	expiringIn7Days, duration, err := cluster.CheckCertsValidity(sshRunner)
+	certExpiryState, err := cluster.CheckCertsValidity(sshRunner)
 	if err != nil {
-		needsCertsRenewal = true
-	} else if exists {
-		// Only show when VM is started from stopped state.
-		if expiringIn7Days {
-			logging.Warnf("Bundle certificates are going to expire in %d days, better to use new release", duration)
+		if certExpiryState == cluster.CertExpired {
+			needsCertsRenewal = true
+		} else {
+			result.Error = err.Error()
+			return *result, errors.New(err.Error())
 		}
 	}
 	// Add nameserver to VM if provided by User
