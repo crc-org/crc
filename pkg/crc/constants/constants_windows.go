d2aded2aacfd61972a3c9ab8326d3e8d7237cfac
Christophe Fergeau <cfergeau@redhat.com>
2019-12-06 11:08:53 +0100

    Issue #509: Rework GetDefaultBundle
    We'll need to be able to get the bundle name for the various supported
    platforms at build time in order to embed it in the binary.
    
    https://github.com/code-ready/crc/issues/509

--- pkg/crc/constants/constants_windows.go
+++ pkg/crc/constants/constants_windows.go
@@ -1,15 +1,5 @@
 package constants
 
-import (
-	"fmt"
-	"github.com/code-ready/crc/pkg/crc/version"
-)
-
 const (
 	OcBinaryName = "oc.exe"
 )
-
-func GetDefaultBundle() string {
-	// TODO: we will change once we have correct bundle for windows
-	return fmt.Sprintf("crc_hyperv_%s.crcbundle", version.GetBundleVersion())
-}
