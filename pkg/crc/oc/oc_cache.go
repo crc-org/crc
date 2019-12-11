03ecaf8e64734aac4777c13ca6a49c6721012777
Christophe Fergeau <cfergeau@redhat.com>
2019-12-11 21:09:45 +0100

    Issue #509: preflight: Use embed package to get oc
    crc binaries generated with make embed_bundle should now embed the
    'oc' tarball. This commit changes the preflight checks to extract 'oc'
    from the binary with a fallback to downloading it if it's not embedded.
    
    https://github.com/code-ready/crc/issues/509

--- pkg/crc/oc/oc_cache.go
+++ pkg/crc/oc/oc_cache.go
@@ -8,6 +8,7 @@ import (
 	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/logging"
 	"github.com/code-ready/crc/pkg/download"
+	"github.com/code-ready/crc/pkg/embed"
 	"github.com/code-ready/crc/pkg/extract"
 	crcos "github.com/code-ready/crc/pkg/os"
 	"github.com/pkg/errors"
@@ -44,17 +45,29 @@ func matchOcBinaryName(filename string) bool {
 	return filepath.Base(filename) == constants.OcBinaryName
 }
 
+func (oc *OcCached) getOc(destDir string) (string, error) {
+	logging.Debug("Trying to extract oc from crc binary")
+	archiveName := filepath.Base(constants.GetOcUrl())
+	destPath := filepath.Join(destDir, archiveName)
+	err := embed.Extract(archiveName, destPath)
+	if err != nil {
+		logging.Debug("Downloading oc")
+		return download.Download(constants.GetOcUrl(), destDir, 0600)
+	}
+
+	return destPath, err
+}
+
 // cacheOc downloads and caches the oc binary into the minishift directory
 func (oc *OcCached) cacheOc() error {
 	if !oc.IsCached() {
-		logging.Debug("Downloading oc")
 		// Create tmp dir to download the oc tarball
 		tmpDir, err := ioutil.TempDir("", "crc")
 		if err != nil {
 			return err
 		}
 		defer os.RemoveAll(tmpDir)
-		assetTmpFile, err := download.Download(constants.GetOcUrl(), tmpDir, 0600)
+		assetTmpFile, err := oc.getOc(tmpDir)
 		if err != nil {
 			return err
 		}
