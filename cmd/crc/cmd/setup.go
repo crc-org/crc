cae2ed9bfbe931dcb0fbb1045e573c392e0efb39
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2020-01-28 12:29:45 +0530

    Issue #966 Remove driver option from setup
    CRC only support the native hypervisor for each platform and doesn't
    provide any different driver to connect a non-native hypervisor. This
    PR will remove unused `driver` option from setup.
    
    Fix #966

--- cmd/crc/cmd/setup.go
+++ cmd/crc/cmd/setup.go
@@ -1,22 +1,16 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
 
 	"github.com/code-ready/crc/cmd/crc/cmd/config"
 	crcConfig "github.com/code-ready/crc/pkg/crc/config"
 	"github.com/code-ready/crc/pkg/crc/constants"
-	"github.com/code-ready/crc/pkg/crc/errors"
-	"github.com/code-ready/crc/pkg/crc/machine"
 	"github.com/code-ready/crc/pkg/crc/output"
 	"github.com/code-ready/crc/pkg/crc/preflight"
-	"github.com/code-ready/crc/pkg/crc/validation"
 )
 
 func init() {
-	setupCmd.Flags().StringVarP(&vmDriver, config.VMDriver.Name, "d",
-		machine.DefaultDriver.Driver, fmt.Sprintf("The driver to use for the OpenShift cluster. Possible values: %v", machine.SupportedDriverValues()))
 	setupCmd.Flags().Bool(config.ExperimentalFeatures.Name, false, "Allow the use of experimental features")
 	_ = crcConfig.BindFlagSet(setupCmd.Flags())
 	rootCmd.AddCommand(setupCmd)
@@ -32,10 +26,6 @@ var setupCmd = &cobra.Command{
 }
 
 func runSetup(arguments []string) {
-	if err := validateSetupFlags(); err != nil {
-		errors.Exit(1)
-	}
-
 	if crcConfig.GetBool(config.ExperimentalFeatures.Name) {
 		preflight.EnableExperimentalFeatures = true
 	}
@@ -45,13 +35,4 @@ func runSetup(arguments []string) {
 		bundle = " -b $bundlename"
 	}
 	output.Outf("Setup is complete, you can now run 'crc start%s' to start the OpenShift cluster\n", bundle)
-}
-
-var vmDriver string
-
-func validateSetupFlags() error {
-	if err := validation.ValidateDriver(vmDriver); err != nil {
-		return err
-	}
-	return nil
 }
