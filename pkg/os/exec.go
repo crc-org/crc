22fba28478778b3da969228869b868dcac38e9b0
Christophe Fergeau <cfergeau@redhat.com>
2020-02-04 17:09:54 +0100

    Issue #990: os: Factor common code in os.Run*
    RunWithPrivilege and RunWithDefaultLocale are running the command in
    almost the same way, so we can move this code to a generic function
    which can manage their specifities (one needs to prepend 'sudo' to the
    command, the other one needs to make some changes to the environment)
    
    This is related to https://github.com/code-ready/crc/issues/990

--- pkg/os/exec.go
+++ pkg/os/exec.go
@@ -8,28 +8,14 @@ import (
 	"strings"
 )
 
-// RunWithPrivilege executes a command using sudo
-// provide a reason why root is needed as the first argument
-func RunWithPrivilege(reason string, cmdAndArgs ...string) (string, string, error) {
-	sudo, err := exec.LookPath("sudo")
-	if err != nil {
-		return "", "", err
-	}
-	cmd := exec.Command(sudo, cmdAndArgs...) // #nosec G204
-	stdOut := new(bytes.Buffer)
-	stdErr := new(bytes.Buffer)
-	cmd.Stdout = stdOut
-	cmd.Stderr = stdErr
-	logging.Infof("Will use root access: %s", reason)
-	logging.Debugf("Running %s with sudo", strings.Join(cmdAndArgs, " "))
-	err = cmd.Run()
-	return stdOut.String(), stdErr.String(), err
-}
-
-func RunWithDefaultLocale(command string, args ...string) (string, string, error) {
+func run(command string, args []string, env map[string]string) (string, string, error) {
 	cmd := exec.Command(command, args...) // #nosec G204
-	cmd.Env = ReplaceEnv(os.Environ(), "LC_ALL", "C")
-	cmd.Env = ReplaceEnv(cmd.Env, "LANG", "C")
+	if len(env) != 0 {
+		cmd.Env = os.Environ()
+		for key, value := range env {
+			cmd.Env = ReplaceEnv(cmd.Env, key, value)
+		}
+	}
 	stdOut := new(bytes.Buffer)
 	stdErr := new(bytes.Buffer)
 	cmd.Stdout = stdOut
@@ -37,4 +23,19 @@ func RunWithDefaultLocale(command string, args ...string) (string, string, error
 	logging.Debugf("Running %s %s", command, strings.Join(args, " "))
 	err := cmd.Run()
 	return stdOut.String(), stdErr.String(), err
+}
+
+// RunWithPrivilege executes a command using sudo
+// provide a reason why root is needed as the first argument
+func RunWithPrivilege(reason string, cmdAndArgs ...string) (string, string, error) {
+	sudo, err := exec.LookPath("sudo")
+	if err != nil {
+		return "", "", err
+	}
+	logging.Infof("Will use root access: %s", reason)
+	return run(sudo, cmdAndArgs, map[string]string{})
+}
+
+func RunWithDefaultLocale(command string, args ...string) (string, string, error) {
+	return run(command, args, map[string]string{"LC_ALL": "C", "LANG": "C"})
 }
