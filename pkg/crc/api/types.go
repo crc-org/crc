669441388023fefeb120ec84ae538ae195446872
Anjan Nath <kaludios@gmail.com>
2020-02-13 16:38:05 +0530

    Move all type definitions to a new file types.go

---
+++ pkg/crc/api/types.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"net"
+)
+
+type ArgsType map[string]string
+type handlerFunc func(ArgsType) string
+
+type CrcApiServer struct {
+	listener               net.Listener
+	clusterOpsRequestsChan chan clusterOpsRequest
+	handlers               map[string]handlerFunc // relates commands to handler func
+}
+
+// clusterOpsRequest struct is used to store the command request and associated socket
+type clusterOpsRequest struct {
+	command commandRequest
+	socket  net.Conn
+}
+
+// commandRequest struct is used to decode the json request from tray
+type commandRequest struct {
+	Command string            `json:"command"`
+	Args    map[string]string `json:"args,omitempty"`
+}
