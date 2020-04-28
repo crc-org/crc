ed8cd4f8a195ad5c677713d34424b7f71fcc4f76
Anjan Nath <kaludios@gmail.com>
2020-04-28 12:25:07 +0530

    Issue #795 Create a window service for the daemon
    In windows to run a process in the background as a service
    it has to follow certain rules, so that it can be managed by
    windows service manager, this patch uses the x/sys/windows
    package to create a service for the daemon.
    
    Its based on the example found in the package's source at
    https://github.com/golang/sys/tree/master/windows/svc/example

--- cmd/crc/cmd/daemon.go
+++ cmd/crc/cmd/daemon.go
@@ -1,11 +1,6 @@
 package cmd
 
 import (
-	"os"
-
-	"github.com/code-ready/crc/pkg/crc/api"
-	"github.com/code-ready/crc/pkg/crc/constants"
-	"github.com/code-ready/crc/pkg/crc/logging"
 	"github.com/spf13/cobra"
 )
 
@@ -21,19 +16,4 @@ var daemonCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		runDaemon()
 	},
-}
-
-func runDaemon() {
-	//setup separate logfile for daemon
-	logging.CloseLogging()
-	logging.InitLogrus(logging.LogLevel)
-	logging.SetupFileHook(constants.DaemonLogFilePath)
-
-	// Remove if an old socket is present
-	os.Remove(constants.DaemonSocketPath)
-	crcApiServer, err := api.CreateApiServer(constants.DaemonSocketPath)
-	if err != nil {
-		logging.Fatal("Failed to launch daemon", err)
-	}
-	crcApiServer.Serve()
 }
