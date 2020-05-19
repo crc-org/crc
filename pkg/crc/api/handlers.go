06e8fc2f7386bdfa2ecea3b0f9960dbda1cbbf73
Anjan Nath <kaludios@gmail.com>
2020-05-19 22:50:59 +0530

    Issue #723 Extend daemon api to support configuring properties
    Three new commands are introduced, setconfig, getconfig and unsetconfig
    
    - setconfig example
      {"command":"setconfig", "args":{"properties":{"bundle":"path/to/bundle", "cpu":"4"}}}
    - getconfig example
      {"command":"getconfig"} witouth the args object it will return all
      {"command":"getconfig", "args":{"properties":["bundle", "memory"]}}
    - unsetconfig example
      {"command":"unsetconfig", "args":{"properties":["bundle"]}}
    
    All of them returns a json object that contains an Error string which is
    empty if no error occured
    
    getconfig returns the configs in a "Configs" object setconfig and unsetconfig
    returns the properties that were successfully modified in the properties array
    
    - setconfig sample output
      {"Error":"","Properties":["bundle", "memory"]}
    - getconfig sample output
      {"Error":"","Configs":{"pull-secret-file":"/secret/pull-secret"}}
    - unsetconfig sample output
      {"Error":"","Properties":["bundle", "pull-secret-file"]}

--- pkg/crc/api/handlers.go
+++ pkg/crc/api/handlers.go
@@ -2,11 +2,13 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 
 	"github.com/code-ready/crc/cmd/crc/cmd/config"
 	crcConfig "github.com/code-ready/crc/pkg/crc/config"
 	"github.com/code-ready/crc/pkg/crc/constants"
+	"github.com/code-ready/crc/pkg/crc/errors"
 	"github.com/code-ready/crc/pkg/crc/logging"
 	"github.com/code-ready/crc/pkg/crc/machine"
 	"github.com/code-ready/crc/pkg/crc/validation"
@@ -74,6 +76,127 @@ func webconsoleURLHandler(client machine.Client, _ json.RawMessage) string {
 	consoleConfig := machine.ConsoleConfig{Name: constants.DefaultName}
 	r, _ := client.GetConsoleURL(consoleConfig)
 	return encodeStructToJSON(r)
+}
+
+func setConfigHandler(_ machine.Client, args json.RawMessage) string {
+	setConfigResult := setOrUnsetConfigResult{}
+	if args == nil {
+		setConfigResult.Error = "No config keys provided"
+		return encodeStructToJSON(setConfigResult)
+	}
+
+	var multiError = errors.MultiError{}
+	var a = make(map[string]interface{})
+
+	err := json.Unmarshal(args, &a)
+	if err != nil {
+		setConfigResult.Error = fmt.Sprintf("%v", err)
+		return encodeStructToJSON(setConfigResult)
+	}
+
+	configs := a["properties"].(map[string]interface{})
+
+	// successProps slice contains the properties that were successfully set
+	var successProps []string
+
+	for k, v := range configs {
+		_, err := crcConfig.Set(k, v)
+		if err != nil {
+			multiError.Collect(err)
+			continue
+		}
+		err = crcConfig.WriteConfig()
+		if err != nil {
+			multiError.Collect(err)
+			continue
+		}
+		successProps = append(successProps, k)
+	}
+
+	if len(multiError.Errors) != 0 {
+		setConfigResult.Error = fmt.Sprintf("%v", multiError.ToError())
+	}
+
+	setConfigResult.Properties = successProps
+	return encodeStructToJSON(setConfigResult)
+}
+
+func unsetConfigHandler(_ machine.Client, args json.RawMessage) string {
+	unsetConfigResult := setOrUnsetConfigResult{}
+	if args == nil {
+		unsetConfigResult.Error = "No config keys provided"
+		return encodeStructToJSON(unsetConfigResult)
+	}
+
+	var multiError = errors.MultiError{}
+	var keys = make(map[string][]string)
+
+	err := json.Unmarshal(args, &keys)
+	if err != nil {
+		unsetConfigResult.Error = fmt.Sprintf("%v", err)
+		return encodeStructToJSON(unsetConfigResult)
+	}
+
+	// successProps slice contains the properties that were successfully unset
+	var successProps []string
+
+	keysToUnset := keys["properties"]
+	for _, key := range keysToUnset {
+		_, err := crcConfig.Unset(key)
+		if err != nil {
+			multiError.Collect(err)
+			continue
+		}
+		err = crcConfig.WriteConfig()
+		if err != nil {
+			multiError.Collect(err)
+			continue
+		}
+		successProps = append(successProps, key)
+	}
+	if len(multiError.Errors) != 0 {
+		unsetConfigResult.Error = fmt.Sprintf("%v", multiError.ToError())
+	}
+	unsetConfigResult.Properties = successProps
+	return encodeStructToJSON(unsetConfigResult)
+}
+
+func getConfigHandler(_ machine.Client, args json.RawMessage) string {
+	configResult := getConfigResult{}
+	if args == nil {
+		allConfigs := crcConfig.AllConfigs()
+		configResult.Error = ""
+		configResult.Configs = allConfigs
+		return encodeStructToJSON(configResult)
+	}
+
+	var a = make(map[string][]string)
+
+	err := json.Unmarshal(args, &a)
+	if err != nil {
+		configResult.Error = fmt.Sprintf("%v", err)
+		return encodeStructToJSON(configResult)
+	}
+
+	keys := a["properties"]
+
+	var configs = make(map[string]interface{})
+
+	for _, key := range keys {
+		v, err := crcConfig.Get(key)
+		if err != nil {
+			continue
+		}
+		configs[key] = v
+	}
+	if len(configs) == 0 {
+		configResult.Error = "Unable to get configs"
+		configResult.Configs = nil
+	} else {
+		configResult.Error = ""
+		configResult.Configs = configs
+	}
+	return encodeStructToJSON(configResult)
 }
 
 func encodeStructToJSON(v interface{}) string {
