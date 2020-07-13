3bf6231701a0abd3ac95e64f36b169ed9a009f77
Guillaume Rose <gurose@redhat.com>
2020-07-13 10:33:41 +0200

    Add tests for errors.RetryAfter

---
+++ pkg/crc/errors/multierror_test.go
@@ -0,0 +1,51 @@
+package errors
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRetryAfter(t *testing.T) {
+	calls := 0
+	ret := RetryAfter(10, func() error {
+		calls++
+		return nil
+	}, 0)
+	assert.NoError(t, ret)
+	assert.Equal(t, 1, calls)
+}
+
+func TestRetryAfterFailure(t *testing.T) {
+	calls := 0
+	ret := RetryAfter(10, func() error {
+		calls++
+		return errors.New("failed")
+	}, 0)
+	assert.EqualError(t, ret, "failed")
+	assert.Equal(t, 1, calls)
+}
+
+func TestRetryAfterMaxAttempts(t *testing.T) {
+	calls := 0
+	ret := RetryAfter(3, func() error {
+		calls++
+		return &RetriableError{Err: errors.New("failed")}
+	}, 0)
+	assert.EqualError(t, ret, "Temporary Error: failed\nTemporary Error: failed\nTemporary Error: failed")
+	assert.Equal(t, 3, calls)
+}
+
+func TestRetryAfterSuccessAfterFailures(t *testing.T) {
+	calls := 0
+	ret := RetryAfter(5, func() error {
+		calls++
+		if calls < 3 {
+			return &RetriableError{Err: errors.New("failed")}
+		}
+		return nil
+	}, 0)
+	assert.NoError(t, ret)
+	assert.Equal(t, 3, calls)
+}
