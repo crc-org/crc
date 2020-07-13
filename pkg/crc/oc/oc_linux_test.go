a2bab9568bc57942642881d080d3e314fc3049f0
Guillaume Rose <gurose@redhat.com>
2020-07-13 11:45:06 +0200

    KubeconfigPath usage belongs to the runner

--- pkg/crc/oc/oc_linux_test.go
+++ pkg/crc/oc/oc_linux_test.go
@@ -12,5 +12,5 @@ func TestRunCommand(t *testing.T) {
 		KubeconfigPath: "kubeconfig-file",
 	}, "a-context", "a-cluster")
 	stdout, _, _ := ocConfig.RunOcCommand("a-command")
-	assert.Equal(t, "a-command --kubeconfig kubeconfig-file --context a-context --cluster a-cluster\n", stdout)
+	assert.Equal(t, "a-command --context a-context --cluster a-cluster --kubeconfig kubeconfig-file\n", stdout)
 }
