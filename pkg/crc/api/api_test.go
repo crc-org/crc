ebfe7dea600896cf63c5edbd0c930bc4f1e45b68
Guillaume Rose <gurose@redhat.com>
2020-07-20 14:37:20 +0200

    Add unit test for api

---
+++ pkg/crc/api/api_test.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/code-ready/crc/pkg/crc/machine"
+	"github.com/code-ready/crc/pkg/crc/version"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestApi(t *testing.T) {
+	dir, err := ioutil.TempDir("", "api")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	socket := filepath.Join(dir, "api.sock")
+	listener, err := net.Listen("unix", socket)
+	require.NoError(t, err)
+	defer listener.Close()
+
+	api, err := createApiServerWithListener(listener)
+	require.NoError(t, err)
+	go api.Serve()
+
+	client, err := net.Dial("unix", socket)
+	require.NoError(t, err)
+
+	jsonReq, err := json.Marshal(commandRequest{
+		Command: "version",
+		Args:    map[string]string{},
+	})
+	assert.NoError(t, err)
+	_, err = client.Write(jsonReq)
+	assert.NoError(t, err)
+
+	payload := make([]byte, 1024)
+	n, err := client.Read(payload)
+	assert.NoError(t, err)
+
+	var versionRes machine.VersionResult
+	assert.NoError(t, json.Unmarshal(payload[0:n], &versionRes))
+	assert.Equal(t, machine.VersionResult{
+		CrcVersion:       version.GetCRCVersion(),
+		CommitSha:        version.GetCommitSha(),
+		OpenshiftVersion: version.GetBundleVersion(),
+		Success:          true,
+	}, versionRes)
+}
