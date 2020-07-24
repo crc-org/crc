892856cd58e57eb3774b3e6f11d8fb1aaaeefc24
Guillaume Rose <gurose@redhat.com>
2020-07-24 15:59:13 +0200

    Ensure all errors have the same format

--- pkg/crc/machine/errors.go
+++ pkg/crc/machine/errors.go
@@ -9,3 +9,56 @@ func startError(name string, description string, err error) (StartResult, error)
 		Error: fullErr.Error(),
 	}, fullErr
 }
+
+func stopError(name string, description string, err error) (StopResult, error) {
+	fullErr := errors.Newf("%s: %v", description, err)
+	return StopResult{
+		Name:    name,
+		Success: false,
+		Error:   fullErr.Error(),
+	}, fullErr
+}
+
+func powerOffError(name string, description string, err error) (PowerOffResult, error) {
+	fullErr := errors.Newf("%s: %v", description, err)
+	return PowerOffResult{
+		Name:    name,
+		Success: false,
+		Error:   fullErr.Error(),
+	}, fullErr
+}
+
+func deleteError(name string, description string, err error) (DeleteResult, error) {
+	fullErr := errors.Newf("%s: %v", description, err)
+	return DeleteResult{
+		Name:    name,
+		Success: false,
+		Error:   fullErr.Error(),
+	}, fullErr
+}
+
+func ipError(name string, description string, err error) (IPResult, error) {
+	fullErr := errors.Newf("%s: %v", description, err)
+	return IPResult{
+		Name:    name,
+		Success: false,
+		Error:   fullErr.Error(),
+	}, fullErr
+}
+
+func statusError(name string, description string, err error) (ClusterStatusResult, error) {
+	fullErr := errors.Newf("%s: %v", description, err)
+	return ClusterStatusResult{
+		Name:    name,
+		Success: false,
+		Error:   fullErr.Error(),
+	}, fullErr
+}
+
+func consoleURLError(description string, err error) (ConsoleResult, error) {
+	fullErr := errors.Newf("%s: %v", description, err)
+	return ConsoleResult{
+		Success: false,
+		Error:   fullErr.Error(),
+	}, fullErr
+}
