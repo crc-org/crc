1fc84e77e5db76df43bea072e8997a240a4c1961
Guillaume Rose <gurose@redhat.com>
2020-07-31 14:22:57 +0200

    Add json output for `crc version`

--- cmd/crc/cmd/version.go
+++ cmd/crc/cmd/version.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
@@ -12,6 +13,7 @@ import (
 )
 
 func init() {
+	versionCmd.Flags().StringVarP(&outputFormat, "output", "o", "", "Output format. One of: json")
 	rootCmd.AddCommand(versionCmd)
 }
 
@@ -20,13 +22,22 @@ var versionCmd = &cobra.Command{
 	Short: "Print version information",
 	Long:  "Print version information",
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := runPrintVersion(os.Stdout, defaultVersion()); err != nil {
+		if err := runPrintVersion(os.Stdout, defaultVersion(), outputFormat); err != nil {
 			exit.WithMessage(1, err.Error())
 		}
 	},
 }
 
-func runPrintVersion(writer io.Writer, version *version) error {
+func runPrintVersion(writer io.Writer, version *version, outputFormat string) error {
+	if outputFormat == jsonFormat {
+		encoder := json.NewEncoder(writer)
+		encoder.SetIndent("", "  ")
+		return encoder.Encode(version)
+	}
+	return printVersion(writer, version)
+}
+
+func printVersion(writer io.Writer, version *version) error {
 	for _, line := range version.lines() {
 		if _, err := fmt.Fprintf(writer, line); err != nil {
 			return err
--- cmd/crc/cmd/version_test.go
+++ cmd/crc/cmd/version_test.go
@@ -14,8 +14,27 @@ func TestPlainVersion(t *testing.T) {
 		Commit:           "aabbcc",
 		OpenshiftVersion: "4.5.4",
 		Embedded:         false,
-	}))
+	}, ""))
 	assert.Equal(t, `CodeReady Containers version: 1.13+aabbcc
 OpenShift version: 4.5.4 (not embedded in binary)
 `, out.String())
+}
+
+func TestJsonVersion(t *testing.T) {
+	out := new(bytes.Buffer)
+	assert.NoError(t, runPrintVersion(out, &version{
+		Version:          "1.13",
+		Commit:           "aabbcc",
+		OpenshiftVersion: "4.5.4",
+		Embedded:         false,
+	}, "json"))
+
+	expected := `{
+  "version": "1.13",
+  "commit": "aabbcc",
+  "openshiftVersion": "4.5.4",
+  "embedded": false
+}
+`
+	assert.Equal(t, expected, out.String())
 }
