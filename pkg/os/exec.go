3bb0d01765f1fa72db6890bd92a95aa80356d2c3
Christophe Fergeau <cfergeau@redhat.com>
2020-08-22 10:00:38 +0200

    Remove exec.RunWithDefaultLocaleAndEnv
    With the removal of oc.EnvRunner, they are only used internally, so we
    can simplify that part of the code as well.

--- pkg/os/exec.go
+++ pkg/os/exec.go
@@ -51,26 +51,12 @@ func RunWithPrivilege(reason string, cmdAndArgs ...string) (string, string, erro
 	return run(sudo, cmdAndArgs, map[string]string{})
 }
 
+var defaultLocaleEnv = map[string]string{"LC_ALL": "C", "LANG": "C"}
+
 func RunWithDefaultLocale(command string, args ...string) (string, string, error) {
-	return RunWithDefaultLocaleAndEnv(command, args, map[string]string{})
+	return run(command, args, defaultLocaleEnv)
 }
 
 func RunWithDefaultLocalePrivate(command string, args ...string) (string, string, error) {
-	return RunWithDefaultLocalePrivateAndEnv(command, args, map[string]string{})
-}
-
-func RunWithDefaultLocaleAndEnv(command string, args []string, extraEnv map[string]string) (string, string, error) {
-	env := map[string]string{"LC_ALL": "C", "LANG": "C"}
-	for k, v := range extraEnv {
-		env[k] = v
-	}
-	return run(command, args, env)
-}
-
-func RunWithDefaultLocalePrivateAndEnv(command string, args []string, extraEnv map[string]string) (string, string, error) {
-	env := map[string]string{"LC_ALL": "C", "LANG": "C"}
-	for k, v := range extraEnv {
-		env[k] = v
-	}
-	return runPrivate(command, args, env)
+	return runPrivate(command, args, defaultLocaleEnv)
 }
