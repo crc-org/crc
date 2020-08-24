c6c08c24722183532f31646f9005e0fa59d45068
Christophe Fergeau <cfergeau@redhat.com>
2020-08-24 19:59:20 +0200

    machine: Remove unneeded conditional block
    Since we error out when the kubelet service is not running, we don't need to
    have a
    if (kubelet_is_running) {
    }
    block in machine.Start()

--- pkg/crc/machine/machine.go
+++ pkg/crc/machine/machine.go
@@ -370,30 +370,29 @@ func (client *client) Start(startConfig StartConfig) (StartResult, error) {
 	if err != nil || kubeletStatus != states.Running {
 		return startError(startConfig.Name, "kubelet service is not running", err)
 	}
-	if kubeletStatus == states.Running {
-		// In Openshift 4.3, when cluster comes up, the following happens
-		// 1. After the openshift-apiserver pod is started, its log contains multiple occurrences of `certificate has expired or is not yet valid`
-		// 2. Initially there is no request-header's client-ca crt available to `extension-apiserver-authentication` configmap
-		// 3. In the pod logs `missing content for CA bundle "client-ca::kube-system::extension-apiserver-authentication::requestheader-client-ca-file"`
-		// 4. After ~1 min /etc/kubernetes/static-pod-resources/kube-apiserver-certs/configmaps/aggregator-client-ca/ca-bundle.crt is regenerated
-		// 5. It is now also appear to `extension-apiserver-authentication` configmap as part of request-header's client-ca content
-		// 6. Openshift-apiserver is able to load the CA which was regenerated
-		// 7. Now apiserver pod log contains multiple occurrences of `error x509: certificate signed by unknown authority`
-		// When the openshift-apiserver is in this state, the cluster is non functional.
-		// A restart of the openshift-apiserver pod is enough to clear that error and get a working cluster.
-		// This is a work-around while the root cause is being identified.
-		// More info: https://bugzilla.redhat.com/show_bug.cgi?id=1795163
-		logging.Debug("Waiting for update of client-ca request header ...")
-		if err := cluster.WaitforRequestHeaderClientCaFile(ocConfig); err != nil {
-			return startError(startConfig.Name, "Failed to wait for the client-ca request header update", err)
-		}
 
-		if err := cluster.DeleteOpenshiftAPIServerPods(ocConfig); err != nil {
-			return startError(startConfig.Name, "Cannot delete OpenShift API Server pods", err)
-		}
-
-		logging.Info("Starting OpenShift cluster ... [waiting 3m]")
+	// In Openshift 4.3, when cluster comes up, the following happens
+	// 1. After the openshift-apiserver pod is started, its log contains multiple occurrences of `certificate has expired or is not yet valid`
+	// 2. Initially there is no request-header's client-ca crt available to `extension-apiserver-authentication` configmap
+	// 3. In the pod logs `missing content for CA bundle "client-ca::kube-system::extension-apiserver-authentication::requestheader-client-ca-file"`
+	// 4. After ~1 min /etc/kubernetes/static-pod-resources/kube-apiserver-certs/configmaps/aggregator-client-ca/ca-bundle.crt is regenerated
+	// 5. It is now also appear to `extension-apiserver-authentication` configmap as part of request-header's client-ca content
+	// 6. Openshift-apiserver is able to load the CA which was regenerated
+	// 7. Now apiserver pod log contains multiple occurrences of `error x509: certificate signed by unknown authority`
+	// When the openshift-apiserver is in this state, the cluster is non functional.
+	// A restart of the openshift-apiserver pod is enough to clear that error and get a working cluster.
+	// This is a work-around while the root cause is being identified.
+	// More info: https://bugzilla.redhat.com/show_bug.cgi?id=1795163
+	logging.Debug("Waiting for update of client-ca request header ...")
+	if err := cluster.WaitforRequestHeaderClientCaFile(ocConfig); err != nil {
+		return startError(startConfig.Name, "Failed to wait for the client-ca request header update", err)
 	}
+
+	if err := cluster.DeleteOpenshiftAPIServerPods(ocConfig); err != nil {
+		return startError(startConfig.Name, "Cannot delete OpenShift API Server pods", err)
+	}
+
+	logging.Info("Starting OpenShift cluster ... [waiting 3m]")
 
 	time.Sleep(time.Minute * 3)
 
@@ -414,7 +413,7 @@ func (client *client) Start(startConfig StartConfig) (StartResult, error) {
 
 	return StartResult{
 		Name:           startConfig.Name,
-		KubeletStarted: (kubeletStatus == states.Running),
+		KubeletStarted: true,
 		ClusterConfig:  *clusterConfig,
 		Status:         vmState.String(),
 	}, err
