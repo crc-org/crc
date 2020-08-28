9da1d49de7f16427d9d2fc25f20276dcbaa7a0f7
Christophe Fergeau <cfergeau@redhat.com>
2020-08-28 11:25:01 +0200

    Allow to configure VM memory/CPUs at crc start time
    Not sure if it's possible to have some generic implementation while keeping
    the type safety?
    For now it's copied and pasted between the 3 platforms we support.
    Let's experiment with memory/sshkeypath first, when this works fine, we can
    add disk resizing to it :)

--- pkg/crc/machine/driver_linux.go
+++ pkg/crc/machine/driver_linux.go
@@ -7,6 +7,7 @@ import (
 	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/machine/config"
 	"github.com/code-ready/crc/pkg/crc/machine/libvirt"
+	machineLibvirt "github.com/code-ready/machine/drivers/libvirt"
 	"github.com/code-ready/machine/libmachine"
 	"github.com/code-ready/machine/libmachine/host"
 )
@@ -18,3 +19,27 @@ func newHost(api libmachine.API, machineConfig config.MachineConfig) (*host.Host
 	}
 	return api.NewHost("libvirt", constants.CrcBinDir, json)
 }
+
+/* FIXME: host.Host is only known here, and libvirt.Driver is only accessible
+ * in libvirt/driver_linux.go
+ */
+func loadDriverConfig(host *host.Host) (*machineLibvirt.Driver, error) {
+	var libvirtDriver machineLibvirt.Driver
+	err := json.Unmarshal(host.RawDriver, &libvirtDriver)
+
+	return &libvirtDriver, err
+}
+
+func updateDriverConfig(host *host.Host, driver *machineLibvirt.Driver) error {
+	driverData, err := json.Marshal(driver)
+	if err != nil {
+		return err
+	}
+	return host.UpdateConfig(driverData)
+}
+
+/*
+func (r *RPCServerDriver) SetConfigRaw(data []byte, _ *struct{}) error {
+	return json.Unmarshal(data, &r.ActualDriver)
+}
+*/
--- pkg/crc/machine/driver_windows.go
+++ pkg/crc/machine/driver_windows.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/code-ready/crc/pkg/crc/machine/config"
 	"github.com/code-ready/crc/pkg/crc/machine/hyperv"
+	machineHyperv "github.com/code-ready/machine/drivers/hyperv"
 	"github.com/code-ready/machine/libmachine"
 	"github.com/code-ready/machine/libmachine/host"
 )
@@ -16,4 +17,19 @@ func newHost(api libmachine.API, machineConfig config.MachineConfig) (*host.Host
 		return nil, errors.New("Failed to marshal driver options")
 	}
 	return api.NewHost("hyperv", "", json)
+}
+
+func loadDriverConfig(host *host.Host) (*machineHyperv.Driver, error) {
+	var hypervDriver machineHyperv.Driver
+	err := json.Unmarshal(host.RawDriver, &hypervDriver)
+
+	return &hypervDriver, err
+}
+
+func updateDriverConfig(host *host.Host, driver *machineHyperv.Driver) error {
+	driverData, err := json.Marshal(driver)
+	if err != nil {
+		return err
+	}
+	return host.UpdateConfig(driverData)
 }
