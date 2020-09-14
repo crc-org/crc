fb24d4b60dd191469f96811c1ac5ff24167e1257
Guillaume Rose <gurose@redhat.com>
2020-09-14 10:00:30 +0200

    Add json support for stop command

--- cmd/crc/cmd/stop.go
+++ cmd/crc/cmd/stop.go
@@ -1,17 +1,21 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/exit"
 	"github.com/code-ready/crc/pkg/crc/input"
-	"github.com/code-ready/crc/pkg/crc/logging"
 	"github.com/code-ready/crc/pkg/crc/machine"
-	"github.com/code-ready/crc/pkg/crc/output"
 	"github.com/code-ready/machine/libmachine/state"
 	"github.com/spf13/cobra"
 )
 
 func init() {
+	addOutputFormatFlag(stopCmd)
 	rootCmd.AddCommand(stopCmd)
 }
 
@@ -20,16 +24,15 @@ var stopCmd = &cobra.Command{
 	Short: "Stop the OpenShift cluster",
 	Long:  "Stop the OpenShift cluster",
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := runStop(); err != nil {
-			exit.WithMessage(1, err.Error())
+		if renderErr := runStop(os.Stdout, machine.NewClient(), outputFormat != jsonFormat, globalForce, outputFormat); renderErr != nil {
+			exit.WithMessage(1, renderErr.Error())
 		}
 	},
 }
 
-func runStop() error {
-	client := machine.NewClient()
+func stopMachine(client machine.Client, interactive, force bool) (bool, error) {
 	if err := checkIfMachineMissing(client, constants.DefaultName); err != nil {
-		return err
+		return false, err
 	}
 
 	result, err := client.Stop(machine.StopConfig{
@@ -37,31 +40,51 @@ func runStop() error {
 		Debug: isDebugLog(),
 	})
 	if err != nil {
+		if !interactive && !force {
+			return false, err
+		}
 		// Here we are checking the VM state and if it is still running then
 		// Ask user to forcefully power off it.
 		if result.State == state.Running {
 			// Most of the time force kill don't work and libvirt throw
 			// Device or resource busy error. To make sure we give some
 			// graceful time to cluster before kill it.
-			yes := input.PromptUserForYesOrNo("Do you want to force power off", globalForce)
+			yes := input.PromptUserForYesOrNo("Do you want to force power off", force)
 			if yes {
 				_, err := client.PowerOff(machine.PowerOffConfig{
 					Name: constants.DefaultName,
 				})
-				if err != nil {
-					return err
-				}
-				output.Outln("Forcibly stopped the OpenShift cluster")
-				return nil
+				return true, err
 			}
 		}
+		return false, err
+	}
+	return false, nil
+}
+
+func runStop(writer io.Writer, client machine.Client, interactive, force bool, outputFormat string) error {
+	forced, err := stopMachine(client, interactive, force)
+	return render(&stopResult{
+		Success: err == nil,
+		Forced:  forced,
+		Error:   errorMessage(err),
+	}, writer, outputFormat)
+}
+
+type stopResult struct {
+	Success bool   `json:"success"`
+	Forced  bool   `json:"forced"`
+	Error   string `json:"error,omitempty"`
+}
+
+func (s *stopResult) prettyPrintTo(writer io.Writer) error {
+	if s.Error != "" {
+		return errors.New(s.Error)
+	}
+	if s.Forced {
+		_, err := fmt.Fprintln(writer, "Forcibly stopped the OpenShift cluster")
 		return err
 	}
-	if result.Success {
-		output.Outln("Stopped the OpenShift cluster")
-	} else {
-		/* If we did not get an error, the status should be true */
-		logging.Warnf("Unexpected status of the OpenShift cluster: %v", result.Success)
-	}
-	return nil
+	_, err := fmt.Fprintln(writer, "Stopped the OpenShift cluster")
+	return err
 }
