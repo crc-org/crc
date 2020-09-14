581f8d26b14d584c179bdef627b549595268b0aa
Guillaume Rose <gurose@redhat.com>
2020-09-14 10:56:25 +0200

    Add json support for delete command

--- cmd/crc/cmd/delete.go
+++ cmd/crc/cmd/delete.go
@@ -1,14 +1,15 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/exit"
 	"github.com/code-ready/crc/pkg/crc/input"
 	"github.com/code-ready/crc/pkg/crc/machine"
-	"github.com/code-ready/crc/pkg/crc/output"
 	"github.com/spf13/cobra"
 )
 
@@ -17,6 +18,7 @@ var clearCache bool
 func init() {
 	deleteCmd.Flags().BoolVarP(&clearCache, "clear-cache", "", false,
 		fmt.Sprintf("Clear the OpenShift cluster cache at: %s", constants.MachineCacheDir))
+	addOutputFormatFlag(deleteCmd)
 	rootCmd.AddCommand(deleteCmd)
 }
 
@@ -25,17 +27,21 @@ var deleteCmd = &cobra.Command{
 	Short: "Delete the OpenShift cluster",
 	Long:  "Delete the OpenShift cluster",
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := runDelete(machine.NewClient()); err != nil {
+		if err := runDelete(os.Stdout, machine.NewClient(), clearCache, constants.MachineCacheDir, outputFormat != jsonFormat, globalForce, outputFormat); err != nil {
 			exit.WithMessage(1, err.Error())
 		}
 	},
 }
 
-func runDelete(client machine.Client) error {
+func deleteMachine(client machine.Client, clearCache bool, cacheDir string, interactive, force bool) error {
+	if !interactive && !force {
+		return errors.New("non-interactive deletion requires --force")
+	}
+
 	if clearCache {
-		yes := input.PromptUserForYesOrNo("Do you want to delete the OpenShift cluster cache", globalForce)
+		yes := input.PromptUserForYesOrNo("Do you want to delete the OpenShift cluster cache", force)
 		if yes {
-			_ = os.RemoveAll(constants.MachineCacheDir)
+			_ = os.RemoveAll(cacheDir)
 		}
 	}
 
@@ -43,15 +49,33 @@ func runDelete(client machine.Client) error {
 		return err
 	}
 
-	yes := input.PromptUserForYesOrNo("Do you want to delete the OpenShift cluster", globalForce)
+	yes := input.PromptUserForYesOrNo("Do you want to delete the OpenShift cluster", force)
 	if yes {
 		_, err := client.Delete(machine.DeleteConfig{
 			Name: constants.DefaultName,
 		})
-		if err != nil {
-			return err
-		}
-		output.Outln("Deleted the OpenShift cluster")
+		return err
 	}
 	return nil
+}
+
+func runDelete(writer io.Writer, client machine.Client, clearCache bool, cacheDir string, interactive, force bool, outputFormat string) error {
+	err := deleteMachine(client, clearCache, cacheDir, interactive, force)
+	return render(&deleteResult{
+		Success: err == nil,
+		Error:   errorMessage(err),
+	}, writer, outputFormat)
+}
+
+type deleteResult struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error,omitempty"`
+}
+
+func (s *deleteResult) prettyPrintTo(writer io.Writer) error {
+	if s.Error != "" {
+		return errors.New(s.Error)
+	}
+	_, err := fmt.Fprintln(writer, "Deleted the OpenShift cluster")
+	return err
 }
