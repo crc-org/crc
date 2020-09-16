6351ebff3f87c301055f828332833ee15a950d10
Christophe Fergeau <cfergeau@redhat.com>
2020-09-16 10:12:18 +0200

    Adjust 'crc config set memory/cpu' message
    Only a 'stop/start' is needed with the previous changes in this series,
    not a full 'delete/start'.

--- pkg/crc/config/callbacks.go
+++ pkg/crc/config/callbacks.go
@@ -7,9 +7,9 @@ import (
 )
 
 func RequiresRestartMsg(key string, _ interface{}) string {
-	return fmt.Sprintf("Changes to configuration property '%s' are only applied when a new CRC instance is created.\n"+
-		"If you already have a CRC instance, then for this configuration change to take effect, "+
-		"delete the CRC instance with 'crc delete' and start a new one with 'crc start'.", key)
+	return fmt.Sprintf("Changes to configuration property '%s' are only applied when the CRC instance is started.\n"+
+		"If you already have a running CRC instance, then for this configuration change to take effect, "+
+		"stop the CRC instance with 'crc stop' and restart it with 'crc start'.", key)
 }
 
 func SuccessfullyApplied(key string, value interface{}) string {
