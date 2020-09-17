e36f073a2de2d5115a95d3c6515e1f4441c0bc07
Christophe Fergeau <cfergeau@redhat.com>
2020-09-17 16:30:32 +0200

    cache: Use common code to get binary version
    The only difference between our various getXxxVersion implementations is
    the arguments we pass to the binary. This can easily be handled through
    a helper.

--- pkg/crc/cache/cache_darwin.go
+++ pkg/crc/cache/cache_darwin.go
@@ -1,13 +1,8 @@
 package cache
 
 import (
-	"fmt"
-	"path/filepath"
-	"strings"
-
 	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/machine/hyperkit"
-	crcos "github.com/code-ready/crc/pkg/os"
 )
 
 func NewMachineDriverHyperkitCache() *Cache {
@@ -18,11 +13,6 @@ func NewHyperkitCache() *Cache {
 	return New(hyperkit.HyperkitCommand, hyperkit.HyperkitDownloadURL, constants.CrcBinDir, "", nil)
 }
 
-func getHyperKitMachineDriverVersion() (string, error) {
-	driverBinPath := filepath.Join(constants.CrcBinDir, hyperkit.MachineDriverCommand)
-	stdOut, _, err := crcos.RunWithDefaultLocale(driverBinPath, "version")
-	if len(strings.Split(stdOut, ":")) < 2 {
-		return "", fmt.Errorf("Unable to parse the version information of %s", driverBinPath)
-	}
-	return strings.TrimSpace(strings.Split(stdOut, ":")[1]), err
+func getHyperKitMachineDriverVersion(binaryPath string) (string, error) {
+	return getVersionGeneric(binaryPath, "version")
 }
