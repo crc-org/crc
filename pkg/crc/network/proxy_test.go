447ba285a66f02fa25a76334118e4d11ec7e02b2
Guillaume Rose <gurose@redhat.com>
2020-09-25 16:59:02 +0200

    Add unit test for ProxyConfig serialization

--- pkg/crc/network/proxy_test.go
+++ pkg/crc/network/proxy_test.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,4 +18,16 @@ func TestHidePassword(t *testing.T) {
 	url, err := URIStringForDisplay("https://user:secret@proxy.org:123")
 	assert.NoError(t, err)
 	assert.Equal(t, "https://user:xxx@proxy.org:123", url)
+}
+
+func TestMarshal(t *testing.T) {
+	bin, err := json.Marshal(&ProxyConfig{
+		HTTPProxy:   "http://proxy.org",
+		HTTPSProxy:  "http://proxy.org",
+		noProxy:     defaultNoProxies,
+		ProxyCAFile: "/home/john/ca.crt",
+		ProxyCACert: "very long string",
+	})
+	assert.NoError(t, err)
+	assert.JSONEq(t, `{"HTTPProxy":"http://proxy.org", "HTTPSProxy":"http://proxy.org", "ProxyCACert":"very long string", "ProxyCAFile":"/home/john/ca.crt"}`, string(bin))
 }
