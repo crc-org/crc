7ef1b8ab8f7624b209cb514b68037dfef8df0742
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2020-09-29 16:09:25 +0530

    Add json output for console command

--- cmd/crc/cmd/console.go
+++ cmd/crc/cmd/console.go
@@ -3,11 +3,12 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/exit"
 	"github.com/code-ready/crc/pkg/crc/machine"
-	"github.com/code-ready/crc/pkg/crc/output"
 	"github.com/code-ready/machine/libmachine/state"
 	"github.com/pkg/browser"
 	"github.com/spf13/cobra"
@@ -19,6 +20,7 @@ var (
 )
 
 func init() {
+	addOutputFormatFlag(consoleCmd)
 	consoleCmd.Flags().BoolVar(&consolePrintURL, "url", false, "Print the URL for the OpenShift Web Console")
 	consoleCmd.Flags().BoolVar(&consolePrintCredentials, "credentials", false, "Print the credentials for the OpenShift Web Console")
 	rootCmd.AddCommand(consoleCmd)
@@ -31,45 +33,99 @@ var consoleCmd = &cobra.Command{
 	Short:   "Open the OpenShift Web Console in the default browser",
 	Long:    `Open the OpenShift Web Console in the default browser or print its URL or credentials`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := runConsole(args); err != nil {
-			exit.WithMessage(1, err.Error())
+		if renderErr := runConsole(os.Stdout, machine.NewClient(), consolePrintURL, consolePrintCredentials, outputFormat); renderErr != nil {
+			exit.WithMessage(1, renderErr.Error())
 		}
 	},
 }
 
-func runConsole(arguments []string) error {
+func showConsole(client machine.Client) (machine.ConsoleResult, error) {
 	consoleConfig := machine.ConsoleConfig{
 		Name: constants.DefaultName,
 	}
 
-	client := machine.NewClient()
 	if err := checkIfMachineMissing(client, consoleConfig.Name); err != nil {
+		// In case of machine doesn't exist then consoleResult error
+		// should be updated so that when rendering the result it have
+		// error details also.
+		consoleResult := machine.ConsoleResult{}
+		consoleResult.Error = err.Error()
+		return consoleResult, err
+	}
+
+	return client.GetConsoleURL(consoleConfig)
+}
+func runConsole(writer io.Writer, client machine.Client, consolePrintURL, consolePrintCredentials bool, outputFormat string) error {
+	result, err := showConsole(client)
+	return render(&consoleResult{
+		Success:                 err == nil,
+		state:                   result.State,
+		ClusterConfig:           toConsoleClusterConfig(&result),
+		Error:                   errorMessage(err),
+		consolePrintURL:         consolePrintURL,
+		consolePrintCredentials: consolePrintCredentials,
+	}, writer, outputFormat)
+}
+
+type consoleResult struct {
+	Success                 bool `json:"success"`
+	state                   state.State
+	Error                   string         `json:"error,omitempty"`
+	ClusterConfig           *clusterConfig `json:"clusterConfig,omitempty"`
+	consolePrintURL         bool
+	consolePrintCredentials bool
+}
+
+func (s *consoleResult) prettyPrintTo(writer io.Writer) error {
+	if s.Error != "" {
+		return errors.New(s.Error)
+	}
+	if s.consolePrintURL {
+		_, err := fmt.Fprintln(writer, s.ClusterConfig.WebConsoleURL)
 		return err
 	}
 
-	result, err := client.GetConsoleURL(consoleConfig)
-	if err != nil {
+	if s.consolePrintCredentials {
+		if _, err := fmt.Fprintf(writer, "To login as a regular user, run 'oc login -u %s -p %s %s'.\n",
+			s.ClusterConfig.DeveloperCredentials.Username, s.ClusterConfig.DeveloperCredentials.Password, s.ClusterConfig.URL); err != nil {
+			return err
+		}
+		_, err := fmt.Fprintf(writer, "To login as an admin, run 'oc login -u %s -p %s %s'\n",
+			s.ClusterConfig.AdminCredentials.Username, s.ClusterConfig.AdminCredentials.Password, s.ClusterConfig.URL)
 		return err
 	}
-
-	if consolePrintURL {
-		output.Outln(result.ClusterConfig.WebConsoleURL)
-	}
-	if consolePrintCredentials {
-		output.Outf("To login as a regular user, run 'oc login -u developer -p developer %s'.\n", result.ClusterConfig.ClusterAPI)
-		output.Outf("To login as an admin, run 'oc login -u kubeadmin -p %s %s'\n", result.ClusterConfig.KubeAdminPass, result.ClusterConfig.ClusterAPI)
-	}
-	if consolePrintURL || consolePrintCredentials {
+	if s.consolePrintURL || s.consolePrintCredentials {
 		return nil
 	}
 
-	if result.State != state.Running {
+	if s.state != state.Running {
 		return errors.New("The OpenShift cluster is not running, cannot open the OpenShift Web Console")
 	}
-	output.Outln("Opening the OpenShift Web Console in the default browser...")
-	err = browser.OpenURL(result.ClusterConfig.WebConsoleURL)
-	if err != nil {
-		return fmt.Errorf("Failed to open the OpenShift Web Console, you can access it by opening %s in your web browser", result.ClusterConfig.WebConsoleURL)
+
+	if _, err := fmt.Fprint(writer, "Opening the OpenShift Web Console in the default browser..."); err != nil {
+		return err
 	}
+	if err := browser.OpenURL(s.ClusterConfig.WebConsoleURL); err != nil {
+		return fmt.Errorf("Failed to open the OpenShift Web Console, you can access it by opening %s in your web browser", s.ClusterConfig.WebConsoleURL)
+	}
+
 	return nil
+}
+
+func toConsoleClusterConfig(result *machine.ConsoleResult) *clusterConfig {
+	if result == nil || result.Error != "" {
+		return nil
+	}
+	return &clusterConfig{
+		WebConsoleURL: result.ClusterConfig.WebConsoleURL,
+		URL:           result.ClusterConfig.ClusterAPI,
+		AdminCredentials: credentials{
+			Username: "kubeadmin",
+			Password: result.ClusterConfig.KubeAdminPass,
+		},
+		DeveloperCredentials: credentials{
+			Username: "developer",
+			Password: "developer",
+		},
+	}
 }
