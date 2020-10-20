dec7d02cabc552cd7bb8f1a848957e4f631a3594
Guillaume Rose <gurose@redhat.com>
2020-10-20 14:20:11 +0200

    Bump openshift vendoring from 4.5 to 4.6
    Using this https://github.com/openshift/oc/blob/release-4.6/go.mod as
    reference.

--- vendor/k8s.io/client-go/tools/clientcmd/loader.go
+++ vendor/k8s.io/client-go/tools/clientcmd/loader.go
@@ -28,7 +28,7 @@ import (
 	"strings"
 
 	"github.com/imdario/mergo"
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -434,6 +434,33 @@ func WriteToFile(config clientcmdapi.Config, filename string) error {
 		return err
 	}
 	return nil
+}
+
+func lockFile(filename string) error {
+	// TODO: find a way to do this with actual file locks. Will
+	// probably need separate solution for windows and Linux.
+
+	// Make sure the dir exists before we try to create a lock file.
+	dir := filepath.Dir(filename)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err = os.MkdirAll(dir, 0755); err != nil {
+			return err
+		}
+	}
+	f, err := os.OpenFile(lockName(filename), os.O_CREATE|os.O_EXCL, 0)
+	if err != nil {
+		return err
+	}
+	f.Close()
+	return nil
+}
+
+func unlockFile(filename string) error {
+	return os.Remove(lockName(filename))
+}
+
+func lockName(filename string) string {
+	return filename + ".lock"
 }
 
 // Write serializes the config to yaml.
