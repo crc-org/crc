dbd32e10a652186736a57f1b9b926a7c692ce54b
Guillaume Rose <gurose@redhat.com>
2020-10-30 16:04:56 +0100

    Default network configuration for macOS uses VMNet

--- pkg/crc/machine/hyperkit/driver_darwin.go
+++ pkg/crc/machine/hyperkit/driver_darwin.go
@@ -18,6 +18,7 @@ func CreateHost(machineConfig config.MachineConfig) *hyperkit.Driver {
 	hyperkitDriver.VmlinuzPath = machineConfig.Kernel
 	hyperkitDriver.InitrdPath = machineConfig.Initramfs
 	hyperkitDriver.HyperKitPath = filepath.Join(constants.CrcBinDir, "hyperkit")
+	hyperkitDriver.VMNet = true
 
 	return hyperkitDriver
 }
