2d753885d44280d8c85ee117ef9413d5c6c9ca51
Christophe Fergeau <cfergeau@redhat.com>
2020-11-12 17:21:21 +0100

    machine: Only call driver.UpdateConfigRaw when value changed
    At the moment, we always call driver.UpdateConfigRaw, even if the
    memory/cpu/... value did not change.
    This causes incorrect warnings with the macos driver which requires
    UpdateConfigRaw to only be called when the value changed.

--- pkg/crc/machine/driver.go
+++ pkg/crc/machine/driver.go
@@ -6,37 +6,53 @@ import (
 	"github.com/code-ready/machine/libmachine/host"
 )
 
-type valueSetter func(driver *libmachine.VMDriver)
+type valueSetter func(driver *libmachine.VMDriver) bool
 
 func updateDriverValue(host *host.Host, setDriverValue valueSetter) error {
 	driver, err := loadDriverConfig(host)
 	if err != nil {
 		return err
 	}
-	setDriverValue(driver.VMDriver)
+	valueChanged := setDriverValue(driver.VMDriver)
+	if !valueChanged {
+		return nil
+	}
 
 	return updateDriverConfig(host, driver)
 }
 
 func setMemory(host *host.Host, memorySize int) error {
-	memorySetter := func(driver *libmachine.VMDriver) {
+	memorySetter := func(driver *libmachine.VMDriver) bool {
+		if driver.Memory == memorySize {
+			return false
+		}
 		driver.Memory = memorySize
+		return true
 	}
 
 	return updateDriverValue(host, memorySetter)
 }
 
 func setVcpus(host *host.Host, vcpus int) error {
-	vcpuSetter := func(driver *libmachine.VMDriver) {
+	vcpuSetter := func(driver *libmachine.VMDriver) bool {
+		if driver.CPU == vcpus {
+			return false
+		}
 		driver.CPU = vcpus
+		return true
 	}
 
 	return updateDriverValue(host, vcpuSetter)
 }
 
 func setDiskSize(host *host.Host, diskSizeGiB int) error {
-	diskSizeSetter := func(driver *libmachine.VMDriver) {
-		driver.DiskCapacity = config.ConvertGiBToBytes(diskSizeGiB)
+	diskSizeSetter := func(driver *libmachine.VMDriver) bool {
+		capacity := config.ConvertGiBToBytes(diskSizeGiB)
+		if driver.DiskCapacity == capacity {
+			return false
+		}
+		driver.DiskCapacity = capacity
+		return true
 	}
 
 	return updateDriverValue(host, diskSizeSetter)
