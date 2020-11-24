35ba24d9cd501dc40d160c34ee17a19bf60231a1
Christophe Fergeau <cfergeau@redhat.com>
2020-11-24 15:21:28 +0100

    systemd: Add test for systemd units state
    The systemd code was missing support for 'Listening' socket unit files,
    and does not handle correctly the 'NotFound' state. The 'NotFound'
    message is written to stderr, while systemd.service() only matches
    stdout.
    This commit extends the systemd unit test to test these code paths.

--- pkg/crc/systemd/systemd_linux_test.go
+++ pkg/crc/systemd/systemd_linux_test.go
@@ -1,6 +1,7 @@
 package systemd
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 
@@ -34,6 +35,26 @@ func TestSystemd(t *testing.T) {
 	assert.Error(t, err)
 }
 
+func TestSystemdStatuses(t *testing.T) {
+	systemctl, _ := newMockCommander(t)
+
+	status, err := systemctl.Status("running.service")
+	assert.NoError(t, err)
+	assert.Equal(t, states.Running.String(), status.String())
+
+	status, err = systemctl.Status("listening.socket")
+	assert.NoError(t, err)
+	assert.Equal(t, states.Listening.String(), status.String())
+
+	status, err = systemctl.Status("stopped.service")
+	assert.NoError(t, err)
+	assert.Equal(t, states.Stopped.String(), status.String())
+
+	status, err = systemctl.Status("notfound.service")
+	assert.NoError(t, err)
+	assert.Equal(t, states.NotFound.String(), status.String())
+}
+
 type mockSystemdRunner struct {
 	test    *testing.T
 	failing bool
@@ -41,7 +62,21 @@ type mockSystemdRunner struct {
 
 func (r *mockSystemdRunner) Run(command string, args ...string) (string, string, error) {
 	assertSystemCtlCommand(r.test, "status", command, args)
-	return r.status(states.Running)
+	assert.GreaterOrEqual(r.test, len(args), 2)
+
+	unitName := args[1]
+	switch unitName {
+	case "running.service":
+		return r.status(states.Running)
+	case "listening.socket":
+		return r.status(states.Listening)
+	case "stopped.service":
+		return r.status(states.Stopped)
+	case "notfound.service":
+		return r.status(states.NotFound)
+	default:
+		return r.status(states.Running)
+	}
 }
 
 func (r *mockSystemdRunner) RunPrivate(command string, args ...string) (string, string, error) {
@@ -69,20 +104,69 @@ func (r *mockSystemdRunner) setFailing(failing bool) {
 	r.failing = failing
 }
 
+const (
+	statusRunning string = `● running.service - running service
+     Loaded: loaded (/usr/lib/systemd/system/running.service; enabled; vendor preset: enabled)
+     Active: active (running) since Tue 2020-11-24 14:52:20 CET; 15s ago
+TriggeredBy: ● listening.socket
+       Docs: man:running(8)
+             https://running.example.com
+   Main PID: 224516 (running)
+      Tasks: 19 (limit: 32768)
+     Memory: 36.9M
+        CPU: 414ms
+     CGroup: /system.slice/running.service
+             ├─ 19588 /usr/sbin/true
+             ├─ 19589 /usr/sbin/true
+             └─224516 /usr/sbin/running
+`
+	statusListening string = `● listening.socket - listening socket
+     Loaded: loaded (/usr/lib/systemd/system/listening.socket; enabled; vendor preset: disabled)
+     Active: active (listening) since Mon 2020-11-16 17:02:29 CET; 1 weeks 0 days ago
+   Triggers: ● running.service
+     Listen: /run/listening/listening-sock (Stream)
+      Tasks: 0 (limit: 38160)
+     Memory: 0B
+        CPU: 0
+     CGroup: /system.slice/listening.socket
+`
+	statusStopped string = `● stopped.service - stopped service
+     Loaded: loaded (/usr/lib/systemd/system/stopped.service; enabled; vendor preset: enabled)
+     Active: inactive (dead) since Tue 2020-11-17 12:22:15 CET; 1 weeks 0 days ago
+TriggeredBy: ● listening.socket
+       Docs: man:stopped(8)
+             https://stopped.example.com
+    Process: 64922 ExecStart=/usr/sbin/true (code=exited, status=0/SUCCESS)
+   Main PID: 64922 (code=exited, status=0/SUCCESS)
+        CPU: 327ms
+`
+	statusNotFound string = "Unit notfound.service could not be found."
+)
+
 func (r *mockSystemdRunner) status(s states.State) (string, string, error) {
 	var (
 		err    error
 		stdout string
+		stderr string
 	)
 
 	if r.failing {
-		stdout = "error"
-		err = fmt.Errorf("Failed to run systemd command")
-	} else {
-		stdout = s.String()
+		return "error", "", fmt.Errorf("Failed to run systemd command")
+	}
+	switch s {
+	case states.Running:
+		stdout = statusRunning
+	case states.Listening:
+		stdout = statusListening
+	case states.Stopped:
+		stdout = statusStopped
+		err = errors.New("exit code: 3 - see EXIT STATUS in man systemctl")
+	case states.NotFound:
+		stderr = statusNotFound
+		err = errors.New("exit code: 4 - see EXIT STATUS in man systemctl")
 	}
 
-	return stdout, "", err
+	return stdout, stderr, err
 }
 
 func assertSystemCtlCommands(t *testing.T, systemctlCommands []string, cmd string, args []string) {
