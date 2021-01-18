fa3af8b926e87fb845ad0ddda12db9a44772b7fd
Guillaume Rose <gurose@redhat.com>
2021-01-18 21:44:28 +0100

    libmachine: move config version constant along with the struct Host
    Version belongs only to this struct.

--- pkg/libmachine/version/version.go
+++
@@ -1,8 +0,0 @@
-package version
-
-var (
-	// ConfigVersion dictates which version of the config.json format is
-	// used. It needs to be bumped if there is a breaking change, and
-	// therefore migration, introduced to the config file format.
-	ConfigVersion = 3
-)
