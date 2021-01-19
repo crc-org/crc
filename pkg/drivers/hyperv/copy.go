f91a0d633d03c61e2f9c273ba1a564abf7fdb360
Guillaume Rose <gurose@redhat.com>
2021-01-19 08:58:52 +0100

    libmachine: move copy file function where it is used, in the driver

---
+++ pkg/drivers/hyperv/copy.go
@@ -0,0 +1,33 @@
+package hyperv
+
+import (
+	"io"
+	"os"
+)
+
+func copyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	if _, err = io.Copy(out, in); err != nil {
+		return err
+	}
+
+	fi, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	return os.Chmod(dst, fi.Mode())
+}
