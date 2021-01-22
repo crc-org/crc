ddb46d6e81ce92e0c99fa97a3c7de7d4bc3e0c5a
Guillaume Rose <gurose@redhat.com>
2021-01-22 21:26:18 +0100

    Prepare an archive with everything to build the macOS pkg. Add codesign.

---
+++ packaging/package.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -euxo pipefail
+
+BASEDIR=$(dirname "$0")
+OUTPUT=$1
+CODESIGN_IDENTITY=${CODESIGN_IDENTITY:-mock}
+PRODUCTSIGN_IDENTITY=${PRODUCTSIGN_IDENTITY:-mock}
+NO_CODESIGN=${NO_CODESIGN:-0}
+
+function sign() {
+  if [ "${NO_CODESIGN}" -eq "1" ]; then
+    return
+  fi
+  local opts=""
+  entitlements="${BASEDIR}/$(basename "$1").entitlements"
+  if [ -f "${entitlements}" ]; then
+      opts="--entitlements ${entitlements}"
+  fi
+  codesign --deep --sign "${CODESIGN_IDENTITY}" --options runtime --force ${opts} "$1"
+}
+
+version=$(ls "${BASEDIR}/root/Library/CodeReady Containers/")
+
+binDir="${BASEDIR}/root/Library/CodeReady Containers/${version}"
+sign "${binDir}/crc"
+sign "${binDir}/admin-helper-darwin"
+sign "${binDir}/crc-driver-hyperkit"
+sign "${binDir}/CodeReady Containers.app"
+
+codesign --verify --verbose "${binDir}/hyperkit"
+
+pkgbuild --identifier com.redhat.crc.${version} --version ${version} \
+  --scripts "${BASEDIR}/darwin/scripts" \
+  --root "${BASEDIR}/root" \
+  --install-location / \
+  --component-plist "${BASEDIR}/components.plist" \
+  "${OUTPUT}/crc.pkg"
+
+productbuild --distribution "${BASEDIR}/darwin/Distribution" \
+  --resources "${BASEDIR}/darwin/Resources" \
+  --package-path "${OUTPUT}" \
+  "${OUTPUT}/crc-unsigned.pkg"
+rm "${OUTPUT}/crc.pkg"
+
+if [ ! "${NO_CODESIGN}" -eq "1" ]; then
+  productsign --sign "${PRODUCTSIGN_IDENTITY}" "${OUTPUT}/crc-unsigned.pkg" "${OUTPUT}/crc-macos-amd64.pkg"
+else
+  mv "${OUTPUT}/crc-unsigned.pkg" "${OUTPUT}/crc-macos-amd64.pkg"
+fi
