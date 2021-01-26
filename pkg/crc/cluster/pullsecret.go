bac38d951468b6ed7830e62423ccebfacb4f7fcd
Guillaume Rose <gurose@redhat.com>
2021-01-26 13:44:49 +0100

    Use non interactive loader in interactive pull secret loader
    Previously, we had 2 ways of loading the pull secret: one for the
    daemon, and one for the CLI. Now it's the same code.

--- pkg/crc/cluster/pullsecret.go
+++ pkg/crc/cluster/pullsecret.go
@@ -1,13 +1,16 @@
 package cluster
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	cmdConfig "github.com/code-ready/crc/cmd/crc/cmd/config"
 	crcConfig "github.com/code-ready/crc/pkg/crc/config"
 	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/input"
+	"github.com/code-ready/crc/pkg/crc/logging"
 	"github.com/code-ready/crc/pkg/crc/validation"
 	crcversion "github.com/code-ready/crc/pkg/crc/version"
 )
@@ -17,58 +20,47 @@ type PullSecretLoader interface {
 }
 
 type interactivePullSecretLoader struct {
-	config *crcConfig.Config
+	nonInteractivePullSecretLoader *nonInteractivePullSecretLoader
 }
 
-func NewInteractivePullSecretLoader(config *crcConfig.Config) PullSecretLoader {
+func NewInteractivePullSecretLoader(config crcConfig.Storage) PullSecretLoader {
 	return &PullSecretMemoizer{
 		Getter: &interactivePullSecretLoader{
-			config: config,
+			nonInteractivePullSecretLoader: &nonInteractivePullSecretLoader{
+				config: config,
+			},
 		},
 	}
 }
 
 func (loader *interactivePullSecretLoader) Value() (string, error) {
-	var (
-		pullsecret string
-		err        error
-	)
+	fromNonInteractive, err := loader.nonInteractivePullSecretLoader.Value()
+	if err == nil {
+		return fromNonInteractive, nil
+	}
 
-	// If crc is built from an OKD bundle, then use the fake pull secret in contants.
-	if crcversion.IsOkdBuild() {
-		return constants.OkdPullSecret, nil
-	}
-	// In case user doesn't provide a file in start command or in config then ask for it.
-	if loader.config.Get(cmdConfig.PullSecretFile).AsString() == "" {
-		pullsecret, err = input.PromptUserForSecret("Image pull secret", fmt.Sprintf("Copy it from %s", constants.CrcLandingPageURL))
-		// This is just to provide a new line after user enter the pull secret.
-		fmt.Println()
-		if err != nil {
-			return "", err
-		}
-	} else {
-		// Read the file content
-		data, err := ioutil.ReadFile(loader.config.Get(cmdConfig.PullSecretFile).AsString())
-		if err != nil {
-			return "", err
-		}
-		pullsecret = string(data)
-	}
-	if err := validation.ImagePullSecret(pullsecret); err != nil {
+	fromUser, err := input.PromptUserForSecret("Image pull secret", fmt.Sprintf("Copy it from %s", constants.CrcLandingPageURL))
+	// This is just to provide a new line after user enter the pull secret.
+	fmt.Println()
+	if err != nil {
 		return "", err
 	}
-
-	return pullsecret, nil
+	if err := validation.ImagePullSecret(fromUser); err != nil {
+		return "", err
+	}
+	return fromUser, nil
 }
 
 type nonInteractivePullSecretLoader struct {
-	path string
+	config crcConfig.Storage
+	path   string
 }
 
-func NewNonInteractivePullSecretLoader(path string) PullSecretLoader {
+func NewNonInteractivePullSecretLoader(config crcConfig.Storage, path string) PullSecretLoader {
 	return &PullSecretMemoizer{
 		Getter: &nonInteractivePullSecretLoader{
-			path: path,
+			config: config,
+			path:   path,
 		},
 	}
 }
@@ -79,13 +71,29 @@ func (loader *nonInteractivePullSecretLoader) Value() (string, error) {
 		return constants.OkdPullSecret, nil
 	}
 
-	data, err := ioutil.ReadFile(loader.path)
+	if loader.path != "" {
+		fromPath, err := loadFile(loader.path)
+		if err == nil {
+			return fromPath, nil
+		}
+		logging.Debugf("Cannot load secret from path %q: %v", loader.path, err)
+	}
+	fromConfig, err := loadFile(loader.config.Get(cmdConfig.PullSecretFile).AsString())
+	if err == nil {
+		return fromConfig, nil
+	}
+	logging.Debugf("Cannot load secret from configuration: %v", err)
+	return "", fmt.Errorf("unable to load pull secret from path %q or from configuration", loader.path)
+}
+
+func loadFile(path string) (string, error) {
+	if path == "" {
+		return "", errors.New("empty path")
+	}
+	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return "", err
 	}
-	pullsecret := string(data)
-	if err := validation.ImagePullSecret(pullsecret); err != nil {
-		return "", err
-	}
-	return pullsecret, nil
+	pullsecret := strings.TrimSpace(string(data))
+	return pullsecret, validation.ImagePullSecret(pullsecret)
 }
