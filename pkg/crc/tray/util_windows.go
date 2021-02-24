9b39ee37934de4d047c5547babba92bf148cd3af
Anjan Nath <kaludios@gmail.com>
2021-02-25 01:28:02 +0530

    Delete daemon autostart config when network mode is default
    If network-mode is VSock then daemon should not be prevented from
    autostarting at login

--- pkg/crc/tray/util_windows.go
+++ pkg/crc/tray/util_windows.go
@@ -1,8 +1,13 @@
 package tray
 
-func DisableEnableTrayAutostart(key string, value interface{}) string {
+func DisableTrayAutostart() error {
 	// noop
-	return ""
+	return nil
+}
+
+func DisableDaemonAutostart() error {
+	// noop
+	return nil
 }
 
 // ValidateTrayAutostart checks tray-auto-start is used in macOS and its a bool
