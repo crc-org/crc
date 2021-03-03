1ead0b3b526d2244851a7b1dad9c17490fea1e17
Anjan Nath <kaludios@gmail.com>
2021-03-03 14:29:46 +0530

    Revert "Issue #1937 Remove tray and daemon preflight checks from experimental"
    This reverts commit ca8f447c63239d38b2baac485786b72f58386530.

--- pkg/crc/preflight/preflight_darwin_test.go
+++ pkg/crc/preflight/preflight_darwin_test.go
@@ -16,9 +16,9 @@ func TestCountConfigurationOptions(t *testing.T) {
 }
 
 func TestCountPreflights(t *testing.T) {
-	assert.Len(t, getPreflightChecks(false, network.DefaultMode), 20)
+	assert.Len(t, getPreflightChecks(false, network.DefaultMode), 14)
 	assert.Len(t, getPreflightChecks(true, network.DefaultMode), 20)
 
-	assert.Len(t, getPreflightChecks(false, network.VSockMode), 19)
+	assert.Len(t, getPreflightChecks(false, network.VSockMode), 13)
 	assert.Len(t, getPreflightChecks(true, network.VSockMode), 19)
 }
