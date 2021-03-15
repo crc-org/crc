1a75b1e19ba7d55cb023a16d8c7c2074e3c9e4c9
Guillaume Rose <gurose@redhat.com>
2021-03-15 14:23:24 +0100

    Add synchronisation between actions. Cancel start when calling stop
    Accesses to the currentState and startCancel fields of the Synchronized
    struct must be protected by the stateLock mutex. This field will be used
    to prevent simultaneous start/stop/delete.
    
    The slightly more complicated part comes from cancel handling and the
    syncOperationDone channel. The channel will be used by Start() to allow
    the cancel method to wait until Start() has completed.
    It's also used for asynchronous Stopping/Deleting/Starting -> Idle
    transition once the operation has completed. For lack of a better place
    to read from that channel and update currentState, this asynchronous
    update is done in currentStateUnlocked(). currentState must not be read
    directly as it can be stale.

---
+++ pkg/crc/machine/sync.go
@@ -0,0 +1,170 @@
+package machine
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+
+	"github.com/code-ready/crc/pkg/crc/logging"
+	"github.com/code-ready/machine/libmachine/state"
+)
+
+const startCancelTimeout = 15 * time.Second
+
+type State string
+
+const (
+	Idle     State = "Idle"
+	Deleting State = "Deleting"
+	Stopping State = "Stopping"
+	Starting State = "Starting"
+)
+
+type Synchronized struct {
+	underlying Client
+
+	stateLock    sync.Mutex
+	currentState State
+	startCancel  context.CancelFunc
+
+	syncOperationDone chan State
+}
+
+func NewSynchronizedMachine(machine Client) *Synchronized {
+	return &Synchronized{
+		underlying:        machine,
+		currentState:      Idle,
+		syncOperationDone: make(chan State, 1),
+	}
+}
+
+func (s *Synchronized) CurrentState() State {
+	s.stateLock.Lock()
+	defer s.stateLock.Unlock()
+
+	return s.currentStateUnlocked()
+}
+
+func (s *Synchronized) currentStateUnlocked() State {
+	select {
+	case st := <-s.syncOperationDone:
+		if s.currentState == st {
+			s.currentState = Idle
+		}
+		if st == Starting {
+			s.startCancel = nil
+		}
+	default:
+	}
+	return s.currentState
+}
+
+func (s *Synchronized) Delete() error {
+	if err := s.prepareStopDelete(Deleting); err != nil {
+		return err
+	}
+
+	err := s.underlying.Delete()
+	s.syncOperationDone <- Deleting
+	return err
+}
+
+func (s *Synchronized) prepareStart(startCancel context.CancelFunc) error {
+	s.stateLock.Lock()
+	defer s.stateLock.Unlock()
+	if s.currentStateUnlocked() != Idle {
+		return errors.New("cluster is busy")
+	}
+	s.startCancel = startCancel
+	s.currentState = Starting
+
+	return nil
+}
+
+func (s *Synchronized) Start(ctx context.Context, startConfig StartConfig) (*StartResult, error) {
+	ctx, startCancel := context.WithCancel(ctx)
+	if err := s.prepareStart(startCancel); err != nil {
+		return nil, err
+	}
+
+	startResult, err := s.underlying.Start(ctx, startConfig)
+	s.syncOperationDone <- Starting
+	return startResult, err
+}
+
+/* cancel ongoing start, and wait until the start is fully cancelled. Time out if cancellation takes more than 'timeout'
+ * s.stateLock must be locked before calling this function
+ */
+func (s *Synchronized) cancelUnlocked(timeout time.Duration) error {
+	if s.startCancel != nil {
+		logging.Infof("Cancelling virtual machine start...")
+		s.startCancel()
+	}
+	select {
+	case <-s.syncOperationDone:
+		return nil
+	case <-time.After(timeout):
+		return errors.New("cannot abort startup sequence quickly enough")
+	}
+}
+
+func (s *Synchronized) prepareStopDelete(state State) error {
+	s.stateLock.Lock()
+	defer s.stateLock.Unlock()
+
+	switch s.currentStateUnlocked() {
+	case Starting:
+		if err := s.cancelUnlocked(startCancelTimeout); err != nil {
+			return err
+		}
+	case Idle:
+		break
+	case Deleting, Stopping:
+		return errors.New("cluster is stopping or deleting")
+	default:
+		return errors.New("invalid condition")
+	}
+
+	s.currentState = state
+	return nil
+}
+
+func (s *Synchronized) Stop() (state.State, error) {
+	if err := s.prepareStopDelete(Stopping); err != nil {
+		return state.Error, err
+	}
+
+	st, err := s.underlying.Stop()
+	s.syncOperationDone <- Stopping
+
+	return st, err
+}
+
+func (s *Synchronized) GetName() string {
+	return s.underlying.GetName()
+}
+
+func (s *Synchronized) Exists() (bool, error) {
+	return s.underlying.Exists()
+}
+
+func (s *Synchronized) GetConsoleURL() (*ConsoleResult, error) {
+	return s.underlying.GetConsoleURL()
+}
+
+func (s *Synchronized) IP() (string, error) {
+	return s.underlying.IP()
+}
+
+func (s *Synchronized) PowerOff() error {
+	return s.underlying.PowerOff()
+}
+
+func (s *Synchronized) Status() (*ClusterStatusResult, error) {
+	return s.underlying.Status()
+}
+
+func (s *Synchronized) IsRunning() (bool, error) {
+	return s.underlying.IsRunning()
+}
