d21345fbcc3933fcce1150b392e777dca46a096f
Guillaume Rose <gurose@redhat.com>
2021-03-18 21:14:11 +0100

    Revert "Validate sudo access before using it with a real command"
    This reverts commit e95e583f6b7502393864f3d2c300ec37a24e9078.
    
    This doesn't work well with NOPASSWD configuration.

--- pkg/os/exec.go
+++ pkg/os/exec.go
@@ -48,9 +48,6 @@ func RunPrivileged(reason string, cmdAndArgs ...string) (string, string, error) 
 	if err != nil {
 		return "", "", errors.New("sudo executable not found")
 	}
-	if _, _, err := run(sudo, []string{"--validate"}, map[string]string{}); err != nil {
-		return "", "", errors.New("user is not allowed to use the 'sudo' command")
-	}
 	logging.Infof("Using root access: %s", reason)
 	return run(sudo, cmdAndArgs, map[string]string{})
 }
