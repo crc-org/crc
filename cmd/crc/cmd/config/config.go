crc-org/crc

commit b634197bd944fad9537b50d53c872b69de571f8e

Issue #2054 Ask to run setup, or cleanup and setup on 'autostart-tray' change
When 'autostart-tray' is changed to true ask user to run setup, when
'autostart-tray' is set to false ask user to run 'crc cleanup' followed
by 'crc setup' since only cleanup removes the created launchd plist
files

cmd/crc/cmd/config/config.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/code-ready/crc/pkg/crc/config"
 	"github.com/code-ready/crc/pkg/crc/constants"
-	"github.com/code-ready/crc/pkg/crc/errors"
 	"github.com/code-ready/crc/pkg/crc/network"
 	"github.com/code-ready/crc/pkg/crc/tray"
+	"github.com/spf13/cast"
 	"github.com/spf13/cobra"
 )
 
@@ -46,20 +46,16 @@ func RegisterSettings(cfg *config.Config) {
 	}
 
 	disableEnableTrayAutostart := func(key string, value interface{}) string {
-		mode := network.ParseMode(cfg.Get(NetworkMode).AsString())
-		var mErr = errors.MultiError{}
-		if err := tray.DisableTrayAutostart(); err != nil {
-			mErr.Collect(err)
+		if cast.ToBool(value) {
+			return fmt.Sprintf(
+				"Successfully configured '%s' to '%s'. Run 'crc setup' for it to take effect.",
+				key, cast.ToString(value),
+			)
 		}
-		if mode == network.DefaultMode {
-			if err := tray.DisableDaemonAutostart(); err != nil {
-				mErr.Collect(err)
-			}
-		}
-		if len(mErr.Errors) > 0 {
-			return fmt.Sprintf("Error occurred on disabling tray autostart: %s", mErr.Error())
-		}
-		return "Successfully disabled autostart of tray at login."
+		return fmt.Sprintf(
+			"Successfully configured '%s' to '%s'. Run 'crc cleanup' and then 'crc setup' for it to take effect.",
+			key, cast.ToString(value),
+		)
 	}
 
 	// Start command settings in config

pkg/crc/tray/util_darwin.go
@@ -2,21 +2,7 @@ package tray
 
 import (
 	"github.com/code-ready/crc/pkg/crc/config"
-	"github.com/code-ready/crc/pkg/os/launchd"
 )
-
-const (
-	trayAgentLabel   = "crc.tray"
-	daemonAgentLabel = "crc.daemon"
-)
-
-func DisableTrayAutostart() error {
-	return launchd.RemovePlist(trayAgentLabel)
-}
-
-func DisableDaemonAutostart() error {
-	return launchd.RemovePlist(daemonAgentLabel)
-}
 
 // ValidateTrayAutostart checks tray-auto-start is used in macOS and its a bool
 func ValidateTrayAutostart(value interface{}) (bool, string) {

pkg/crc/tray/util_linux.go
@@ -1,15 +1,5 @@
 package tray
 
-func DisableTrayAutostart() error {
-	// noop
-	return nil
-}
-
-func DisableDaemonAutostart() error {
-	// noop
-	return nil
-}
-
 // ValidateTrayAutostart checks tray-auto-start is used in macOS and its a bool
 func ValidateTrayAutostart(value interface{}) (bool, string) {
 	return false, "Not supported on Linux"

pkg/crc/tray/util_windows.go
@@ -1,15 +1,5 @@
 package tray
 
-func DisableTrayAutostart() error {
-	// noop
-	return nil
-}
-
-func DisableDaemonAutostart() error {
-	// noop
-	return nil
-}
-
 // ValidateTrayAutostart checks tray-auto-start is used in macOS and its a bool
 func ValidateTrayAutostart(value interface{}) (bool, string) {
 	return false, "Not supported on Windows"

commit ffab668232a48b98d978231d454262f6d77ed44b

Remove unused import
Merge of b634197bd944fad9537b50d53c872b69de571f8e broke the build.

cmd/crc/cmd/config/config.go
@@ -11,7 +11,6 @@ import (
 	"github.com/code-ready/crc/pkg/crc/config"
 	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/network"
-	"github.com/code-ready/crc/pkg/crc/tray"
 	"github.com/spf13/cast"
 	"github.com/spf13/cobra"
 )