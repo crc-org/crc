c6540c68e463622b45feab03ef61b2b2a30a7a75
Guillaume Rose <gurose@redhat.com>
2021-04-13 21:42:37 +0200

    integration: test `enable-cluster-monitoring` feature and collect data
    `oc adm top pods -A` output is saved to a file that can be archived and
    analyzed per test run.

---
+++ test/integration/monitoring_test.go
@@ -0,0 +1,34 @@
+package integration_test
+
+import (
+	"io/ioutil"
+	"os/exec"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("run openshift with monitoring stack", func() {
+	BeforeAll(cleanUp())
+
+	It("setup CRC", func() {
+		RunCRCExpectSuccess("config", "set", "memory", "16000")
+		RunCRCExpectSuccess("config", "set", "enable-cluster-monitoring", "true")
+		Expect(RunCRCExpectSuccess("setup")).To(ContainSubstring("Your system is correctly setup for using CodeReady Containers"))
+	})
+
+	It("start CRC", func() {
+		if bundlePath == "embedded" {
+			Expect(RunCRCExpectSuccess("start", "-p", pullSecretPath)).To(ContainSubstring("Started the OpenShift cluster"))
+		} else {
+			Expect(RunCRCExpectSuccess("start", "-b", bundlePath, "-p", pullSecretPath)).To(ContainSubstring("Started the OpenShift cluster"))
+		}
+	})
+
+	It("collect pods usage", func() {
+		stdout, _, err := Exec(exec.Command("oc", "adm", "top", "pods", "-A"), nil)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(ioutil.WriteFile(filepath.Join("out", "oc-adm-top-pods.txt"), []byte(stdout), 0600)).To(Succeed())
+	})
+})
