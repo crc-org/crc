3b2f45deddcd3e3302f60ae8a857c503012cb1cf
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-05-06 11:11:57 +0200

    Added initial test for installer on windows

--- test/extended/crc/ux/installer/installer_windows.go
+++ test/extended/crc/ux/installer/installer_windows.go
@@ -4,6 +4,7 @@ package installer
 
 import (
 	"fmt"
+	"time"
 
 	goautoit "github.com/adrianriobo/goautoit"
 )
@@ -27,10 +28,37 @@ func RequiredResourcesPath() (string, error) {
 
 func (a autoitHandler) Install() error {
 	command := fmt.Sprintf("msiexec.exe /i %s /qf", *a.InstallerPath)
-	installerPid := goautoit.RunWait(command)
+	installerPid := goautoit.Run(command)
+	fmt.Printf("After run\n")
 	if installerPid == 0 {
 		return fmt.Errorf("error starting the msi installer")
 	}
-	goautoit.MouseClick("Next")
+	fmt.Printf("Waiting for window\n")
+	// Calling Sleep method
+	// time.Sleep(8 * time.Second)
+
+	goautoit.WinWait("CodeReady Containers Setup")
+	fmt.Printf("After Waiting for window\n")
+	windowActive := goautoit.WinActive("CodeReady Containers Setup")
+	fmt.Printf("After run\n")
+	if windowActive == 0 {
+		return fmt.Errorf("error windows active 0")
+	}
+	fmt.Printf("Window is active\n")
+	// goautoit.ControlClick("CodeReady Containers Setup", "&Next", "Button1")
+	goautoit.ControlClick("CodeReady Containers Setup", "&Next", "Button1")
+	time.Sleep(2 * time.Second)
+	goautoit.ControlClick("CodeReady Containers Setup", "&accept", "Button1")
+	time.Sleep(2 * time.Second)
+	// goautoit.WinActive("CodeReady Containers Setup")
+	goautoit.ControlClick("CodeReady Containers Setup", "&Next", "Button3")
+	goautoit.ControlClick("CodeReady Containers Setup", "&Next", "Button1")
+	time.Sleep(2 * time.Second)
+	goautoit.ControlClick("", "&Install", "Button1")
+	time.Sleep(30 * time.Second)
+	goautoit.ControlClick("CodeReady Containers Setup", "&Finish", "Button1")
+	//I &accept the terms in the License Agreement
+	// fmt.Printf("After click\n")
+	// TODO which should be executed from a new cmd (to ensure ENVs)
 	return nil
 }
