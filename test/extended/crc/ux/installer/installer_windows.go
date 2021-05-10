b219477d71d0446616df67824f73942c11f34969
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-05-10 11:21:18 +0200

    Added happy path for windows installer

--- test/extended/crc/ux/installer/installer_windows.go
+++ test/extended/crc/ux/installer/installer_windows.go
@@ -9,6 +9,27 @@ import (
 	goautoit "github.com/adrianriobo/goautoit"
 )
 
+const (
+	installerWindowTitle string        = "CodeReady Containers Setup"
+	elementClickDelay    time.Duration = 2 * time.Second
+	installationTime     time.Duration = 30 * time.Second
+)
+
+type installerElement struct {
+	name   string
+	id     string
+	screen string
+}
+
+var (
+	welcomeNextButton         = installerElement{name: "Next", id: "1", screen: "welcome"}
+	licenseAcceptCheck        = installerElement{name: "accept", id: "1", screen: "license"}
+	licenseNextButton         = installerElement{name: "Next", id: "3", screen: "license"}
+	destinantionNextButton    = installerElement{name: "Next", id: "1", screen: "destination"}
+	installationInstallButton = installerElement{name: "Install", id: "1", screen: "installation"}
+	finalizationFinishButton  = installerElement{name: "Finish", id: "1", screen: "finalization"}
+)
+
 type autoitHandler struct {
 	CurrentUserPassword *string
 	InstallerPath       *string
@@ -27,36 +48,57 @@ func RequiredResourcesPath() (string, error) {
 }
 
 func (a autoitHandler) Install() error {
-	command := fmt.Sprintf("msiexec.exe /i %s /qf", *a.InstallerPath)
+	if err := runInstaller(*a.InstallerPath); err != nil {
+		return err
+	}
+	// Welcome screen
+	if err := clickButton(welcomeNextButton); err != nil {
+		return err
+	}
+	// License screen
+	if err := clickButton(licenseAcceptCheck); err != nil {
+		return err
+	}
+	if err := clickButton(licenseNextButton); err != nil {
+		return err
+	}
+	// Destination
+	if err := clickButton(destinantionNextButton); err != nil {
+		return err
+	}
+	// Installation
+	if err := clickButton(installationInstallButton); err != nil {
+		return err
+	}
+	// wait installation process
+	time.Sleep(installationTime)
+	// Finalization
+	if err := clickButton(finalizationFinishButton); err != nil {
+		return err
+	}
+	// TODO which should be executed from a new cmd (to ensure ENVs)
+	return nil
+}
+
+func runInstaller(installerPath string) error {
+	command := fmt.Sprintf("msiexec.exe /i %s /qf", installerPath)
 	installerPid := goautoit.Run(command)
-	fmt.Printf("After run\n")
 	if installerPid == 0 {
 		return fmt.Errorf("error starting the msi installer")
 	}
-	fmt.Printf("Waiting for window\n")
-	// Calling Sleep method
-	// time.Sleep(8 * time.Second)
+	goautoit.WinWait(installerWindowTitle)
+	return nil
+}
 
-	goautoit.WinWait("CodeReady Containers Setup")
-	fmt.Printf("After Waiting for window\n")
-	windowActive := goautoit.WinActive("CodeReady Containers Setup")
-	fmt.Printf("After run\n")
+func clickButton(element installerElement) error {
+	// Ensure the installer is the active window
+	windowActive := goautoit.WinActive(installerWindowTitle)
 	if windowActive == 0 {
-		return fmt.Errorf("error windows active 0")
+		return fmt.Errorf("error activating %s", installerWindowTitle)
 	}
-	fmt.Printf("Window is active\n")
-	// goautoit.ControlClick("CodeReady Containers Setup", "&Next", "Button1")
-	goautoit.ControlClick("CodeReady Containers Setup", "&Next", "Button1")
-	time.Sleep(2 * time.Second)
-	goautoit.ControlClick("CodeReady Containers Setup", "&accept", "Button1")
-	time.Sleep(2 * time.Second)
-	// goautoit.WinActive("CodeReady Containers Setup")
-	goautoit.ControlClick("CodeReady Containers Setup", "&Next", "Button3")
-	goautoit.ControlClick("CodeReady Containers Setup", "&Next", "Button1")
-	time.Sleep(2 * time.Second)
-	goautoit.ControlClick("", "&Install", "Button1")
-	time.Sleep(30 * time.Second)
-	goautoit.ControlClick("CodeReady Containers Setup", "&Finish", "Button1")
-	// TODO which should be executed from a new cmd (to ensure ENVs)
+	if exitCode := goautoit.ControlClick("", "&"+element.name, "Button"+element.id); exitCode == 0 {
+		return fmt.Errorf("error clicking on  %s button on screen %s", element.name, element.screen)
+	}
+	time.Sleep(elementClickDelay)
 	return nil
 }
