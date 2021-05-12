48c9c3c0274600dc8503152c10438e0d82ebe9a7
Guillaume Rose <gurose@redhat.com>
2021-05-12 17:07:55 +0200

    Push up addOpenShiftHosts in RunPostStart

--- pkg/crc/services/dns/dns_linux.go
+++ pkg/crc/services/dns/dns_linux.go
@@ -1,7 +1,5 @@
 package dns
 
 func runPostStartForOS(serviceConfig ServicePostStartConfig) error {
-	// We might need to set the firewall here to forward
-	// Update /etc/hosts file for host
-	return addOpenShiftHosts(serviceConfig)
+	return nil
 }
