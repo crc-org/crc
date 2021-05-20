a5c187d63ec2fac4da25947d026ec53fc3d8241d
Guillaume Rose <gurose@redhat.com>
2021-05-20 15:41:48 +0200

    Change `crc podman-env` to use the root Podman socket
    Add a flag to choose rootless Podman.

--- cmd/crc/cmd/podman_env.go
+++ cmd/crc/cmd/podman_env.go
@@ -8,6 +8,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	rootless bool
+)
+
 var podmanEnvCmd = &cobra.Command{
 	Use:   "podman-env",
 	Short: "Setup podman environment",
@@ -33,18 +37,24 @@ func runPodmanEnv() error {
 		return err
 	}
 
+	socket := "/run/podman/podman.sock"
+	if rootless {
+		socket = "/run/user/1000/podman/podman.sock"
+	}
 	fmt.Println(shell.GetPathEnvString(userShell, constants.CrcOcBinDir))
 	fmt.Println(shell.GetEnvString(userShell, "CONTAINER_SSHKEY", connectionDetails.SSHKeys[0]))
 	fmt.Println(shell.GetEnvString(userShell, "CONTAINER_HOST",
-		fmt.Sprintf("ssh://%s@%s:%d/run/user/1000/podman/podman.sock",
+		fmt.Sprintf("ssh://%s@%s:%d%s",
 			connectionDetails.SSHUsername,
 			connectionDetails.IP,
-			connectionDetails.SSHPort)))
+			connectionDetails.SSHPort,
+			socket)))
 	fmt.Println(shell.GenerateUsageHintWithComment(userShell, "crc podman-env"))
 	return nil
 }
 
 func init() {
-	rootCmd.AddCommand(podmanEnvCmd)
+	podmanEnvCmd.Flags().BoolVar(&rootless, "rootless", false, "Use rootless podman in the virtual machine")
 	podmanEnvCmd.Flags().StringVar(&forceShell, "shell", "", "Set the environment for the specified shell: [fish, cmd, powershell, tcsh, bash, zsh]. Default is auto-detect.")
+	rootCmd.AddCommand(podmanEnvCmd)
 }
