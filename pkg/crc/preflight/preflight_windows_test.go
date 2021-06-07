19171e86f65accffe7478f58f96f1223fbca5d30
Guillaume Rose <gurose@redhat.com>
2021-06-07 09:34:22 +0200

    Reboot only once
    Create crc-users group and the current user to it at the same moment
    as crc adds the user in the Hyper-V group.
    
    It keeps the Hyper-V config key `check-user-in-hyperv-group` for
    backward compatibility.

--- pkg/crc/preflight/preflight_windows_test.go
+++ pkg/crc/preflight/preflight_windows_test.go
@@ -11,13 +11,13 @@ import (
 func TestCountConfigurationOptions(t *testing.T) {
 	cfg := config.New(config.NewEmptyInMemoryStorage())
 	RegisterSettings(cfg)
-	assert.Len(t, cfg.AllConfigs(), 16)
+	assert.Len(t, cfg.AllConfigs(), 15)
 }
 
 func TestCountPreflights(t *testing.T) {
-	assert.Len(t, getPreflightChecks(false, false, network.SystemNetworkingMode), 21)
-	assert.Len(t, getPreflightChecks(true, true, network.SystemNetworkingMode), 21)
+	assert.Len(t, getPreflightChecks(false, false, network.SystemNetworkingMode), 20)
+	assert.Len(t, getPreflightChecks(true, true, network.SystemNetworkingMode), 20)
 
-	assert.Len(t, getPreflightChecks(false, false, network.UserNetworkingMode), 22)
-	assert.Len(t, getPreflightChecks(true, true, network.UserNetworkingMode), 22)
+	assert.Len(t, getPreflightChecks(false, false, network.UserNetworkingMode), 21)
+	assert.Len(t, getPreflightChecks(true, true, network.UserNetworkingMode), 21)
 }
