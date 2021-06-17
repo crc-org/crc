47c97acb7685b5fa53eaecb6777b1505124b804d
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-06-17 14:11:42 +0200

    ux testing expected messages fix syntax

--- test/extended/crc/ux/notification/const.go
+++ test/extended/crc/ux/notification/const.go
@@ -4,7 +4,7 @@
 package notification
 
 const (
-	startMessage  string = "OpenShift Cluster is running"
+	startMessage  string = "OpenShift cluster is running"
 	stopMessage   string = "The OpenShift Cluster was successfully stopped"
 	deleteMessage string = "The OpenShift Cluster is successfully deleted"
 	// copyCommandMessage string = "OC Login command copied to clipboard, go ahead and login to your cluster"
