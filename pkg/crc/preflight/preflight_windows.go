crc-org/crc

commit f548d92c12351f1c2e604fb1e7c57dad9f35d109

Issue #2499 Add error message to crc-users group preflight checks

pkg/crc/preflight/preflight_windows.go
@@ -57,16 +57,16 @@ var hypervPreflightChecks = []Check{
 		checkDescription: "Checking if crc-users group exists",
 		check: func() error {
 			_, _, err := powershell.Execute("Get-LocalGroup -Name crc-users")
-			return err
+			return fmt.Errorf("'crc-users' group does not exist: %v", err)
 		},
 		fixDescription: "Creating crc-users group",
 		fix: func() error {
 			_, _, err := powershell.ExecuteAsAdmin("create crc-users group", "New-LocalGroup -Name crc-users")
-			return err
+			return fmt.Errorf("failed to create 'crc-users' group: %v", err)
 		},
 		cleanup: func() error {
 			_, _, err := powershell.ExecuteAsAdmin("remove crc-users group", "Remove-LocalGroup -Name crc-users")
-			return err
+			return fmt.Errorf("failed to remove 'crc-users' group: %v", err)
 		},
 		labels: labels{Os: Windows},
 	},

commit c5fab7cdc9c82cf3f067afb5e1d6a6792a2c77ed

Issue #2499 Check if err is nil before returning
commit f548d92c introduced a bug where an error was
always returned even if the actual returned err was
nil, this checks that err is not nil

pkg/crc/preflight/preflight_windows.go
@@ -56,18 +56,24 @@ var hypervPreflightChecks = []Check{
 		configKeySuffix:  "check-crc-users-group-exists",
 		checkDescription: "Checking if crc-users group exists",
 		check: func() error {
-			_, _, err := powershell.Execute("Get-LocalGroup -Name crc-users")
-			return fmt.Errorf("'crc-users' group does not exist: %v", err)
+			if _, _, err := powershell.Execute("Get-LocalGroup -Name crc-users"); err != nil {
+				return fmt.Errorf("'crc-users' group does not exist: %v", err)
+			}
+			return nil
 		},
 		fixDescription: "Creating crc-users group",
 		fix: func() error {
-			_, _, err := powershell.ExecuteAsAdmin("create crc-users group", "New-LocalGroup -Name crc-users")
-			return fmt.Errorf("failed to create 'crc-users' group: %v", err)
+			if _, _, err := powershell.ExecuteAsAdmin("create crc-users group", "New-LocalGroup -Name crc-users"); err != nil {
+				return fmt.Errorf("failed to create 'crc-users' group: %v", err)
+			}
+			return nil
 		},
 		cleanupDescription: "Removing 'crc-users' group",
 		cleanup: func() error {
-			_, _, err := powershell.ExecuteAsAdmin("remove crc-users group", "Remove-LocalGroup -Name crc-users")
-			return fmt.Errorf("failed to remove 'crc-users' group: %v", err)
+			if _, _, err := powershell.ExecuteAsAdmin("remove crc-users group", "Remove-LocalGroup -Name crc-users"); err != nil {
+				return fmt.Errorf("failed to remove 'crc-users' group: %v", err)
+			}
+			return nil
 		},
 		labels: labels{Os: Windows},
 	},