fb92b114ca863a7e10e0520874071b50db6204d2
Christophe Fergeau <cfergeau@redhat.com>
2021-07-07 16:55:45 +0200

    constants: Use os.UserHomeDir()
    constants.GetHomeDir() is duplicating what os.UserHomeDir() provides.
    One difference is that it favours HOMEDRIVE/HOMEPATH over USERPROFILE,
    but these are deprecated, see
    https://github.com/mitchellh/go-homedir/issues/23 for example.

--- pkg/crc/constants/constants.go
+++ pkg/crc/constants/constants.go
@@ -128,20 +128,9 @@ func BinDir() string {
 
 // GetHomeDir returns the home directory for the current user
 func GetHomeDir() string {
-	if runtime.GOOS == "windows" {
-		if homeDrive, homePath := os.Getenv("HOMEDRIVE"), os.Getenv("HOMEPATH"); len(homeDrive) > 0 && len(homePath) > 0 {
-			homeDir := filepath.Join(homeDrive, homePath)
-			if _, err := os.Stat(homeDir); err == nil {
-				return homeDir
-			}
-		}
-		if userProfile := os.Getenv("USERPROFILE"); len(userProfile) > 0 {
-			if _, err := os.Stat(userProfile); err == nil {
-				return userProfile
-			}
-		}
-	}
-	return os.Getenv("HOME")
+	homeDir, _ := os.UserHomeDir()
+
+	return homeDir
 }
 
 // EnsureBaseDirectoryExists create the ~/.crc directory if it is not present
