082c32e7b2eaf5ee53dcec22b95e44c8a02cefdd
Guillaume Rose <gurose@redhat.com>
2021-07-13 16:46:26 +0200

    api: remove binary protocol server

--- pkg/crc/api/types.go
+++ pkg/crc/api/types.go
@@ -1,21 +1,5 @@
 package api
 
-import (
-	"encoding/json"
-	"net"
-)
-
-type Server struct {
-	handler  *Handler
-	listener net.Listener
-}
-
-// commandRequest struct is used to decode the json request from tray
-type commandRequest struct {
-	Command string          `json:"command"`
-	Args    json.RawMessage `json:"args,omitempty"`
-}
-
 type loggerResult struct {
 	Success  bool
 	Messages []string
