3d076828454b9b3c202a04dc1a8909613193728a
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2021-07-16 10:35:28 +0530

    Pull secret should be added as soon as apiserver is responding
    Right now we are delay pull secret addition with proxy and SSHKey,
    better to have it just after apiserver start responding.

--- pkg/crc/machine/start.go
+++ pkg/crc/machine/start.go
@@ -390,16 +390,16 @@ func (client *client) Start(ctx context.Context, startConfig types.StartConfig) 
 		return nil, errors.Wrap(err, "Error waiting for apiserver")
 	}
 
+	if err := cluster.EnsurePullSecretPresentInTheCluster(ocConfig, startConfig.PullSecret); err != nil {
+		return nil, errors.Wrap(err, "Failed to update cluster pull secret")
+	}
+
 	if err := cluster.EnsureSSHKeyPresentInTheCluster(ocConfig, constants.GetPublicKeyPath()); err != nil {
 		return nil, errors.Wrap(err, "Failed to update ssh public key to machine config")
 	}
 
 	if err := ensureProxyIsConfiguredInOpenShift(ocConfig, sshRunner, proxyConfig, instanceIP); err != nil {
 		return nil, errors.Wrap(err, "Failed to update cluster proxy configuration")
-	}
-
-	if err := cluster.EnsurePullSecretPresentInTheCluster(ocConfig, startConfig.PullSecret); err != nil {
-		return nil, errors.Wrap(err, "Failed to update cluster pull secret")
 	}
 
 	if err := cluster.UpdateKubeAdminUserPassword(ocConfig, startConfig.KubeAdminPassword); err != nil {
