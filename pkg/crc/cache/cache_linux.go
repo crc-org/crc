54f8c379e554559204fcf0b45e8ac61f3f962014
Christophe Fergeau <cfergeau@redhat.com>
2021-08-17 16:19:27 +0200

    cache: Remove 'destDir' from New() method
    All users set it to `constants.CrcBinDir`, so we can hardcode it to this
    value instead of having all callers pass the same value.

--- pkg/crc/cache/cache_linux.go
+++ pkg/crc/cache/cache_linux.go
@@ -1,12 +1,11 @@
 package cache
 
 import (
-	"github.com/code-ready/crc/pkg/crc/constants"
 	"github.com/code-ready/crc/pkg/crc/machine/libvirt"
 )
 
 func NewMachineDriverLibvirtCache() *Cache {
-	return New(libvirt.MachineDriverCommand, libvirt.MachineDriverDownloadURL, constants.CrcBinDir, libvirt.MachineDriverVersion, getCurrentLibvirtDriverVersion)
+	return New(libvirt.MachineDriverCommand, libvirt.MachineDriverDownloadURL, libvirt.MachineDriverVersion, getCurrentLibvirtDriverVersion)
 }
 
 func getCurrentLibvirtDriverVersion(executablePath string) (string, error) {
