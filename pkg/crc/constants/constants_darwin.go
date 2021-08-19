7c23452bda09a571cf26a2b861bd6ea1b94276e9
Christophe Fergeau <cfergeau@redhat.com>
2021-08-19 16:45:30 +0200

    macos: Rework construction of TrayExecutablePath
    The way TrayExecutablePath is currently initialized is dependent on
    whether version.GetMacosInstallPath() has a trailing '/' or not:
    filepath.Dir("/foo/bar/") returns "/foo/bar" while
    filepath.Dir("/foo/bar") returns "/foo".
    
    This can be unexpected if we ever change version.GetMacosInstallPath()
    implementation. Using filepath.Clean() before calling filepath.Dir()
    would take care of the potential trailing slash, but if we use
    filepath.Clean(), we can as well replace the calls to filepath.Dir()
    with ".." and let filepath.Clean() clean this up.

--- pkg/crc/constants/constants_darwin.go
+++ pkg/crc/constants/constants_darwin.go
@@ -12,7 +12,7 @@ const (
 )
 
 var (
-	TrayExecutablePath   = filepath.Join(filepath.Dir(filepath.Dir(version.GetMacosInstallPath())), "MacOS", "CodeReady Containers")
+	TrayExecutablePath   = filepath.Clean(filepath.Join(version.GetMacosInstallPath(), "..", "MacOS", "CodeReady Containers"))
 	TapSocketPath        = filepath.Join(CrcBaseDir, "tap.sock")
 	DaemonHTTPSocketPath = filepath.Join(CrcBaseDir, "crc-http.sock")
 )
