1eb6ca0e9da14bc6d9cc4075f70a15894f2de8d7
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2021-09-30 12:14:26 +0200

    e2e: notification waiting timeout reduction

--- test/extended/crc/ux/notification/const.go
+++ test/extended/crc/ux/notification/const.go
@@ -3,6 +3,6 @@ package notification
 const (
 	// copyCommandMessage string = "OC Login command copied to clipboard, go ahead and login to your cluster"
 
-	notificationWaitTimeout int = 200
+	notificationWaitTimeout int = 50
 	notificationWaitRetries int = 10
 )
