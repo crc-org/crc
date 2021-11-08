33bdc6940a6aa12539cf5ed67b5510654934ef45
Christophe Fergeau <cfergeau@redhat.com>
2021-11-08 17:18:47 +0100

    bundle: Use hardcoded URL for bundle download info
    The URL is associated with a sha256sum, so it's better if the URL does
    not change because other parts of the code were updated, and
    version.GetBundleVersion() no longer returns the same value as when the
    sha256 sum was added.

--- pkg/crc/machine/bundle/constants.go
+++ pkg/crc/machine/bundle/constants.go
@@ -1,10 +1,6 @@
 package bundle
 
 import (
-	"fmt"
-
-	"github.com/code-ready/crc/pkg/crc/constants"
-	"github.com/code-ready/crc/pkg/crc/version"
 	"github.com/code-ready/crc/pkg/download"
 )
 
@@ -12,7 +8,7 @@ var bundleLocations = map[string]bundlesDownloadInfo{
 	"amd64": {
 		"darwin": download.NewRemoteFile("", ""),
 		"linux": download.NewRemoteFile(
-			fmt.Sprintf("https://storage.googleapis.com/crc-bundle-github-ci/%s/%s", version.GetBundleVersion(), constants.GetDefaultBundle()),
+			"https://storage.googleapis.com/crc-bundle-github-ci/4.9.5/crc_libvirt_4.9.5.crcbundle",
 			"603d87599f4c129683d3a8a11bcd41f240a529b2dd81075755eebf3d2b8cacc6",
 		),
 		"windows": download.NewRemoteFile("", ""),
