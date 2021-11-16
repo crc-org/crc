7707f552fa4066dbc41d1b933e370a5444dbfa83
Christophe Fergeau <cfergeau@redhat.com>
2021-11-16 11:31:51 +0100

    config: Rework validatePreset
    Since we have a 'preset' package, it's better to have a string ->
    Preset conversion helper in it rather than doing it in config.
    This also moves validatePreset to validation.go with the other
    validation functions.

--- pkg/crc/preset/preset.go
+++ pkg/crc/preset/preset.go
@@ -1,8 +1,33 @@
 package preset
 
+import (
+	"fmt"
+
+	"github.com/code-ready/crc/pkg/crc/logging"
+)
+
 type Preset string
 
 const (
 	Podman    Preset = "podman"
 	OpenShift Preset = "openshift"
 )
+
+func ParsePresetE(input string) (Preset, error) {
+	switch input {
+	case string(Podman):
+		return Podman, nil
+	case string(OpenShift):
+		return OpenShift, nil
+	default:
+		return OpenShift, fmt.Errorf("Cannot parse preset '%s'", input)
+	}
+}
+func ParsePreset(input string) Preset {
+	preset, err := ParsePresetE(input)
+	if err != nil {
+		logging.Errorf("unexpected preset mode %s, using default", input)
+		return OpenShift
+	}
+	return preset
+}
