3cf0d68b084f0ad5d361980d67d0d2df5bdb4336
Christophe Fergeau <cfergeau@redhat.com>
2021-11-24 10:26:58 +0100

    constants: windows: Remove unused constants
    They are no longer used since the removal of the tray preflight checks.

--- pkg/crc/constants/constants_windows.go
+++ pkg/crc/constants/constants_windows.go
@@ -1,18 +1,8 @@
 package constants
 
-import (
-	"path/filepath"
-)
-
 const (
 	OcExecutableName           = "oc.exe"
 	PodmanRemoteExecutableName = "podman.exe"
-	TrayExecutableName         = "crc-tray.exe"
-	TrayShortcutName           = "crc-tray.lnk"
 	TapSocketPath              = ""
 	DaemonHTTPNamedPipe        = `\\.\pipe\crc-http`
-)
-
-var (
-	TrayExecutablePath = filepath.Join(BinDir(), TrayExecutableName)
 )
