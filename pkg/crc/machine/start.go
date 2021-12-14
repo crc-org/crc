8cddeefa1b7ad4fbe3fecace409525513a2e4430
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2021-12-14 15:59:38 +0530

    [Start] Don't generate kubeadmin password for podman preset
    This patch will avoid generating the kubeadmin password in case of
    podman preset is used.

--- pkg/crc/machine/start.go
+++ pkg/crc/machine/start.go
@@ -186,7 +186,7 @@ func (client *client) Start(ctx context.Context, startConfig types.StartConfig) 
 		if crcBundleMetadata.IsOpenShift() {
 			machineConfig.KubeConfig = crcBundleMetadata.GetKubeConfigPath()
 		}
-		if err := createHost(machineConfig); err != nil {
+		if err := createHost(machineConfig, crcBundleMetadata); err != nil {
 			return nil, errors.Wrap(err, "Error creating machine")
 		}
 	} else {
@@ -516,7 +516,7 @@ func (client *client) validateStartConfig(startConfig types.StartConfig) error {
 	return nil
 }
 
-func createHost(machineConfig config.MachineConfig) error {
+func createHost(machineConfig config.MachineConfig, crcBundleInfo *bundle.CrcBundleInfo) error {
 	api, cleanup := createLibMachineClient()
 	defer cleanup()
 
@@ -545,8 +545,10 @@ func createHost(machineConfig config.MachineConfig) error {
 	if err := crcssh.GenerateSSHKey(constants.GetPrivateKeyPath()); err != nil {
 		return fmt.Errorf("Error generating ssh key pair: %v", err)
 	}
-	if err := cluster.GenerateKubeAdminUserPassword(); err != nil {
-		return errors.Wrap(err, "Error generating new kubeadmin password")
+	if crcBundleInfo.IsOpenShift() {
+		if err := cluster.GenerateKubeAdminUserPassword(); err != nil {
+			return errors.Wrap(err, "Error generating new kubeadmin password")
+		}
 	}
 	if err := api.SetExists(vm.Name); err != nil {
 		return fmt.Errorf("Failed to record VM existence: %s", err)
