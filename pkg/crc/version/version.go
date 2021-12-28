9804a791a61132ab1f014402c7a5c1c5b4fca167
praveenkumar <kumarpraveen.nitdgp@gmail.com>
2021-12-28 14:07:45 +0530

    Embed electron based mac tray to installer

--- pkg/crc/version/version.go
+++ pkg/crc/version/version.go
@@ -43,7 +43,7 @@ var (
 const (
 	releaseInfoLink = "https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/crc/latest/release-info.json"
 	// Tray version to be embedded in executable
-	crcMacTrayVersion = "1.0.12"
+	crcMacTrayVersion = "0.9.0"
 	// Windows forms application version type major.minor.buildnumber.revesion
 	crcWindowsTrayVersion = "0.11.0.0"
 )
