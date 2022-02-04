ff5d17656d1677301b42b78297e18e98f4a4618a
praveenkumar <kumarpraveen.nitdgp@gmail.com>
2022-02-04 14:13:07 +0530

    Version: Remove bundle path detail from version command
    Now bundle is decoupled from binary so it doesn't make sense to
    show bundle path or tell user `bundle is not embedded`. This PR
    remove that detail from version command.
    
    ```
    $ crc version
    CodeReady Containers version: 1.99.1+4381fb9c
    OpenShift version: 4.9.12
    Podman version: 3.4.4
    ```

--- cmd/crc/cmd/version_test.go
+++ cmd/crc/cmd/version_test.go
@@ -10,14 +10,13 @@ import (
 func TestPlainVersion(t *testing.T) {
 	out := new(bytes.Buffer)
 	assert.NoError(t, runPrintVersion(out, &version{
-		Version:             "1.13",
-		Commit:              "aabbcc",
-		OpenshiftVersion:    "4.5.4",
-		PodmanVersion:       "3.4.4",
-		InstalledBundlePath: "",
+		Version:          "1.13",
+		Commit:           "aabbcc",
+		OpenshiftVersion: "4.5.4",
+		PodmanVersion:    "3.4.4",
 	}, ""))
 	assert.Equal(t, `CodeReady Containers version: 1.13+aabbcc
-OpenShift version: 4.5.4 (not embedded in executable)
+OpenShift version: 4.5.4
 Podman version: 3.4.4
 `, out.String())
 }
@@ -25,11 +24,10 @@ Podman version: 3.4.4
 func TestJsonVersion(t *testing.T) {
 	out := new(bytes.Buffer)
 	assert.NoError(t, runPrintVersion(out, &version{
-		Version:             "1.13",
-		Commit:              "aabbcc",
-		OpenshiftVersion:    "4.5.4",
-		PodmanVersion:       "3.4.4",
-		InstalledBundlePath: "",
+		Version:          "1.13",
+		Commit:           "aabbcc",
+		OpenshiftVersion: "4.5.4",
+		PodmanVersion:    "3.4.4",
 	}, "json"))
 
 	expected := `{"version": "1.13", "commit": "aabbcc", "openshiftVersion": "4.5.4", "podmanVersion": "3.4.4"}`
