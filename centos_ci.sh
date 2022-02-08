bff666f740cc2bba0e12fe3add4b4bac4ca877d0
praveenkumar <kumarpraveen.nitdgp@gmail.com>
2022-02-08 11:51:27 +0530

    update bundle to 4.9.15

--- centos_ci.sh
+++ centos_ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # bundle location
-BUNDLE_VERSION=4.9.12
+BUNDLE_VERSION=4.9.15
 BUNDLE=crc_libvirt_$BUNDLE_VERSION.crcbundle
 GO_VERSION=1.16.10
 
--- pkg/crc/machine/bundle/constants.go
+++ pkg/crc/machine/bundle/constants.go
@@ -8,23 +8,23 @@ import (
 var bundleLocations = map[string]bundlesDownloadInfo{
 	"amd64": {
 		"darwin": {
-			preset.OpenShift: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/4.9.12/crc_hyperkit_4.9.12.crcbundle",
-				"73167e735337b24efa18654667d8bff7cb2b7eb2d0bd2c6ffae4f1df4d8f6ced"),
+			preset.OpenShift: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/4.9.15/crc_hyperkit_4.9.15.crcbundle",
+				"9e90bcbce5837cfc7c13f94d269fe043ba38c3228b57ba4748cc52d47ededf86"),
 			preset.Podman: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/3.4.4/crc_podman_hyperkit_3.4.4.crcbundle",
 				"c413375524a774149cca563b523f6316fa3af2a0a669a923cce3e707ff5251c7"),
 		},
 
 		"linux": {
 			preset.OpenShift: download.NewRemoteFile(
-				"https://storage.googleapis.com/crc-bundle-github-ci/4.9.12/crc_libvirt_4.9.12.crcbundle",
-				"715fa19aebb36a1b3817554f9b8f6f67a9b5c53045e28b9716388186da2dc03b",
+				"https://storage.googleapis.com/crc-bundle-github-ci/4.9.15/crc_libvirt_4.9.15.crcbundle",
+				"c0af00cbb4eee11913606382dbd5355bd526b850921d5ce8b5be9625b0f2b364",
 			),
 			preset.Podman: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/3.4.4/crc_podman_libvirt_3.4.4.crcbundle",
 				"7df37d2292dfe29d58d9dbcbdaabe6b9cc64f339bc370a88169682e1ba16c191"),
 		},
 		"windows": {
-			preset.OpenShift: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/4.9.12/crc_hyperv_4.9.12.crcbundle",
-				"d5dd89ee500dc1ef9d5af2059f8fd24dc55d07dd5463b57d8de45f29702b9e28"),
+			preset.OpenShift: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/4.9.15/crc_hyperv_4.9.15.crcbundle",
+				"67dee61a3de54e7c5e35a67fa1552a4a38e867459d67c716556be920cd78114e"),
 			preset.Podman: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/3.4.4/crc_podman_hyperv_3.4.4.crcbundle",
 				"fec1dd6f57cb0ffd74da28648438925d49f517e53088f5c7f13b96a88c7130f1"),
 		},
