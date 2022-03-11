406705293e3bb2718b12cb9fcc6d3b5e56615538
praveenkumar <kumarpraveen.nitdgp@gmail.com>
2022-03-11 20:21:35 +0530

    Update bundle links to 4.10.3 and 3.4.4 podman (mirror.openshift)

--- pkg/crc/machine/bundle/constants.go
+++ pkg/crc/machine/bundle/constants.go
@@ -8,23 +8,23 @@ import (
 var bundleLocations = map[string]bundlesDownloadInfo{
 	"amd64": {
 		"darwin": {
-			preset.OpenShift: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/4.9.18/crc_hyperkit_4.9.18.crcbundle",
-				"d354e7c9bfb88abfb822151bf721d1df45a83b4ac0330f3f5c2bb930466ae86d"),
-			preset.Podman: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/3.4.4-1/crc_podman_hyperkit_3.4.4_amd64.crcbundle",
-				"1195daa0bccd6293609673f424c788b7ad153a44829d55168460370fb88920e1"),
+			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.10.3/crc_hyperkit_4.10.3_amd64.crcbundle",
+				"57c8adae49beeb83d7a180aaba962256c2833863e1f59492295841a3e6e3f016"),
+			preset.Podman: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/podman/3.4.4/crc_podman_hyperkit_3.4.4_amd64.crcbundle",
+				"12c0b610e8e3a0d446ac106e2b6f4345ee6fc45304ed05d529933a1261a6dd04"),
 		},
 
 		"linux": {
-			preset.OpenShift: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/4.9.18/crc_libvirt_4.9.18.crcbundle",
-				"120d435caad8349c7d7fa5f836f982efc0c0433fc18ccf0247cd0f30b63f66b9"),
-			preset.Podman: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/3.4.4-1/crc_podman_libvirt_3.4.4_amd64.crcbundle",
-				"7db6a63da95a8a9e336fb77f35d63f336c67930b26e7dbdad04a1b214298c511"),
+			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.10.3/crc_libvirt_4.10.3_amd64.crcbundle",
+				"d15f0171de51f5fe0e15e30e52ea6c03a785d7e70eed484384e83f2bf86d919d"),
+			preset.Podman: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/podman/3.4.4/crc_podman_libvirt_3.4.4_amd64.crcbundle",
+				"8e8d150dfbefcec93639df53e6181377d1bfc0df3c8719a6fedf0929779f6f63"),
 		},
 		"windows": {
-			preset.OpenShift: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/4.9.18/crc_hyperv_4.9.18.crcbundle",
-				"009b2e33ec795a6cdb47083425966f37f22f3d37790fedc7fdb927f52cbfb94c"),
-			preset.Podman: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/3.4.4-1/crc_podman_hyperv_3.4.4_amd64.crcbundle",
-				"21329103dfe706b1c772b9c9ff2f78888d2e8783a5e9138f46fc78efa6a4da00"),
+			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.10.3/crc_hyperv_4.10.3_amd64.crcbundle",
+				"6361a803c97fa67d702056caf0e52151522c6186ad6048354f4f512b95fef9f8"),
+			preset.Podman: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/podman/3.4.4/crc_podman_hyperv_3.4.4_amd64.crcbundle",
+				"e2a55c818b8d2f071f4cc0d26aba11f0a852aebdb7588a08eb96bd41faea0ef4"),
 		},
 	},
 }
