889b132a304b3178659df80ed6d3674b474bbe0c
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2022-03-31 13:29:29 +0530

    Windows: Check if daemon process is already running
    Without this PR following scenario happen
    - User installed the crc first time and do the setup
    - As part of the setup crc daemon is started by task schedular
    - User reboot
    - Tray autostart with reboot but `crc setup --check-only` fails
    because daemon task is not running.
    
    With this PR, check for daemon task running should first validate
    if an existing process of `crc daemon` is running and the version
    it get from the api should be same as the binary then task running
    check succeed. So this PR will makes `crc setup --check-only` work
    again with tray autostart.

--- pkg/crc/preflight/preflight_daemon_task_check_windows.go
+++ pkg/crc/preflight/preflight_daemon_task_check_windows.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/code-ready/crc/pkg/crc/constants"
+	"github.com/code-ready/crc/pkg/crc/daemonclient"
 	"github.com/code-ready/crc/pkg/crc/logging"
 	"github.com/code-ready/crc/pkg/crc/version"
 	"github.com/code-ready/crc/pkg/os/windows/powershell"
@@ -111,6 +112,9 @@ func removeDaemonTask() error {
 }
 
 func checkIfDaemonTaskRunning() error {
+	if isDaemonRunningWithReleasedVersion() {
+		return nil
+	}
 	stdout, stderr, err := powershell.Execute(fmt.Sprintf(`(Get-ScheduledTask -TaskName "%s").State`, constants.DaemonTaskName))
 	if err != nil {
 		logging.Debugf("%s task is not running: %v : %s", constants.DaemonTaskName, err, stderr)
@@ -140,4 +144,16 @@ func checkIfOlderTask() error {
 		return fmt.Errorf("expected %s task to be on version '%s' but got '%s'", constants.DaemonTaskName, version.GetCRCVersion(), stdout)
 	}
 	return nil
+}
+
+func isDaemonRunningWithReleasedVersion() bool {
+	ver, err := daemonclient.New().APIClient.Version()
+	if err != nil {
+		return false
+	}
+	if ver.CrcVersion != version.GetCRCVersion() {
+		logging.Debugf("Daemon is running with %s version but binary version is %s", ver.CrcVersion, version.GetCRCVersion())
+		return false
+	}
+	return true
 }
