2439887bcdee5f94c71838b08db53b28acd643f3
Jakub Sliacan <jsliacan@redhat.com>
2022-04-22 16:41:10 +0200

    Follow-up to PR#3136: Change CodeReady Containers to CRC in extended tests folder

--- test/extended/crc/ux/tray/tray_windows.go
+++ test/extended/crc/ux/tray/tray_windows.go
@@ -25,7 +25,7 @@ type gowinxHandler struct {
 const (
 	trayAssemblyName string = "crc-tray.exe"
 
-	notificationIcon string = "CodeReady Containers"
+	notificationIcon string = "CRC"
 	contextMenu      string = "menu"
 	loginMenu        string = "loginMenu"
 
