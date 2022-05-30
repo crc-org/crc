078e44625cdad01c64d73091c8d71627646e48f8
Anjan Nath <kaludios@gmail.com>
2022-05-30 12:30:06 +0530

    add function to get the version from http request
    GetVersion returns the request version based on the request's header
    it tries to read the version from:
     - "Accept" header, i.e Accept: "application/json; version=1.0"
     - "Accept-Version" header, i.e Accept-Version: "1.0"
    
    the code for GetVersion is copied from: github.com/kataras/versioning
    with minor modification

--- pkg/crc/api/helpers.go
+++ pkg/crc/api/helpers.go
@@ -5,9 +5,22 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 	"sync"
 
 	"github.com/code-ready/crc/pkg/crc/logging"
+)
+
+
+const (
+	// AcceptVersionHeaderKey is the header key of "Accept-Version".
+	AcceptVersionHeaderKey = "Accept-Version"
+	// AcceptHeaderKey is the header key of "Accept".
+	AcceptHeaderKey = "Accept"
+	// AcceptHeaderVersionValue is the Accept's header value search term the requested version.
+	AcceptHeaderVersionValue = "version"
+
+	NotFound = "notfound"
 )
 
 type context struct {
@@ -123,4 +136,44 @@ func (s *server) Handler() http.Handler {
 			logging.Error("Failed to send response: ", err)
 		}
 	})
+}
+
+// GetVersion returns the current request version.
+//
+// By default `GetVersion` will try to read from:
+// - "Accept" header, i.e Accept: "application/json; version=1.0"
+// - "Accept-Version" header, i.e Accept-Version: "1.0"
+func GetVersion(r *http.Request) string {
+	// firstly by the "Accept-Version" header.
+	if version := r.Header.Get(AcceptVersionHeaderKey); version != "" {
+		return version
+	}
+
+	// secondly by the "Accept" header which is like "...; version=1.0"
+	acceptValue := r.Header.Get(AcceptHeaderKey)
+	if acceptValue != "" {
+		if idx := strings.Index(acceptValue, AcceptHeaderVersionValue); idx != -1 {
+			rem := acceptValue[idx:]
+			startVersion := strings.Index(rem, "=")
+			if startVersion == -1 || len(rem) < startVersion+1 {
+				return NotFound
+			}
+
+			rem = rem[startVersion+1:]
+
+			end := strings.Index(rem, " ")
+			if end == -1 {
+				end = strings.Index(rem, ";")
+			}
+			if end == -1 {
+				end = len(rem)
+			}
+
+			if version := rem[:end]; version != "" {
+				return version
+			}
+		}
+	}
+
+	return NotFound
 }
