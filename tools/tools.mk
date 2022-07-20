cfe50548139200d914eb4b2ee8209a87437f77bf
Christophe Fergeau <cfergeau@redhat.com>
2022-07-20 10:10:48 +0200

    build: Move tools building to a sub Makefile in tools/
    This keeps everything related to build tools in the same place, and will
    ease sharing with other projects.

---
+++ tools/tools.mk
@@ -0,0 +1,10 @@
+TOOLS_BINDIR = $(realpath $(TOOLS_DIR)/bin)
+
+$(TOOLS_BINDIR)/makefat: $(TOOLS_DIR)/go.mod
+	cd $(TOOLS_DIR) && GOBIN=$(TOOLS_BINDIR) go install github.com/randall77/makefat
+
+$(TOOLS_BINDIR)/golangci-lint: $(TOOLS_DIR)/go.mod
+	cd $(TOOLS_DIR) && GOBIN=$(TOOLS_BINDIR) go install github.com/golangci/golangci-lint/cmd/golangci-lint
+
+$(TOOLS_BINDIR)/gomod2rpmdeps: $(TOOLS_DIR)/go.mod
+	cd $(TOOLS_DIR) && GOBIN=$(TOOLS_BINDIR) go install github.com/cfergeau/gomod2rpmdeps/cmd/gomod2rpmdeps
