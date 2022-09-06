01debf63d4cfb6ef41583bd3588f9410840000c2
Anjan Nath <kaludios@gmail.com>
2022-09-07 00:01:58 +0530

    Add storage backend for configs based on OS provided secret store
    this implements the RawStorage interface by using the OS provided
    keyring/secret store as the persistent store of config settings
    
    currently config settings are peristed to disk by writing to a JSON
    file in ~/.crc/crc.json this becomes an issue if we want to have  a
    config setting that stores a password or anyother secret as we dont
    want it to get written to disk unencrypted
    
    creating an instance of Config with SecretStore as the storage  the
    the settings/secrets are written to the OS keyring instead

---
+++ pkg/crc/config/secret_config.go
@@ -0,0 +1,69 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/code-ready/crc/pkg/crc/logging"
+	"github.com/spf13/cast"
+	"github.com/zalando/go-keyring"
+)
+
+const secretServiceName = "crc"
+
+var ErrSecretsNotAccessible = errors.New("secret store is not accessible")
+
+type SecretStorage struct {
+	secretService   string
+	storeAccessible bool
+}
+
+func NewSecretStorage() *SecretStorage {
+	return &SecretStorage{
+		secretService:   secretServiceName,
+		storeAccessible: keyringAccessible(),
+	}
+}
+
+func (c *SecretStorage) Get(key string) interface{} {
+	if !c.storeAccessible {
+		return nil
+	}
+	secret, err := keyring.Get(c.secretService, key)
+	if errors.Is(err, keyring.ErrNotFound) {
+		return nil
+	}
+	return secret
+}
+
+func (c *SecretStorage) Set(key string, value interface{}) error {
+	if !c.storeAccessible {
+		return ErrSecretsNotAccessible
+	}
+	secret, err := cast.ToStringE(value)
+	if err != nil {
+		return fmt.Errorf("Failed to cast secret value to string: %w", err)
+	}
+	return keyring.Set(c.secretService, key, secret)
+}
+
+func (c *SecretStorage) Unset(key string) error {
+	if !c.storeAccessible {
+		return ErrSecretsNotAccessible
+	}
+	err := keyring.Delete(c.secretService, key)
+	if errors.Is(err, keyring.ErrNotFound) {
+		return nil
+	}
+	return err
+}
+
+func keyringAccessible() bool {
+	err := keyring.Set("crc-test", "foo", "bar")
+	if err == nil {
+		_ = keyring.Delete("crc-test", "foo")
+		return true
+	}
+	logging.Debugf("Keyring is not accessible: %v", err)
+	return false
+}
