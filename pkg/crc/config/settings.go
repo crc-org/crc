b48775c14f6dbbc37ff2c365a30a62556ed98446
Anjan Nath <kaludios@gmail.com>
2022-09-08 14:57:49 +0530

    Fixes #3281 remove alpha from preset config option help
    this was missed and as preset feature has been working for users
    for many releases now we can remove the alpha feature tag

--- pkg/crc/config/settings.go
+++ pkg/crc/config/settings.go
@@ -57,7 +57,7 @@ func RegisterSettings(cfg *Config) {
 
 	// Preset setting should be on top because CPUs/Memory config depend on it.
 	cfg.AddSetting(Preset, string(preset.OpenShift), validatePreset, RequiresDeleteAndSetupMsg,
-		fmt.Sprintf("Virtual machine preset (alpha feature - valid values are: %s, %s and %s)", preset.Podman, preset.OpenShift, preset.OKD))
+		fmt.Sprintf("Virtual machine preset (valid values are: %s, %s and %s)", preset.Podman, preset.OpenShift, preset.OKD))
 	// Start command settings in config
 	cfg.AddSetting(Bundle, defaultBundlePath(cfg), validateBundlePath, SuccessfullyApplied,
 		fmt.Sprintf("Bundle path (string, default '%s')", defaultBundlePath(cfg)))
