2b74564c9063fb74315380c75296072920351407
Adrian Riobo Lorenzo <ariobolo@redhat.com>
2022-09-15 21:52:41 +0200

    e2e remove ux testing related functionality
    Current ux testing is covering old native trays. The new tray based on electron would require some adjustments, due to expected roadmap for CRC these adjustments for testing probably will be handle outside of CRC.
    This commit cleanup the e2e extended classes which support testing old native trays. Which are not used anymore.

--- test/extended/crc/ux/notification/const.go
+++
@@ -1,8 +0,0 @@
-package notification
-
-const (
-	// copyCommandMessage string = "OC Login command copied to clipboard, go ahead and login to your cluster"
-
-	notificationWaitTimeout int = 50
-	notificationWaitRetries int = 10
-)
--- vendor/github.com/RedHatQE/gowinx/pkg/win32/api/system-services/types.go
+++
@@ -1,7 +0,0 @@
-// +build windows
-package system_services
-
-// Process
-const (
-	PROCESS_ALL_ACCESS = 0x1F0FFF
-)
