27cd14835f1f7795f59587479a8e9c536340588b
Christophe Fergeau <cfergeau@redhat.com>
2022-09-20 10:53:39 +0200

    constants: Remove IsRelease/linuxReleaseBuild
    IsRelease() is no longer used in the codebase, so it can be removed.
    This in turn makes linuxReleaseBuild unused.

--- pkg/crc/version/version.go
+++ pkg/crc/version/version.go
@@ -33,11 +33,6 @@ var (
 	// will be true for releases on macos and windows
 	// will be false for git builds on macos and windows
 	installerBuild = "false"
-
-	// will always false on macos and windows
-	// will be true for release on linux
-	// will be false for git builds on linux
-	linuxReleaseBuild = "false"
 )
 
 const (
@@ -87,10 +82,6 @@ func GetTrayVersion() string {
 
 func IsInstaller() bool {
 	return installerBuild != "false"
-}
-
-func IsLinuxRelease() bool {
-	return linuxReleaseBuild != "false"
 }
 
 func InstallPath() string {
