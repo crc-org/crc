36e86cd99920bab43b12034bb3e7708302b3465f
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2022-11-17 10:19:25 +0530

    Update openshift bundle to 4.11.13 (fast channel)

--- pkg/crc/machine/bundle/constants.go
+++ pkg/crc/machine/bundle/constants.go
@@ -8,23 +8,23 @@ import (
 var bundleLocations = map[string]bundlesDownloadInfo{
 	"amd64": {
 		"darwin": {
-			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.11.7/crc_vfkit_4.11.7_amd64.crcbundle",
-				"c79a8de58b1c9e5dc9515e3d07da6516d28bf400c5c528d72940b008aaf81460"),
+			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.11.13/crc_vfkit_4.11.13_amd64.crcbundle",
+				"5e86e132e9a2d441dbbfa696c4f3256ade2018de66391d932f560bd5afa1415a"),
 		},
 
 		"linux": {
-			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.11.7/crc_libvirt_4.11.7_amd64.crcbundle",
-				"b6361b96d2c5c59e84279e3fc21a99bad9c680c3a3fe52b4302848c5f195dbb0"),
+			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.11.13/crc_libvirt_4.11.13_amd64.crcbundle",
+				"f5e3fb8be91dff8b573edcf915d0234314199a8547316ebf1b9bd467121c88db"),
 		},
 		"windows": {
-			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.11.7/crc_hyperv_4.11.7_amd64.crcbundle",
-				"6cd9d53309dd491139f3b8f1d00e11ccfc347e96726a8df19ff94ad6d951871a"),
+			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.11.13/crc_hyperv_4.11.13_amd64.crcbundle",
+				"e622db3f985d42e29c1029f7f7d02997feac2a13930faa2ed6580139570f4d30"),
 		},
 	},
 	"arm64": {
 		"darwin": {
-			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.11.7/crc_vfkit_4.11.7_arm64.crcbundle",
-				"4f8f088fcfd709417865e034c3c5d0854510d0737ce8be4117679e6f7b011f0a"),
+			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.11.13/crc_vfkit_4.11.13_arm64.crcbundle",
+				"86ce28ad5acf79e9eb2bd189d9059056fb1bb9689326abaf1db83aa456427e2c"),
 		},
 	},
 }
