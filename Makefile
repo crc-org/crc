crc-org/crc

commit 27cd14835f1f7795f59587479a8e9c536340588b

constants: Remove IsRelease/linuxReleaseBuild
IsRelease() is no longer used in the codebase, so it can be removed.
This in turn makes linuxReleaseBuild unused.

Makefile
@@ -257,7 +257,6 @@ gen_release_info:
 	@sed -i s/@PODMAN_VERSION@/$(PODMAN_VERSION)/ $(RELEASE_INFO)
 
 .PHONY: linux-release-binary macos-release-binary windows-release-binary
-linux-release-binary: LDFLAGS += -X '$(REPOPATH)/pkg/crc/version.linuxReleaseBuild=true' $(RELEASE_VERSION_VARIABLES)
 linux-release-binary: $(BUILD_DIR)/linux-amd64/crc
 
 macos-release-binary: LDFLAGS+= -X '$(REPOPATH)/pkg/crc/version.installerBuild=true' $(RELEASE_VERSION_VARIABLES)

pkg/crc/constants/constants.go
@@ -162,10 +162,6 @@ func EnsureBaseDirectoriesExist() error {
 	return nil
 }
 
-func IsRelease() bool {
-	return version.IsInstaller() || version.IsLinuxRelease()
-}
-
 func GetPublicKeyPath() string {
 	return filepath.Join(MachineInstanceDir, DefaultName, "id_ecdsa.pub")
 }

pkg/crc/version/version.go
@@ -33,11 +33,6 @@ var (
 	// will be true for releases on macos and windows
 	// will be false for git builds on macos and windows
 	installerBuild = "false"
-
-	// will always false on macos and windows
-	// will be true for release on linux
-	// will be false for git builds on linux
-	linuxReleaseBuild = "false"
 )
 
 const (
@@ -87,10 +82,6 @@ func GetTrayVersion() string {
 
 func IsInstaller() bool {
 	return installerBuild != "false"
-}
-
-func IsLinuxRelease() bool {
-	return linuxReleaseBuild != "false"
 }
 
 func InstallPath() string {

commit 30512d6e8da0a623df28a163cc67a67e418b89f0

build: Re-add release flags to 'linux-release-binary'
$(RELEASE_VERSION_VARIABLES) needs to be used when building a release
binary in order to use the correct telemetry key.
This was broken in https://github.com/crc-org/crc/commit/27cd14835f1f7795f59587479a8e9c536340588b
and explains why linux usage dropped since release 2.10.1.

Makefile
@@ -267,6 +267,7 @@ gen_release_info:
 	@sed -i s/@PODMAN_VERSION@/$(PODMAN_VERSION)/ $(RELEASE_INFO)
 
 .PHONY: linux-release-binary macos-release-binary windows-release-binary
+linux-release-binary: LDFLAGS+= $(RELEASE_VERSION_VARIABLES)
 linux-release-binary: $(BUILD_DIR)/linux-amd64/crc
 
 macos-release-binary: LDFLAGS+= -X '$(REPOPATH)/pkg/crc/version.installerBuild=true' $(RELEASE_VERSION_VARIABLES)