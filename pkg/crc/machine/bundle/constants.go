9b7b22333a64eee772fba2696167234591883885
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2023-03-21 17:29:31 +0530

    Update microshift bundle to 4.12.6 and use gcp location
    With this patch the bundle will be downloaded from the GCP location
    and we can able to create intermediate release.

--- pkg/crc/machine/bundle/constants.go
+++ pkg/crc/machine/bundle/constants.go
@@ -10,28 +10,28 @@ var bundleLocations = map[string]bundlesDownloadInfo{
 		"darwin": {
 			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.5/crc_vfkit_4.12.5_amd64.crcbundle",
 				"7a691040952b138b200d8bd7dd57ba352ba99f0c4006c976b3660bf7e2d5ccc0"),
-			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.5/crc_microshift_vfkit_4.12.5_amd64.crcbundle",
-				"18fa7827c5a15318cac75feae2aba40da5ae2c83703301276190b6212e6d93a8"),
+			preset.Microshift: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/microshift/4.12.6/crc_microshift_vfkit_4.12.6_amd64.crcbundle",
+				"ca1c72a9bc6a28b4b808899604eb4826d439d99f727c3ff0bc13fa0adaa4c43f"),
 		},
 		"linux": {
 			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.5/crc_libvirt_4.12.5_amd64.crcbundle",
 				"12797290dda62930f65e74fd12b8d634ef4d265f50ec708563f37b91ad68edc6"),
-			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.5/crc_microshift_libvirt_4.12.5_amd64.crcbundle",
-				"aa79ee3c88d5855f864096512e78627b153d896b8aa07782799a6cfdfef77322"),
+			preset.Microshift: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/microshift/4.12.6/crc_microshift_libvirt_4.12.6_amd64.crcbundle",
+				"ec97707b1ae4fa981a619d595e2d7d970c5a8f8ab81e00cae588a718ded89a98"),
 		},
 		"windows": {
 			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.5/crc_hyperv_4.12.5_amd64.crcbundle",
 				"f49945f40c9f219845d2086b66fcc29cbca3b8490bf6319d2a40f4e3e7bd1ad8"),
-			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.5/crc_microshift_hyperv_4.12.5_amd64.crcbundle",
-				"ed8eba68baba622011ceb9fd08a985097777c53ad941b3493b398c95512542c1"),
+			preset.Microshift: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/microshift/4.12.6/crc_microshift_hyperv_4.12.6_amd64.crcbundle",
+				"19a1dee43daca0d86a741ad75ce8797b715e01d8175fa63172f938801c54c9c2"),
 		},
 	},
 	"arm64": {
 		"darwin": {
 			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.5/crc_vfkit_4.12.5_arm64.crcbundle",
 				"f89fc8fa21c762e8e483575950bef5b2aa62e575df6ed0fb76051258ef6352e5"),
-			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.5/crc_microshift_vfkit_4.12.5_arm64.crcbundle",
-				"97fc903ce29ca4af99c8f834d4dc5b55366244e53375179063bd12111e251a73"),
+			preset.Microshift: download.NewRemoteFile("https://storage.googleapis.com/crc-bundle-github-ci/microshift/4.12.6/crc_microshift_vfkit_4.12.6_arm64.crcbundle",
+				"c1b1cc4ea86542995c8692977731e415d076adb3b27df8e8b49749fdb09af639"),
 		},
 	},
 }
--- pkg/crc/machine/bundle/constants_test.go
+++ pkg/crc/machine/bundle/constants_test.go
@@ -13,15 +13,15 @@ import (
 )
 
 func TestBundleVersionURI(t *testing.T) {
-	checkBundleVersionURI(t, preset.OpenShift)
-	checkBundleVersionURI(t, preset.Microshift)
+	checkBundleVersionURI(t, preset.OpenShift, "https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/%s/%s")
+	checkBundleVersionURI(t, preset.Microshift, "https://storage.googleapis.com/crc-bundle-github-ci/%s/%s")
 }
 
-func checkBundleVersionURI(t *testing.T, p preset.Preset) {
+func checkBundleVersionURI(t *testing.T, p preset.Preset, uriTmpl string) {
 	bundleVersion := version.GetBundleVersion(p)
 	require.NotEqual(t, bundleVersion, "0.0.0-unset", fmt.Sprintf("%s version is unset (%s), build flags are incorrect", p, bundleVersion))
 
-	uriPrefix := fmt.Sprintf("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/%s/%s", p, bundleVersion)
+	uriPrefix := fmt.Sprintf(uriTmpl, p, bundleVersion)
 	for _, osInfo := range bundleLocations {
 		for _, presetInfo := range osInfo {
 			for preset, remoteFile := range presetInfo {
