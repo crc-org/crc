93a9f29962dade8d48f1b43ce02ed6b0f17928e3
Anjan Nath <kaludios@gmail.com>
2023-04-26 20:55:33 +0530

    bump microshift openshift to 4.12.13 and update bundle hashes

--- pkg/crc/machine/bundle/constants.go
+++ pkg/crc/machine/bundle/constants.go
@@ -8,30 +8,30 @@ import (
 var bundleLocations = map[string]bundlesDownloadInfo{
 	"amd64": {
 		"darwin": {
-			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.9/crc_vfkit_4.12.9_amd64.crcbundle",
-				"7e83b6a4c4da6766b6b4981655d4bb38fd8f9da36ef1a5d16d017cd07d6ee7e9"),
-			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.9/crc_microshift_vfkit_4.12.9_amd64.crcbundle",
-				"8edf2df61e6b310f633bb06a658c340a7e94d1f304c6358e7020f43daac9c3dc"),
+			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.13/crc_vfkit_4.12.13_amd64.crcbundle",
+				"7f4bbdb80a748ffeb3331edbb604681372af581efde10797c51a3cf3abe3d627"),
+			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.13/crc_microshift_vfkit_4.12.13_amd64.crcbundle",
+				"ac1fd99e68c41c85e3454e65f379e4f2f5b3f94b1b83164a4d2f585ac6f0e00b"),
 		},
 		"linux": {
-			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.9/crc_libvirt_4.12.9_amd64.crcbundle",
-				"f57ab331ad092d8cb1f354b4308046c5ffd15bd143b19f841cb64b0fda89db67"),
-			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.9/crc_microshift_libvirt_4.12.9_amd64.crcbundle",
-				"8e2ef526bfe7973642e75cdb167d9122ae8b549eb2bb2d70432e607e1240bf3b"),
+			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.13/crc_libvirt_4.12.13_amd64.crcbundle",
+				"a5d500a61460f353882e08176e06802cafdc7c21401da25452f551deb13d5e34"),
+			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.13/crc_microshift_libvirt_4.12.13_amd64.crcbundle",
+				"b8e084b323f1cb5e644c78afe79fe18abdd5dd3eea17560270373f9f22a6e86c"),
 		},
 		"windows": {
-			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.9/crc_hyperv_4.12.9_amd64.crcbundle",
-				"a8267d09eac58e3c7f0db093f3cba83091390e5ac623b0a0282f6f55102b7681"),
-			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.9/crc_microshift_hyperv_4.12.9_amd64.crcbundle",
-				"f5ac6a032b50e340122d790cfec7bebcda7bb74d26fbd9479a5d001b2809beb7"),
+			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.13/crc_hyperv_4.12.13_amd64.crcbundle",
+				"418a7a6742d8861aa01fbf7b74d5355b4fe05bec290bd3c368569a99c3690556"),
+			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.13/crc_microshift_hyperv_4.12.13_amd64.crcbundle",
+				"ebcc47729ae0ecc1e6e1a2dd2fa9fc95f29296e38ae981d135c5f8228b059ac0"),
 		},
 	},
 	"arm64": {
 		"darwin": {
-			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.9/crc_vfkit_4.12.9_arm64.crcbundle",
-				"412d20e4969e872c24b14e55cbaa892848a1657b95a20f4af8ad4629ffdf73ab"),
-			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.9/crc_microshift_vfkit_4.12.9_arm64.crcbundle",
-				"13fd3074632f016ae6a98bc09eb069020d7a817b3d76f7b62be0b4bc6dee4a9e"),
+			preset.OpenShift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/openshift/4.12.13/crc_vfkit_4.12.13_arm64.crcbundle",
+				"91c488573e413e238b3703730918fbf5e21218597593687e9f2337cc4ff72a81"),
+			preset.Microshift: download.NewRemoteFile("https://mirror.openshift.com/pub/openshift-v4/clients/crc/bundles/microshift/4.12.13/crc_microshift_vfkit_4.12.13_arm64.crcbundle",
+				"f855c912cf5495e91a6ab5bcc16bbd373bae9964adb31085276886ae5f9221c3"),
 		},
 	},
 }
