2ff2e7a265f9e4ca37e47029f01bb3b53a0010b7
Yevhen Vydolob <yvydolob@redhat.com>
2023-06-06 16:13:16 +0300

    Add SSE client implementation
    Signed-off-by: Yevhen Vydolob <yvydolob@redhat.com>

---
+++ pkg/crc/api/client/sse_client.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/crc-org/crc/pkg/crc/logging"
+	"github.com/crc-org/crc/pkg/crc/machine/types"
+	"github.com/r3labs/sse/v2"
+)
+
+type SSEClient struct {
+	client *sse.Client
+}
+
+func NewSSEClient(transport *http.Transport) *SSEClient {
+	client := sse.NewClient("http://unix/events")
+	client.Connection.Transport = transport
+	return &SSEClient{
+		client: client,
+	}
+}
+
+func (c *SSEClient) Status(statusCallback func(*types.ClusterLoadResult)) error {
+	err := c.client.Subscribe("status", func(msg *sse.Event) {
+		wmState := &types.ClusterLoadResult{}
+		err := json.Unmarshal(msg.Data, wmState)
+		if err != nil {
+			logging.Errorf("Could not parse status event: %s", err)
+			return
+		}
+		statusCallback(wmState)
+	})
+
+	return err
+}
