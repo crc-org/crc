87d64392c7602fbcd7b12f429e652cdde2e003b0
Christophe Fergeau <cfergeau@redhat.com>
2023-07-18 15:09:44 +0300

    lint: cache: Fix 'redefines-builtin-id' error
    pkg/crc/cache/cache.go:36:1: redefines-builtin-id: redefinition of the built-in function new (revive)
    func new(executablePath string, archiveURL string, version string, getVersion func(string) (string, error)) *Cache {
    	return &Cache{executablePath: executablePath, archiveURL: archiveURL, version: version, getVersion: getVersion}

--- pkg/crc/cache/cache_linux.go
+++ pkg/crc/cache/cache_linux.go
@@ -5,7 +5,7 @@ import (
 )
 
 func NewMachineDriverLibvirtCache() *Cache {
-	return new(libvirt.MachineDriverPath(), libvirt.MachineDriverDownloadURL, libvirt.MachineDriverVersion, getCurrentLibvirtDriverVersion)
+	return newCache(libvirt.MachineDriverPath(), libvirt.MachineDriverDownloadURL, libvirt.MachineDriverVersion, getCurrentLibvirtDriverVersion)
 }
 
 func getCurrentLibvirtDriverVersion(executablePath string) (string, error) {
