d18927985e959af003045a4b203408e81065668a
Christophe Fergeau <cfergeau@redhat.com>
2023-09-29 12:41:22 +0200

    build: Add missing /v2/ to imports
    I broke the build by pushing too hastily a PR made before the /v2/
    changes :-/

--- pkg/crc/config/callbacks.go
+++ pkg/crc/config/callbacks.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"path/filepath"
 
-	"github.com/crc-org/crc/pkg/crc/constants"
-	"github.com/crc-org/crc/pkg/os"
+	"github.com/crc-org/crc/v2/pkg/crc/constants"
+	"github.com/crc-org/crc/v2/pkg/os"
 	"github.com/spf13/cast"
 )
 
