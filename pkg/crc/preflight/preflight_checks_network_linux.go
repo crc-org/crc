4e822f023958a37f78a60719aa1d37533bda31ef
Björn Lindström <bnl@vizrt.com>
2023-10-12 13:32:35 +0200

    Fix broken link to wiki page on userland network mode

--- pkg/crc/preflight/preflight_checks_network_linux.go
+++ pkg/crc/preflight/preflight_checks_network_linux.go
@@ -20,7 +20,7 @@ var nmPreflightChecks = []Check{
 		configKeySuffix:  "check-systemd-networkd-running",
 		checkDescription: "Checking if systemd-networkd is running",
 		check:            checkSystemdNetworkdIsNotRunning,
-		fixDescription:   "Network configuration with systemd-networkd is not supported. Perhaps you can try this new network mode: https://github.com/crc-org/crc/v2/wiki/VPN-support--with-an--userland-network-stack",
+		fixDescription:   "Network configuration with systemd-networkd is not supported. Perhaps you can try this new network mode: https://github.com/crc-org/crc/wiki/VPN-support--with-an--userland-network-stack",
 		flags:            NoFix,
 
 		labels: labels{Os: Linux, NetworkMode: System},
