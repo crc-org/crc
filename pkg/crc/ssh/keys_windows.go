7436243b86395e09553ef75704451c327e6f92c3
Christophe Fergeau <cfergeau@redhat.com>
2024-03-20 15:52:45 +0100

    windows: Fix permission of private ssh keys
    The default ssh client on Windows has strict checks on the permissions
    of the private key passed to `ssh -i`.
    crc uses hectane/go-acl to try to set acceptable permissions, but this
    is not enough, and ssh still complains.
    This PR uses acl.Apply directly instead of the acl.Chmod wrapper in
    order to limit access to the user running crc and noone else.
    
    Signed-off-by: Christophe Fergeau <cfergeau@redhat.com>

--- pkg/crc/ssh/keys_windows.go
+++ pkg/crc/ssh/keys_windows.go
@@ -5,7 +5,26 @@ import (
 	"os"
 
 	"github.com/hectane/go-acl"
+	"golang.org/x/sys/windows"
 )
+
+// This is based on https://github.com/hectane/go-acl/blob/ca0b05cb1adbf3d91585d8acab7bc804ee0b8583/chmod.go#L11-L38
+// but only sets an ACL for the current user, and not for the group or other users
+func winChmodUser(name string, fileMode os.FileMode) error {
+	// https://support.microsoft.com/en-us/help/243330/well-known-security-identifiers-in-windows-operating-systems
+	creatorOwnerSID, err := windows.StringToSid("S-1-3-0")
+	if err != nil {
+		return err
+	}
+
+	mode := uint32(fileMode)
+	return acl.Apply(
+		name,
+		true,
+		false,
+		acl.GrantSid(((mode&0700)<<23)|((mode&0200)<<9), creatorOwnerSID),
+	)
+}
 
 // WriteToFile writes keypair to files
 func (kp *KeyPair) WriteToFile(privateKeyPath string, publicKeyPath string) error {
@@ -35,7 +54,7 @@ func (kp *KeyPair) WriteToFile(privateKeyPath string, publicKeyPath string) erro
 			return ErrUnableToWriteFile
 		}
 
-		if err := acl.Chmod(v.File, 0600); err != nil {
+		if err := winChmodUser(v.File, 0600); err != nil {
 			return err
 		}
 	}
