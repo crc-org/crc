896d7f45e74778de3c03f302a5f15a0fa3f0453b
Christophe Fergeau <cfergeau@redhat.com>
2024-05-27 14:21:50 +0200

    macos: Boot the VM using UEFI

--- pkg/drivers/vfkit/driver_darwin.go
+++ pkg/drivers/vfkit/driver_darwin.go
@@ -170,11 +170,10 @@ func (d *Driver) Start() error {
 		return err
 	}
 
-	bootLoader := config.NewLinuxBootloader(
-		d.VmlinuzPath,
-		"console=hvc0 "+d.Cmdline,
-		d.InitrdPath,
-	)
+	efiStore := d.ResolveStorePath("efistore.nvram")
+	create := !crcos.FileExists(efiStore)
+
+	bootLoader := config.NewEFIBootloader(efiStore, create)
 
 	vm := config.NewVirtualMachine(
 		uint(d.CPU),
