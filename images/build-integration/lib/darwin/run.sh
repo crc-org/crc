7f5b7078024d3334c48e4bc1d56298c40b2297cf
Anjan Nath <kaludios@gmail.com>
2024-05-30 12:33:18 +0530

    Revert "[qe] unified darwin and linux scripts to execute integration and e2e tests remotely"
    This reverts commit 0f14537a2d9df5e3c839679f748eddf9461ef418.
    
    the ${OS} variable referred to on the scripts is only present when
    running inside the container, we'll have to figure out a different
    way to get the OS information when running on the host

--- images/build-integration/lib/darwin/run.sh
+++ images/build-integration/lib/darwin/run.sh
@@ -1,1 +1,63 @@
-./../linux/run.sh
+#!/bin/bash
+
+# Parameters
+bundleLocation=""
+targetFolder="crc-integration"
+junitFilename="integration-junit.xml"
+suiteTimeout="90m"
+labelFilter=""
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -bundleLocation)
+        bundleLocation="$2"
+        shift 
+        shift 
+        ;;
+        -targetFolder)
+        targetFolder="$2"
+        shift 
+        shift 
+        ;;
+        -junitFilename)
+        junitFilename="$2"
+        shift 
+        shift 
+        ;;
+        -suiteTimeout)
+        suiteTimeout="$2"
+        shift 
+        shift 
+        ;;
+        -labelFilter)
+        labelFilter="$2"
+        shift 
+        shift 
+        *)    # unknown option
+        shift 
+        ;;
+    esac
+done
+
+# Prepare resuslts folder
+mkdir -p $targetFolder/results
+
+# Run tests
+export PATH="$PATH:${HOME}/$targetFolder/bin"
+export PULL_SECRET_PATH="${HOME}/$targetFolder/pull-secret"
+if [ ! -z "$bundleLocation" ]
+then
+    export BUNDLE_PATH="$bundleLocation"
+fi
+cd $targetFolder/bin
+if [ ! -z "$labelFilter" ]
+then
+    ./integration.test --ginkgo.timeout $suiteTimeout --ginkgo.label-filter $labelFilter > integration.results
+else
+    ./integration.test --ginkgo.timeout $suiteTimeout > integration.results
+fi
+
+# Copy results
+cd ..
+cp bin/integration.results results/integration.results
+cp bin/out/integration.xml results/$junitFilename
