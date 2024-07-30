crc-org/crc

commit 1d104598dd1c33486fd142333abbdf1ca5c970a9

release-info: Test its format as part of 'make test'
This commit makes use of `//go:embed` to run the content of
`release-info.json` through golang `json.Valid` to check if it's valid
JSON.

This is better/simpler than using `jq` as we don't have to change all
our container images/CI envs/... to make sure `jq` is installed, and
`json.Valid` uses the same json parser as crc so this avoids possible
differences of behaviour between json parsers.

The `cross-lint` target must depend on `gen_release_info` otherwise it fails:
```
release_info_test.go:9:12: pattern release-info.json: no matching files found (typecheck)
```

Signed-off-by: Christophe Fergeau <cfergeau@redhat.com>

Makefile
@@ -128,8 +128,8 @@ generate_mocks: $(TOOLS_BINDIR)/mockery
 	$(TOOLS_BINDIR)/mockery --srcpkg ./pkg/crc/api/client --name Client --output test/mocks/api  --filename client.go
 
 .PHONY: test
-test:
-	go test -race --tags "build $(BUILDTAGS)" -v -ldflags="$(VERSION_VARIABLES)" ./pkg/... ./cmd/...
+test: gen_release_info
+	go test -race --tags "build $(BUILDTAGS)" -v -ldflags="$(VERSION_VARIABLES)" . ./pkg/... ./cmd/...
 
 .PHONY: spec test-rpmbuild
 
@@ -277,10 +277,10 @@ fmt: $(TOOLS_BINDIR)/goimports
 
 # Run golangci-lint against code
 .PHONY: lint cross-lint
-lint: $(TOOLS_BINDIR)/golangci-lint
+lint: $(TOOLS_BINDIR)/golangci-lint gen_release_info
 	"$(TOOLS_BINDIR)"/golangci-lint run
 
-cross-lint: $(TOOLS_BINDIR)/golangci-lint
+cross-lint: $(TOOLS_BINDIR)/golangci-lint gen_release_info
 	GOARCH=amd64 GOOS=darwin "$(TOOLS_BINDIR)"/golangci-lint run
 	GOARCH=arm64 GOOS=darwin "$(TOOLS_BINDIR)"/golangci-lint run
 	GOARCH=amd64 GOOS=linux "$(TOOLS_BINDIR)"/golangci-lint run

release_info_test.go
@@ -1,0 +1,16 @@
+package main
+
+import (
+	_ "embed"
+	"encoding/json"
+	"testing"
+)
+
+//go:embed release-info.json
+var releaseInfo []byte
+
+func TestReleaseInfo(t *testing.T) {
+	if !json.Valid(releaseInfo) {
+		t.Fatal("release-info.json is not a valid json file")
+	}
+}

commit 16b83db18f2580e0a84c30263ab0a9da1030a140

build: Ensure binaries are rebuilt on any source change
The Makefile has a `SOURCES` variable which contains a list of all go
source files used by crc. It's used as a dependency for the targets
building binaries so that the binaries are rebuilt on any source
changes.

This broke in 1d104598 when a release_info_test.go file was added to
the top level directory. Since `SOURCES` is set to
`git ls-files *.go ":^vendor"` shell expansion will happen before `git
ls-files` runs, so we'll be running
`git ls-files release_info_test.go ":^vendor"`, the `SOURCES` variable
will only contain `release_info_test.go` instead of the list of all
source files, and binaries won't get rebuilt when one of the go files
change.

This commit adds quoting around `*.go` so that this works properly.

Signed-off-by: Christophe Fergeau <cfergeau@redhat.com>

Makefile
@@ -36,7 +36,7 @@ ORG := github.com/crc-org
 MODULEPATH = $(ORG)/crc/v2
 PACKAGE_DIR := packaging/$(GOOS)
 
-SOURCES := $(shell git ls-files  *.go ":^vendor")
+SOURCES := $(shell git ls-files '*.go' ":^vendor")
 
 RELEASE_INFO := release-info.json
 