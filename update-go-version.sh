7b3ce360786c0ebfd17e35e60a2adbaaace5f664
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2024-09-17 12:13:18 +0530

    update-go-version: Add regex to change go version for containerfile also

--- update-go-version.sh
+++ update-go-version.sh
@@ -14,6 +14,7 @@ go mod edit -go "${golang_base_version}.0" tools/go.mod
 
 sed -i "s,^GOVERSION = 1.[0-9]\+,GOVERSION = ${golang_base_version}," Makefile
 sed -i "s,^\(FROM registry.ci.openshift.org/openshift/release:rhel-8-release-golang-\)1.[0-9]\+,\1${golang_base_version}," images/*/Dockerfile
+sed -i "s,^\(FROM registry.ci.openshift.org/openshift/release:rhel-8-release-golang-\)1.[0-9]\+,\1${golang_base_version}," images/*/Containerfile
 sed -i "s,^FROM registry.access.redhat.com/ubi8/go-toolset:[.0-9]\+,FROM registry.access.redhat.com/ubi8/go-toolset:${golang_base_version}," images/*/Dockerfile
 sed -i "s,^FROM registry.access.redhat.com/ubi8/go-toolset:[.0-9]\+,FROM registry.access.redhat.com/ubi8/go-toolset:${golang_base_version}," images/*/Containerfile
 for f in .github/workflows/*.yml; do
