38bc580523fc1388de0f1d527fe5e4e7ec820721
Matus Skvarla <mskvarla@redhat.com>
2025-03-10 06:39:56 +0100

    !!!!9p: added code from christophe

---
+++ pkg/crc/services/9p/plan9_windows.go
@@ -0,0 +1,35 @@
+package plan9
+
+import (
+	"fmt"
+
+	"github.com/linuxkit/virtsock/pkg/hvsock"
+	"github.com/sirupsen/logrus"
+)
+
+// StartVsockShares serving the given shares on Windows HVSocks for use by a Hyper-V VM.
+// Mounts is formatted as a map of directory to be shared to vsock GUID.
+// The vsocks used must already be defined before StartVsockShares is called; it's
+// expected that the vsocks will be created and torn down by the program calling
+// gvproxy.
+func StartVsockShares(mounts map[string]string) (defErr error) {
+	plan9Mounts := []Mount{}
+	for path, guid := range mounts {
+		service, err := hvsock.GUIDFromString(guid)
+		if err != nil {
+			return fmt.Errorf("parsing vsock guid %s: %w", guid, err)
+		}
+
+		listener, err := hvsock.Listen(hvsock.Addr{
+			VMID:      hvsock.GUIDWildcard,
+			ServiceID: service,
+		})
+		if err != nil {
+			return fmt.Errorf("retrieving listener for vsock %s: %w", guid, err)
+		}
+
+		logrus.Debugf("Going to serve directory %s on vsock %s", path, guid)
+		plan9Mounts = append(plan9Mounts, Mount{Listener: listener, Path: path})
+	}
+	return StartShares(plan9Mounts)
+}
