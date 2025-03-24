7546623023f8b74cb309f834549b42fb12712f9d
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-03-24 16:54:40 +0530

    Update-go-version: Add logic to update devcontainer image
    This PR make sure same image use for the devcontainer file and also for
    github action image

--- update-go-version.sh
+++ update-go-version.sh
@@ -15,6 +15,7 @@ go mod edit -go "${golang_base_version}.1" tools/go.mod
 sed -i "s,^GOVERSION = 1.[0-9]\+,GOVERSION = ${golang_base_version}," Makefile
 sed -i "s,^\(FROM registry.ci.openshift.org/openshift/release:rhel-8-release-golang-\)1.[0-9]\+,\1${golang_base_version}," images/*/Dockerfile
 sed -i "s,^\(FROM registry.ci.openshift.org/openshift/release:rhel-8-release-golang-\)1.[0-9]\+,\1${golang_base_version}," images/*/Containerfile
+sed -i "s,\(registry.ci.openshift.org/openshift/release:rhel-8-release-golang-\)1.[0-9]\+,\1${golang_base_version}," .devcontainer/devcontainer.json
 for f in .github/workflows/*.yml; do
     if [ $(yq  eval '.jobs.build.strategy.matrix | has("go")' "$f") == "true" ]; then
       yq eval --inplace ".jobs.build.strategy.matrix.go[0] = ${golang_base_version} | .jobs.build.strategy.matrix.go[0] style=\"single\"" "$f";
@@ -23,3 +24,5 @@ for f in .github/workflows/*.yml; do
       yq eval --inplace ".jobs.build-installer.strategy.matrix.go[0] = ${golang_base_version} | .jobs.build-installer.strategy.matrix.go[0] style=\"single\"" "$f";
     fi
 done
+# Use sed for verify-devcontainer because image is from registry.ci.openshift
+sed -i "s,\(registry.ci.openshift.org/openshift/release:rhel-8-release-golang-\)1.[0-9]\+,\1${golang_base_version}," .github/workflows/verify-devcontainer.yml
