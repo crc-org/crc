6c3f1dce4ac99c2a5d215cd44b9e1cdb27961b2a
Matus Skvarla <mskvarla@redhat.com>
2025-04-19 16:56:30 +0200

    9p: add libraries and dependencies

---
+++ vendor/github.com/DeedleFake/p9/proto/client.go
@@ -0,0 +1,223 @@
+package proto
+
+import (
+	"context"
+	"io"
+	"log"
+	"net"
+	"sync/atomic"
+	"time"
+
+	"github.com/DeedleFake/p9/internal/debug"
+)
+
+// Client provides functionality for sending requests to and receiving
+// responses from a server for a given protocol. It automatically
+// handles message tags, properly blocking until a matching tag
+// response has been received.
+type Client struct {
+	done   chan struct{}
+	cancel func()
+
+	p Proto
+	c net.Conn
+
+	nextTag   chan uint16
+	sentMsg   chan clientMsg
+	recvMsg   chan clientMsg
+	cancelMsg chan uint16
+
+	msize uint32
+}
+
+// NewClient initializes a client that communicates using c. The
+// Client will close c when the Client is closed.
+func NewClient(p Proto, c net.Conn) *Client {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	client := &Client{
+		done:   make(chan struct{}),
+		cancel: cancel,
+
+		p: p,
+		c: c,
+
+		nextTag:   make(chan uint16),
+		sentMsg:   make(chan clientMsg),
+		recvMsg:   make(chan clientMsg),
+		cancelMsg: make(chan uint16),
+
+		msize: 1024,
+	}
+	go client.reader(ctx)
+	go client.coord(ctx)
+
+	return client
+}
+
+// Dial is a convenience function that dials and creates a client in
+// the same step.
+func Dial(p Proto, network, addr string) (*Client, error) {
+	c, err := net.Dial(network, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewClient(p, c), nil
+}
+
+// Close cleans up resources created by the client as well as closing
+// the underlying connection.
+func (c *Client) Close() error {
+	c.cancel()
+	return c.c.Close()
+}
+
+// reader reads messages from the connection, sending them to the
+// coordinator to be sent to waiting Send calls.
+func (c *Client) reader(ctx context.Context) {
+	for {
+		err := c.c.SetReadDeadline(time.Now().Add(10 * time.Second))
+		if err != nil {
+			if ctx.Err() != nil {
+				return
+			}
+
+			log.Printf("Failed to set conn deadline: %v", err)
+			return
+		}
+
+		msg, tag, err := c.p.Receive(c.c, c.Msize())
+		if err != nil {
+			if (ctx.Err() != nil) || (err == io.EOF) {
+				return
+			}
+
+			continue
+		}
+
+		select {
+		case <-ctx.Done():
+			return
+
+		case c.recvMsg <- clientMsg{
+			tag:  tag,
+			recv: msg,
+		}:
+		}
+	}
+}
+
+// coord coordinates between Send calls and the reader.
+func (c *Client) coord(ctx context.Context) {
+	defer close(c.done)
+
+	var nextTag uint16
+	tags := make(map[uint16]chan any)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case cm := <-c.sentMsg:
+			tags[cm.tag] = cm.ret
+
+		case cm := <-c.recvMsg:
+			rcm, ok := tags[cm.tag]
+			if !ok {
+				continue
+			}
+
+			rcm <- cm.recv
+			delete(tags, cm.tag)
+
+		case tag := <-c.cancelMsg:
+			delete(tags, tag)
+
+		case c.nextTag <- nextTag:
+			for {
+				nextTag++
+				if _, ok := tags[nextTag]; !ok {
+					break
+				}
+			}
+		}
+	}
+}
+
+// Msize returns the maxiumum size of a message. This does not perform
+// any communication with the server.
+func (c *Client) Msize() uint32 {
+	return atomic.LoadUint32(&c.msize)
+}
+
+// SetMsize sets the maximum size of a message. This does not perform
+// any communication with the server.
+func (c *Client) SetMsize(size uint32) {
+	atomic.StoreUint32(&c.msize, size)
+}
+
+// Send sends a message to the server, blocking until a response has
+// been received. It is safe to place multiple Send calls
+// concurrently, and each will return when the response to that
+// request has been received.
+func (c *Client) Send(msg any) (any, error) {
+	debug.Log("client -> %#v\n", msg)
+
+	tag := NoTag
+	if _, ok := msg.(P9NoTag); !ok {
+		select {
+		case <-c.done:
+			return nil, ErrClientClosed
+		case tag = <-c.nextTag:
+		}
+	}
+
+	ret := make(chan any, 1)
+	select {
+	case <-c.done:
+		return nil, ErrClientClosed
+
+	case c.sentMsg <- clientMsg{
+		tag: tag,
+		ret: ret,
+	}:
+	}
+
+	err := c.p.Send(c.c, tag, msg)
+	if err != nil {
+		select {
+		case <-c.done:
+		case c.cancelMsg <- tag:
+		}
+		return nil, err
+	}
+
+	select {
+	case <-c.done:
+		return nil, ErrClientClosed
+	case rsp := <-ret:
+		debug.Log("client <- %#v\n", rsp)
+
+		if err, ok := rsp.(error); ok {
+			return nil, err
+		}
+		return rsp, nil
+	}
+}
+
+// Sometimes I think that some type of tuples would be nice...
+type clientMsg struct {
+	tag  uint16
+	recv any
+	ret  chan any
+}
+
+// P9NoTag is implemented by any types that should not use tags for
+// communicating. In 9P, for example, this is true of the Tversion
+// message type, as it must be the first thing sent and no further
+// communication can happen before an Rversion is sent in response.
+type P9NoTag interface {
+	P9NoTag()
+}
---
+++ vendor/github.com/DeedleFake/p9/proto/proto.go
@@ -0,0 +1,171 @@
+// Package proto provides a usage-inspecific wrapper around 9P's data
+// serialization and communication scheme.
+package proto
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"reflect"
+	"sync"
+
+	"github.com/DeedleFake/p9/internal/util"
+)
+
+var (
+	// ErrLargeMessage is returned by various functions if a message is
+	// larger than the current maximum message size.
+	ErrLargeMessage = errors.New("message larger than msize")
+
+	// ErrClientClosed is returned by attempts to send to a closed
+	// client.
+	ErrClientClosed = errors.New("client closed")
+)
+
+const (
+	// NoTag is a special tag that is used when a tag is unimportant.
+	NoTag uint16 = 0xFFFF
+)
+
+var bufPool = sync.Pool{
+	New: func() any {
+		return new(bytes.Buffer)
+	},
+}
+
+// Proto represents a protocol. It maps between message type IDs and
+// the Go types that those IDs correspond to.
+type Proto struct {
+	rmap map[uint8]reflect.Type
+	smap map[reflect.Type]uint8
+}
+
+// NewProto builds a Proto from the given one-way mapping.
+func NewProto(mapping map[uint8]reflect.Type) Proto {
+	rmap := make(map[uint8]reflect.Type, len(mapping))
+	smap := make(map[reflect.Type]uint8, len(mapping))
+	for id, t := range mapping {
+		if t.Kind() == reflect.Ptr {
+			t = t.Elem()
+		}
+
+		rmap[id] = t
+		smap[t] = id
+	}
+
+	return Proto{
+		rmap: rmap,
+		smap: smap,
+	}
+}
+
+// TypeFromID returns the Go type that corresponds to the given ID. If
+// the ID is not recognized, it returns nil.
+func (p Proto) TypeFromID(id uint8) reflect.Type {
+	return p.rmap[id]
+}
+
+// IDFromType returns the message type ID that corresponds to the
+// given Go type, and a boolean indicating that the mapping is valid.
+func (p Proto) IDFromType(t reflect.Type) (uint8, bool) {
+	id, ok := p.smap[t]
+	return id, ok
+}
+
+// Receive receives a message from r using the given maximum message
+// size. It returns the message, the tag that the message was sent
+// with, and an error, if any.
+func (p Proto) Receive(r io.Reader, msize uint32) (msg any, tag uint16, err error) {
+	var size uint32
+	err = Read(r, &size)
+	if err != nil {
+		return nil, NoTag, util.Errorf("receive: %w", err)
+	}
+
+	if (msize > 0) && (size > msize) {
+		return nil, NoTag, util.Errorf("receive: %w", ErrLargeMessage)
+	}
+
+	lr := &util.LimitedReader{
+		R: r,
+		N: size,
+		E: ErrLargeMessage,
+	}
+
+	var msgType uint8
+	err = Read(lr, &msgType)
+	if err != nil {
+		return nil, NoTag, util.Errorf("receive: failed to read message type: %w", err)
+	}
+
+	t := p.TypeFromID(msgType)
+	if t == nil {
+		if err != nil {
+			return nil, NoTag, err
+		}
+
+		return nil, NoTag, util.Errorf("receive: invalid message type: %v", msgType)
+	}
+
+	tag = NoTag
+	err = Read(lr, &tag)
+	if err != nil {
+		return nil, tag, util.Errorf("receive: failed to read tag: %w", err)
+	}
+
+	m := reflect.New(t)
+	err = Read(lr, m.Interface())
+	if err != nil {
+		return nil, tag, util.Errorf("receive %v: %w", m.Type().Elem(), err)
+	}
+
+	return m.Interface(), tag, err
+}
+
+// Send writes a message to w with the given tag. It returns any
+// errors that occur.
+//
+// Encoded messages are buffered locally before sending to ensure that
+// pieces of a message don't get mixed with another one.
+func (p Proto) Send(w io.Writer, tag uint16, msg any) (err error) {
+	buf := bufPool.Get().(*bytes.Buffer)
+	defer func() {
+		if err == nil {
+			_, err = io.Copy(w, buf)
+			if err != nil {
+				err = util.Errorf("send: %w", err)
+			}
+		}
+
+		buf.Reset()
+		bufPool.Put(buf)
+	}()
+
+	msgType, ok := p.IDFromType(reflect.Indirect(reflect.ValueOf(msg)).Type())
+	if !ok {
+		return util.Errorf("send: invalid message type: %T", msg)
+	}
+
+	write := func(v any) {
+		if err != nil {
+			return
+		}
+
+		err = Write(buf, v)
+		if err != nil {
+			err = util.Errorf("send %T: %w", msg, err)
+		}
+	}
+
+	n, err := Size(msg)
+	if err != nil {
+		return util.Errorf("send %T: %w", msg, err)
+	}
+
+	write(4 + 1 + 2 + n)
+	write(msgType)
+	write(tag)
+	write(msg)
+
+	return err
+}
