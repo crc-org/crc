4d0431db76eb2aed92ca148ff96718be6dcb18fc
Matus Skvarla <mskvarla@redhat.com>
2025-04-19 16:56:30 +0200

    9p: added libraries and dependencies

---
+++ vendor/github.com/DeedleFake/p9/addr_other.go
@@ -0,0 +1,29 @@
+// +build !linux,!darwin
+
+package p9
+
+import (
+	"os"
+	"os/user"
+	"path/filepath"
+)
+
+// NamespaceDir returns the path of the directory that is used for the
+// current namespace. On Unix-like systems, this is
+// /tmp/ns.$USER.$DISPLAY.
+//
+// If looking up the current user's name fails, this function will
+// panic.
+func NamespaceDir() string {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	display, ok := os.LookupEnv("DISPLAY")
+	if !ok {
+		display = ":0"
+	}
+
+	return filepath.Join(os.TempDir(), "ns."+u.Username+"."+display)
+}
