362ab9e0ea595d9b30a1969df3a3e6bae96de1ad
Matus Skvarla <mskvarla@redhat.com>
2025-04-19 16:56:30 +0200

    9p: added dependencies and libraries

---
+++ vendor/github.com/DeedleFake/p9/dir_other.go
@@ -0,0 +1,14 @@
+// +build !linux,!darwin,!plan9,!windows
+
+package p9
+
+import "os"
+
+func infoToEntry(fi os.FileInfo) DirEntry {
+	return DirEntry{
+		FileMode:  ModeFromOS(fi.Mode()),
+		MTime:     fi.ModTime(),
+		Length:    uint64(fi.Size()),
+		EntryName: fi.Name(),
+	}
+}
