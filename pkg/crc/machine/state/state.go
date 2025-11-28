10ff03a4ea12b31b1f32aee2f3d8b0539adf3ab8
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-11-28 16:02:05 +0530

    Remove libmachine api and use macadam binary
    This commit have following
    - macadam helper to provide value based on macadam command run
    - cloud init helper to create user metadata
    - remove of libmachine api
    - updating vsock to work with gvproxy socket
    - Some todo which need to be improved

--- pkg/crc/machine/state/state.go
+++ pkg/crc/machine/state/state.go
@@ -1,7 +1,5 @@
 package state
 
-import libmachinestate "github.com/crc-org/machine/libmachine/state"
-
 // State represents the state of crc (both VM and components)
 type State string
 
@@ -12,13 +10,3 @@ const (
 	Starting State = "Starting"
 	Error    State = "Error"
 )
-
-func FromMachine(input libmachinestate.State) State {
-	switch input {
-	case libmachinestate.Running:
-		return Running
-	case libmachinestate.Stopped:
-		return Stopped
-	}
-	return Error
-}
