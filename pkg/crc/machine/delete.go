4c369bde573999d0a866a79fef18f941158c4050
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-11-28 16:02:05 +0530

    Remove libmachine api and use macadam binary
    This commit have following
    - macadam helper to provide value based on macadam command run
    - cloud init helper to create user metadata
    - remove of libmachine api
    - updating vsock to work with gvproxy socket
    - Some todo which need to be improved

--- pkg/crc/machine/delete.go
+++ pkg/crc/machine/delete.go
@@ -9,21 +9,10 @@ import (
 )
 
 func (client *client) Delete() error {
-	vm, err := loadVirtualMachine(client.name, client.useVSock())
-	if err != nil && !errors.Is(err, errInvalidBundleMetadata) {
-		return errors.Wrap(err, "Cannot load machine")
-	}
-	defer vm.Close()
-
-	if err := vm.Remove(); err != nil {
+	m := getMacadamClient()
+	_, _, err := m.DeleteVM(client.name)
+	if err != nil {
 		return errors.Wrap(err, "Cannot remove machine")
-	}
-
-	// In case usermode networking make sure all the port bind on host should be released
-	if client.useVSock() {
-		if err := unexposePorts(); err != nil {
-			return err
-		}
 	}
 
 	if err := cleanKubeconfig(getGlobalKubeConfigPath(), getGlobalKubeConfigPath()); err != nil {
