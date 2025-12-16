75f3f531402e0e9e7bddef8d3f124f8c9a4a32af
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-12-16 12:34:01 +0530

    systemd: Add result helper to get service exit result

--- pkg/crc/systemd/systemd.go
+++ pkg/crc/systemd/systemd.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/crc-org/crc/v2/pkg/crc/ssh"
 	"github.com/crc-org/crc/v2/pkg/crc/systemd/actions"
+	"github.com/crc-org/crc/v2/pkg/crc/systemd/results"
 	"github.com/crc-org/crc/v2/pkg/crc/systemd/states"
 	crcos "github.com/crc-org/crc/v2/pkg/os"
 )
@@ -55,6 +56,18 @@ func (c Commander) Stop(name string) error {
 func (c Commander) Status(name string) (states.State, error) {
 	return c.service(name, actions.Status)
 
+}
+
+// Result returns the result of a service execution (success, exit-code, etc.)
+func (c Commander) Result(name string) (results.Result, error) {
+	stdOut, stdErr, err := c.commandRunner.Run("systemctl", "show", name, "--property=Result")
+	if err != nil {
+		return results.Unknown, fmt.Errorf("failed to get service result: %s %v: %s", stdOut, err, stdErr)
+	}
+
+	// Output format is "Result=success" or "Result=exit-code", etc.
+	output := strings.TrimSpace(stdOut)
+	return results.Compare(output), nil
 }
 
 func (c Commander) DaemonReload() error {
