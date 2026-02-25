1d80935be59bafed8d9e50580229ea446cc4861d
Brandon Palm <bpalm@redhat.com>
2026-02-25 09:20:38 -0600

    Issue #4752 Add generate-kubeconfig command
    Add a crc generate-kubeconfig command that outputs the kubeconfig for the
    CRC instance to stdout. Checks that the VM exists and is running before
    attempting to read the kubeconfig.

---
+++ cmd/crc/cmd/generate_kubeconfig.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/crc-org/crc/v2/pkg/crc/constants"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(generateKubeconfigCmd)
+}
+
+var generateKubeconfigCmd = &cobra.Command{
+	Use:   "generate-kubeconfig",
+	Short: "Generate a kubeconfig file for the CRC instance",
+	Long:  "Output the kubeconfig file for the CRC instance to stdout",
+	RunE: func(_ *cobra.Command, _ []string) error {
+		return runGenerateKubeconfig()
+	},
+}
+
+func runGenerateKubeconfig() error {
+	client := newMachine()
+	if err := checkIfMachineMissing(client); err != nil {
+		return err
+	}
+
+	running, err := client.IsRunning()
+	if err != nil {
+		return err
+	}
+	if !running {
+		return fmt.Errorf("the CRC instance is not running, cannot retrieve kubeconfig")
+	}
+
+	data, err := os.ReadFile(constants.KubeconfigFilePath)
+	if err != nil {
+		return fmt.Errorf("Error reading kubeconfig: %v", err)
+	}
+	fmt.Print(string(data))
+	return nil
+}
