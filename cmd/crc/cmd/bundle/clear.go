14148e8f62e753cca24ef8d2dfc02e4aa0c3f9ee
Brandon Palm <bpalm@redhat.com>
2026-03-11 10:28:19 -0500

    Issue #5037 Add bundle download command

---
+++ cmd/crc/cmd/bundle/clear.go
@@ -0,0 +1,58 @@
+package bundle
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/crc-org/crc/v2/pkg/crc/constants"
+	"github.com/crc-org/crc/v2/pkg/crc/logging"
+	"github.com/spf13/cobra"
+)
+
+func getClearCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "clear",
+		Short: "Clear cached CRC bundles",
+		Long:  "Delete all downloaded CRC bundles from the cache directory.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runClear()
+		},
+	}
+}
+
+func runClear() error {
+	cacheDir := constants.MachineCacheDir
+	if _, err := os.Stat(cacheDir); os.IsNotExist(err) {
+		logging.Infof("Cache directory %s does not exist", cacheDir)
+		return nil
+	}
+
+	files, err := os.ReadDir(cacheDir)
+	if err != nil {
+		return err
+	}
+
+	cleared := false
+	var lastErr error
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".crcbundle") {
+			safeName := filepath.Base(file.Name())
+			filePath := filepath.Join(cacheDir, safeName)
+			logging.Infof("Deleting %s", filePath)
+			if err := os.Remove(filePath); err != nil {
+				logging.Errorf("Failed to remove %s: %v", filePath, err)
+				lastErr = err
+			} else {
+				cleared = true
+			}
+		}
+	}
+
+	if !cleared && lastErr == nil {
+		logging.Infof("No bundles found in %s", cacheDir)
+	} else if cleared {
+		logging.Infof("Cleared cached bundles in %s", cacheDir)
+	}
+	return lastErr
+}
