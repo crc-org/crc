84ce09c600f7febc36eeab6459c2264f8f05679b
Brandon Palm <bpalm@redhat.com>
2026-04-08 13:37:50 -0500

    fix: return structured JSON errors from config API
    When setting config via the API with invalid values, detect MultiError
    at the HTTP response layer and return a JSON array of structured
    ValidationError objects with HTTP 422 and Content-Type: application/json.
    Preserve error response body in the API client via HTTPError.
    
    Resolves #2973

--- pkg/crc/api/helpers.go
+++ pkg/crc/api/helpers.go
@@ -2,11 +2,14 @@ package api
 
 import (
 	"encoding/json"
+	stderrors "errors"
 	"io"
 	"net/http"
 	"net/url"
 	"sync"
 
+	"github.com/crc-org/crc/v2/pkg/crc/api/client"
+	"github.com/crc-org/crc/v2/pkg/crc/errors"
 	"github.com/crc-org/crc/v2/pkg/crc/logging"
 )
 
@@ -111,6 +114,11 @@ func (s *server) Handler() http.Handler {
 			url:         r.URL,
 		}
 		if err := handler(c); err != nil {
+			var multiErr errors.MultiError
+			if stderrors.As(err, &multiErr) {
+				writeMultiError(w, multiErr)
+				return
+			}
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -124,4 +132,21 @@ func (s *server) Handler() http.Handler {
 			logging.Error("Failed to send response: ", err)
 		}
 	})
+}
+
+func writeMultiError(w http.ResponseWriter, multiErr errors.MultiError) {
+	validationErrors := make([]client.ValidationError, 0, len(multiErr.Errors))
+	for _, e := range multiErr.Errors {
+		if e == nil {
+			continue
+		}
+		validationErrors = append(validationErrors, client.ValidationError{
+			Message: e.Error(),
+		})
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusUnprocessableEntity)
+	if err := json.NewEncoder(w).Encode(validationErrors); err != nil {
+		logging.Error("Failed to write error response: ", err)
+	}
 }
